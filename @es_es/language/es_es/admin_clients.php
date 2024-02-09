4f56a126bb939a2138b333b0c147cc2fbbfcb1e0
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 11:50:08 -0800

    Updated es_es language files

---
+++ @es_es/language/es_es/admin_clients.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Admin Clients
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminClients.!success.note_added'] = 'La nota se ha creado correctamente.';
+$lang['AdminClients.!success.note_updated'] = 'La nota se ha actualizado correctamente.';
+$lang['AdminClients.!success.note_deleted'] = 'La nota se ha eliminado correctamente.';
+$lang['AdminClients.!success.client_added'] = 'El cliente se ha creado correctamente.';
+$lang['AdminClients.!success.client_updated'] = 'El cliente se ha actualizado correctamente.';
+$lang['AdminClients.!success.client_deleted'] = 'El cliente se ha eliminado correctamente.';
+$lang['AdminClients.!success.contact_added'] = 'El contacto se ha creado correctamente.';
+$lang['AdminClients.!success.contact_updated'] = 'El contacto se ha actualizado correctamente.';
+$lang['AdminClients.!success.invoice_added'] = 'La factura #%1$s se ha creado correctamente.';
+$lang['AdminClients.!success.invoice_updated'] = 'La factura #%1$s se ha actualizado correctamente.';
+$lang['AdminClients.!success.invoice_split'] = 'La factura #%1$s se ha dividido correctamente en la factura #%2$s.';
+$lang['AdminClients.!success.draftinvoice_added'] = 'Se ha creado correctamente el borrador de factura #%1$s.';
+$lang['AdminClients.!success.draftinvoice_updated'] = 'El borrador de factura #%1$s se ha actualizado correctamente.';
+$lang['AdminClients.!success.draftinvoice_created'] = 'El borrador de factura #%1$s se ha creado correctamente como factura #%2$s.';
+$lang['AdminClients.!success.quotation_added'] = 'Se ha creado correctamente la cita #%1$s.';
+$lang['AdminClients.!success.quotation_updated'] = 'La cita #%1$s se ha actualizado correctamente.';
+$lang['AdminClients.!success.draftquotation_added'] = 'Se ha creado correctamente el proyecto de presupuesto #%1$s.';
+$lang['AdminClients.!success.draftquotation_updated'] = 'El proyecto de presupuesto #%1$s se ha actualizado correctamente.';
+$lang['AdminClients.!success.approvequotation_approved'] = 'El presupuesto #%1$s ha sido aprobado.';
+$lang['AdminClients.!success.invoicequotation_invoiced'] = 'El presupuesto #%1$s se ha facturado correctamente como factura #%2$s.';
+$lang['AdminClients.!success.addccaccount_added'] = 'La cuenta de la tarjeta de crédito se ha creado correctamente.';
+$lang['AdminClients.!success.editccaccount_updated'] = 'La cuenta de la tarjeta de crédito se ha editado correctamente.';
+$lang['AdminClients.!success.addachaccount_added'] = 'La cuenta ACH se ha creado correctamente.';
+$lang['AdminClients.!success.editachaccount_updated'] = 'La cuenta ACH se ha editado correctamente.';
+$lang['AdminClients.!success.deleteccaccount_deleted'] = 'La cuenta de pago se ha eliminado correctamente.';
+$lang['AdminClients.!success.deleteachaccount_deleted'] = 'La cuenta de pago se ha eliminado correctamente.';
+$lang['AdminClients.!success.verifyachaccount_verified'] = 'La cuenta de pago se ha verificado correctamente.';
+$lang['AdminClients.!success.accounts_updated'] = 'Se ha actualizado la cuenta de pago que se utilizará para el adeudo automático.';
+$lang['AdminClients.!success.accounts_deleted'] = 'Se ha eliminado la cuenta de pago que se utilizará para el adeudo automático.';
+$lang['AdminClients.!success.accounttypes_updated'] = 'La configuración del tipo de pago aceptado se ha actualizado correctamente.';
+$lang['AdminClients.!success.recurinvoice_deleted'] = 'La factura periódica se ha eliminado correctamente.';
+$lang['AdminClients.!success.recurinvoice_updated'] = 'La factura periódica se ha actualizado correctamente.';
+$lang['AdminClients.!success.edittransaction_updated'] = 'La transacción se ha actualizado correctamente.';
+$lang['AdminClients.!success.transaction_unapplied'] = 'La transacción se ha anulado correctamente de la factura.';
+$lang['AdminClients.!success.invoices_queued'] = 'Las facturas seleccionadas se han puesto en cola para su entrega.';
+$lang['AdminClients.!success.invoices_delivered'] = 'Las facturas seleccionadas se han entregado correctamente.';
+$lang['AdminClients.!success.invoices_voided'] = 'Las siguientes facturas se han anulado correctamente: %1$s.';
+$lang['AdminClients.!success.quotations_delivered'] = 'Los presupuestos seleccionados se entregaron con éxito.';
+$lang['AdminClients.!success.quotations_status_updated'] = 'Se han actualizado correctamente las siguientes citas: %1$s.';
+$lang['AdminClients.!success.makepayment_processed'] = 'El pago de %1$s se ha procesado correctamente. Número de transacción: %2$s';
+$lang['AdminClients.!success.recordpayment_processed'] = 'El pago se ha registrado correctamente para %1$s.';
+$lang['AdminClients.!success.recordpayment_credits'] = 'Los créditos se grabaron correctamente.';
+$lang['AdminClients.!success.deletedraftinvoice_deleted'] = 'El borrador de factura se ha eliminado correctamente.';
+$lang['AdminClients.!success.email_sent'] = 'El correo electrónico se ha enviado correctamente.';
+$lang['AdminClients.!success.contact_deleted'] = 'El contacto %1$s %2$s se ha eliminado correctamente.';
+$lang['AdminClients.!success.service_added'] = 'El servicio se ha añadido correctamente.';
+$lang['AdminClients.!success.service_edited'] = 'El servicio se ha actualizado correctamente.';
+$lang['AdminClients.!success.service_deleted'] = 'El servicio se ha eliminado correctamente.';
+$lang['AdminClients.!success.packages_restricted'] = 'Se ha actualizado el acceso restringido a paquetes para este cliente.';
+$lang['AdminClients.!success.service_tab'] = 'Los datos se han actualizado correctamente.';
+$lang['AdminClients.!success.services_scheduled_cancel'] = 'Los servicios seleccionados fueron programados con éxito para ser cancelados.';
+$lang['AdminClients.!success.services_scheduled_uncancel'] = 'Ya no está prevista la cancelación de los servicios seleccionados.';
+$lang['AdminClients.!success.services_renewed'] = 'Los servicios seleccionados se renovaron con éxito.';
+$lang['AdminClients.!success.services_pushed'] = 'Los servicios seleccionados se han transferido correctamente al nuevo cliente.';
+$lang['AdminClients.!success.services_resent'] = 'El correo electrónico de bienvenida para los servicios seleccionados se ha generado correctamente.';
+$lang['AdminClients.!success.suspend_date_updated'] = 'La fecha de suspensión automática se ha actualizado correctamente.';
+$lang['AdminClients.!success.passwordreset.sent'] = 'El correo electrónico de restablecimiento de contraseña se ha enviado correctamente.';
+$lang['AdminClients.!error.invoices_emailinvalid'] = 'La dirección de correo electrónico no es válida.';
+$lang['AdminClients.!error.password'] = 'La contraseña no es válida.';
+$lang['AdminClients.!error.pay_with.required'] = 'Debe seleccionar un método de pago.';
+$lang['AdminClients.!error.invoice_credits.required'] = 'Seleccione una factura a la que se puedan aplicar créditos.';
+$lang['AdminClients.!error.quotation_invalid_status'] = 'El estado seleccionado no es válido.';
+$lang['AdminClients.!error.future_cancel_date'] = 'La fecha prevista de cancelación no debe ser anterior.';
+$lang['AdminClients.!error.invoices_not_voided'] = 'No se han podido anular las siguientes facturas porque contienen pagos parciales: %1$s.';
+$lang['AdminClients.!error.passwordreset.failed'] = 'No se ha podido enviar el correo electrónico de restablecimiento de contraseña.';
+$lang['AdminClients.!error.invoices_renew_service'] = 'El servicio no podrá renovarse hasta que se hayan abonado todas las facturas que lo contengan.';
+$lang['AdminClients.!notice.invoice_tax_rules_differ'] = 'ADVERTENCIA: Esta factura contiene normas fiscales que ya no están en vigor. Si añade o elimina una partida o actualiza la cantidad, el coste unitario o el estado fiscal, se aplicarán las normas fiscales más recientes a esta factura.';
+$lang['AdminClients.!notice.payment_type'] = 'ADVERTENCIA: %1$s pagos no están habilitados actualmente. Para habilitarlos, actualice el <a href="%2$s">%3$s</a>.';
+$lang['AdminClients.!notice.transactions_already_applied'] = 'ADVERTENCIA: La modificación del estado de esta transacción eliminará esta transacción de todas las facturas aplicadas actualmente.';
+$lang['AdminClients.!notice.service_in_review'] = 'El estado de este servicio es actualmente %1$s y debe cambiarse a %2$s antes de que pueda activarse. Esto puede hacerse a través del widget Pedidos de la página Resumen de facturación mediante el plugin Pedidos, o mediante un plugin de terceros responsable de la gestión de pedidos.';
+$lang['AdminClients.!notice.queued_service_change'] = 'Este servicio tiene cambios pendientes. La actualización de este servicio para subir o bajar cualquiera de sus valores hará que se cancelen los cambios pendientes actuales.';
+$lang['AdminClients.!notice.delete_client'] = 'Esto eliminará permanentemente el cliente y todos los registros asociados (por ejemplo, contactos, servicios, facturas, transacciones, cuentas de pago, etc.). SE TRATA DE UNA ACCIÓN PERMANENTE E IRREVOCABLE.';
+$lang['AdminClients.!notice.passwordreset.client_inactive'] = 'El correo electrónico de restablecimiento de contraseña sólo puede enviarse a clientes que estén activos.';
+$lang['AdminClients.!notice.client_limit'] = 'Este cliente ha alcanzado el número máximo de servicios para este paquete.';
+$lang['AdminClients.!notice.force_email_usernames'] = 'Tu nombre de usuario será el mismo que tu dirección de correo electrónico.';
+$lang['AdminClients.!notice.ach_verification'] = 'Deberá verificar esta cuenta antes de poder utilizarla para efectuar un pago.';
+
