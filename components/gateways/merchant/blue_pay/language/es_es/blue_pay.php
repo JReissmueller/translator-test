359fb4e8166744e00a00fdd0c805eb62d73f5d43
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:21:55 -0800

    Updated es_es language files

--- components/gateways/merchant/blue_pay/language/es_es/blue_pay.php
+++ components/gateways/merchant/blue_pay/language/es_es/blue_pay.php
@@ -9,12 +9,12 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Blue_pay.!error.account_id.empty'] = 'Di tnuocca ruoy retne esaelp';
-$lang['Blue_pay.!error.secret_key.empty'] = 'Yek terces ruoy retne esaelp';
-$lang['Blue_pay.!error.test_mode.valid'] = 'Nevig fi "eurt" ot tes eb tsum edom tset';
-$lang['Blue_pay.name'] = 'Yapeulb';
-$lang['Blue_pay.description'] = 'Acirema htron ni setarepo .sdeen s\'tnahcrem yreve tif ot snoitulos gnissecorp tnemyap eruces dna ,ysae ,tsaf sreffo yapeulb';
-$lang['Blue_pay.account_id'] = 'Di tnuocca';
-$lang['Blue_pay.secret_key'] = 'Yek terces';
-$lang['Blue_pay.test_mode'] = 'Edom tset';
+$lang['Blue_pay.!error.account_id.empty'] = 'Por favor, introduzca su ID de cuenta.';
+$lang['Blue_pay.!error.secret_key.empty'] = 'Por favor, introduzca su clave secreta.';
+$lang['Blue_pay.!error.test_mode.valid'] = 'El modo de prueba debe establecerse en "true" si se indica.';
+$lang['Blue_pay.name'] = 'BluePay';
+$lang['Blue_pay.description'] = 'BluePay ofrece soluciones de procesamiento de pagos rápidas, sencillas y seguras que se adaptan a las necesidades de cada comerciante. Opera en Norteamérica';
+$lang['Blue_pay.account_id'] = 'ID de cuenta';
+$lang['Blue_pay.secret_key'] = 'Clave secreta';
+$lang['Blue_pay.test_mode'] = 'Modo de prueba';
 
