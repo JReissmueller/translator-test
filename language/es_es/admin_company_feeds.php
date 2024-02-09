a66e02d6b0e28921c6b1181a28f9a3c144096b96
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:24:42 -0800

    Updated es_es language files

--- language/es_es/admin_company_feeds.php
+++ language/es_es/admin_company_feeds.php
@@ -9,15 +9,15 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['AdminCompanyFeeds.!success.datafeeds_updated'] = 'Detadpu yllufsseccus neeb evah sgnittes sdeef atad eht';
-$lang['AdminCompanyFeeds.!success.settings_updated'] = 'Detadpu yllufsseccus neeb evah sgnittes deef eht';
-$lang['AdminCompanyFeeds.index.page_title'] = 'Sdeef atad > ynapmoc > sgnittes';
-$lang['AdminCompanyFeeds.index.boxtitle_data_feeds'] = 'Sdeef atad';
-$lang['AdminCompanyFeeds.index.data_feeds_description'] = 'Erom dna ,secirp ,seman egakcap ekil ,setisbew lanretxe no deddebme eb ot atselb morf tnetnoc wolla sdeef atad';
-$lang['AdminCompanyFeeds.index.field_enable_data_feeds'] = 'Sdeef atad elbane';
-$lang['AdminCompanyFeeds.index.field_submit'] = 'Sgnittes etadpu';
-$lang['AdminCompanyFeeds.settings.page_title'] = 'Sgnittes deef > sdeef atad > ynapmoc > sgnittes';
-$lang['AdminCompanyFeeds.settings.boxtitle_feed_settings'] = 'Sgnittes deef';
-$lang['AdminCompanyFeeds.settings.header_endpoints'] = 'Stniopdne';
-$lang['AdminCompanyFeeds.settings.field_submit'] = 'Sgnittes etadpu';
+$lang['AdminCompanyFeeds.!success.datafeeds_updated'] = 'La configuración de la alimentación de datos se ha actualizado correctamente.';
+$lang['AdminCompanyFeeds.!success.settings_updated'] = 'Los ajustes de alimentación se han actualizado correctamente.';
+$lang['AdminCompanyFeeds.index.page_title'] = 'Configuración > Empresa > Alimentación de datos';
+$lang['AdminCompanyFeeds.index.boxtitle_data_feeds'] = 'Fuentes de datos';
+$lang['AdminCompanyFeeds.index.data_feeds_description'] = 'Los feeds de datos permiten incrustar contenido de Blesta en sitios web externos, como nombres de paquetes, precios y mucho más.';
+$lang['AdminCompanyFeeds.index.field_enable_data_feeds'] = 'Activar la alimentación de datos';
+$lang['AdminCompanyFeeds.index.field_submit'] = 'Actualizar ajustes';
+$lang['AdminCompanyFeeds.settings.page_title'] = 'Configuración > Empresa > Alimentación de datos > Configuración de la alimentación';
+$lang['AdminCompanyFeeds.settings.boxtitle_feed_settings'] = 'Ajustes de alimentación';
+$lang['AdminCompanyFeeds.settings.header_endpoints'] = 'Puntos finales';
+$lang['AdminCompanyFeeds.settings.field_submit'] = 'Actualizar ajustes';
 
