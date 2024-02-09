73072cf39bc9f9509f28b604166776fe254e5e38
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:10 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/blue_pay/language/es_es/blue_pay.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Blue Pay
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Blue_pay.!error.account_id.empty'] = 'Di tnuocca ruoy retne esaelp';
+$lang['Blue_pay.!error.secret_key.empty'] = 'Yek terces ruoy retne esaelp';
+$lang['Blue_pay.!error.test_mode.valid'] = 'Nevig fi "eurt" ot tes eb tsum edom tset';
+$lang['Blue_pay.name'] = 'Yapeulb';
+$lang['Blue_pay.description'] = 'Acirema htron ni setarepo .sdeen s\'tnahcrem yreve tif ot snoitulos gnissecorp tnemyap eruces dna ,ysae ,tsaf sreffo yapeulb';
+$lang['Blue_pay.account_id'] = 'Di tnuocca';
+$lang['Blue_pay.secret_key'] = 'Yek terces';
+$lang['Blue_pay.test_mode'] = 'Edom tset';
+
