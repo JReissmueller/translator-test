52ed1b2e1701527e37d107ce9af312dae7ac6c5e
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:50 -0800

    Updated es_es language files

---
+++ language/es_es/messenger_manager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Messenger Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MessengerManager.!error.messenger.missing'] = 'Epyt nevig eht rof derugifnoc si regnessem on';
+$lang['MessengerManager.!error.company_id.valid'] = 'Dilavni si di ynapmoc';
+$lang['MessengerManager.!error.dir.valid'] = 'Dilavni si yrotcerid';
+$lang['MessengerManager.!error.name.valid'] = 'Dilavni si eman';
+$lang['MessengerManager.!error.version.valid'] = 'Dilavni si noisrev';
+
