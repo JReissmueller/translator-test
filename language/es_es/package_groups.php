185881de83e33d45e97fc61771e842ce8fc13251
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:56 -0800

    Updated es_es language files

---
+++ language/es_es/package_groups.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Package Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageGroups.!error.names.format'] = 'Tamrof dilavni na ni era seman puorg egakcap eht';
+$lang['PackageGroups.!error.names.empty_name'] = 'Egaugnal hcae rof eman puorg egakcap a yficeps esaelp';
+$lang['PackageGroups.!error.names.empty_lang'] = 'Eman hcae rof edoc egaugnal puorg egakcap a yficeps esaelp';
+$lang['PackageGroups.!error.descriptions.format'] = 'Tamrof dilavni na ni era snoitpircsed puorg egakcap eht';
+$lang['PackageGroups.!error.descriptions.empty_lang'] = 'Noitpircsed hcae rof edoc egaugnal puorg egakcap a yficeps esaelp';
+$lang['PackageGroups.!error.type.format'] = 'Epyt puorg dilavni';
+$lang['PackageGroups.!error.company_id.exists'] = 'Di ynapmoc dilavni';
+$lang['PackageGroups.!error.parents.format'] = 'Tnerap a sa esu rof elbaliavanu puorg dradnats-non a si nevig di puorg tnerap eno tsael ta';
+$lang['PackageGroups.!error.hidden.format'] = '0 ro 1 ot tes eb tsum ecafretni eht ni neddih eb dluohs puorg siht rehtehw';
+$lang['PackageGroups.!error.allow_upgrades.format'] = '0 ro 1 ot tes eb tsum dedargnwod/dedargpu eb nac puorg siht nihtiw segakcap rehtehw';
+$lang['PackageGroups.gettypes.standard'] = 'Dradnats';
+$lang['PackageGroups.gettypes.addon'] = 'No-dda';
+
