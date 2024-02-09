732dd7f06ce19104ddcb38a848ab56c60d8798a9
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:24 -0800

    Updated es_es language files

---
+++ plugins/client_documents/language/es_es/admin_main.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.document_uploaded'] = 'Dedaolpu neeb sah tnemucod eht';
+$lang['AdminMain.!success.document_deleted'] = 'Deteled neeb sah tnemucod eht';
+$lang['AdminMain.index.page_title'] = 'Stnemucod';
+$lang['AdminMain.add.page_title'] = 'Dda > stnemucod';
+$lang['AdminMain.index.categorylink_adddocument'] = 'Tnemucod dda';
+$lang['AdminMain.index.boxtitle_client'] = '%3$s %2$s - %1$s# tneilc';
+$lang['AdminMain.index.boxtitle_documents'] = 'Stnemucod';
+$lang['AdminMain.index.no_results'] = 'Tneilc siht rof tes stnemucod on era ereht';
+$lang['AdminMain.index.heading_name'] = 'Eman';
+$lang['AdminMain.index.heading_description'] = 'Noitpircsed';
+$lang['AdminMain.index.heading_date_added'] = 'Dedda etad';
+$lang['AdminMain.index.heading_date_options'] = 'Snoitpo';
+$lang['AdminMain.index.option_delete'] = 'Eteled';
+$lang['AdminMain.index.option_download'] = 'Daolnwod';
+$lang['AdminMain.modal.delete_document'] = '?tnemucod siht eteled yllaer';
+$lang['AdminMain.add.boxtitle_adddocument'] = 'Tnemucod daolpu';
+$lang['AdminMain.add.field_name'] = 'Eman';
+$lang['AdminMain.add.field_document'] = 'Tnemucod';
+$lang['AdminMain.add.field_description'] = 'Noitpircsed';
+$lang['AdminMain.add.field_submit'] = 'Tnemucod daolpu';
+
