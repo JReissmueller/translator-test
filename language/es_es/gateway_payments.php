8b173964f986d1b3d4e0c999d5b0bbd3507550d1
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:43 -0800

    Updated es_es language files

---
+++ language/es_es/gateway_payments.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Gateway Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GatewayPayments.!error.gateway.exists'] = 'Delbane ton si ro tsixe ton seod yawetag eht';
+$lang['GatewayPayments.!error.transaction_id.exists'] = 'Tneilc taht rof dnuof eb ton dluoc noitcasnart eht';
+$lang['GatewayPayments.!error.account_id.exists'] = 'Tneilc taht rof dnuof eb ton dluoc tnuocca tnemyap eht';
+$lang['GatewayPayments.!error.contact_id.exists'] = 'Tneilc taht rof dnuof eb ton dluoc tcatnoc eht';
+$lang['GatewayPayments.!error.type.valid'] = 'Yawetag siht rof detroppus ton si epyt tnemyap eht';
+$lang['GatewayPayments.!error.type.instance'] = 'Rennam siht ni stnemyap troppus ton seod yawetag eht';
+$lang['GatewayPayments.!error.amount.valid'] = 'Orez naht retaerg eb tsum tnuoma';
+$lang['GatewayPayments.!error.reference_id.store'] = 'Tnuocca tnemyap eht erots ton dluoc yawetag tnemyap eht';
+$lang['GatewayPayments.!error.reference_id.update'] = 'Tnuocca tnemyap eht etadpu ton dluoc yawetag tnemyap eht';
+$lang['GatewayPayments.!error.reference_id.verify'] = 'Tnuocca tnemyap eht yfirev ton dluoc yawetag tnemyap eht';
+$lang['GatewayPayments.!error.reference_id.remove'] = 'Tnuocca tnemyap eht evomer ton dluoc yawetag tnemyap eht';
+$lang['GatewayPayments.!error.response_status'] = 'Esnopser detcepxenu na denruter yawetag tnemyap eht';
+$lang['GatewayPayments.!error.gateway.declined'] = 'Denilced saw tnemyap eht';
+$lang['GatewayPayments.!error.gateway.error'] = 'Tseuqer eht gnissecorp nehw rorre na denruter yawetag tnemyap eht';
+$lang['GatewayPayments.!error.type.invalid'] = '"cc" ro "hca" eb tsum epyt tnuocca eht';
+
