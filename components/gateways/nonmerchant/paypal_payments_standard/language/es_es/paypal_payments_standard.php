fa1df88975849719463f265e472217be4bf7cc9a
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:34 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/paypal_payments_standard/language/es_es/paypal_payments_standard.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Paypal Payments Standard
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PaypalPaymentsStandard.name'] = 'Dradnats stnemyap lapyap';
+$lang['PaypalPaymentsStandard.description'] = 'Enilno stnemyap lapyap dna drac tiderc tpecca yleruces ot syaw tseisae eht fo eno si dradnats stnemyap lapyap';
+$lang['PaypalPaymentsStandard.account_id'] = 'Liame tnuocca lapyap';
+$lang['PaypalPaymentsStandard.page_style'] = 'Eman elyts egap';
+$lang['PaypalPaymentsStandard.page_style_note'] = 'Elyts egap tluafed eht rof knalb evael .lapyap ni detaerc uoy emeht egap tnemyap motsuc eht fo eman elyts egap eht';
+$lang['PaypalPaymentsStandard.dev_mode'] = 'Edom repoleved';
+$lang['PaypalPaymentsStandard.dev_mode_note'] = 'Tnuocca xobdnas lapyap a htiw gnitset era uoy fi noitpo siht elbane ylno .tnemnorivne xobdnas lapyap eht ot snoitcasnart tsop lliw noitpo siht gnilbane';
+$lang['PaypalPaymentsStandard.api_username'] = 'Emanresu ipa';
+$lang['PaypalPaymentsStandard.api_username_note'] = 'Lapyap hguorht sdnufer ssecorp ot redro ni deriuqer era ,erutangis ipa dna drowssap ipa sa llew sa ,siht';
+$lang['PaypalPaymentsStandard.api_password'] = 'Drowssap ipa';
+$lang['PaypalPaymentsStandard.api_signature'] = 'Erutangis ipa';
+$lang['PaypalPaymentsStandard.pay_type'] = 'Snoitpo tnemyap';
+$lang['PaypalPaymentsStandard.pay_type_onetime'] = 'Ylno stnemyap emit eno';
+$lang['PaypalPaymentsStandard.pay_type_subscribe'] = 'Ylno stnemyap noitpircsbus';
+$lang['PaypalPaymentsStandard.pay_type_both'] = 'Elbissop nehw stnemyap noitpircsbus dna emit eno';
+$lang['PaypalPaymentsStandard.buildprocess.submit'] = 'Lapyap htiw yap';
+$lang['PaypalPaymentsStandard.!error.account_id.valid'] = 'Sserdda liame dilav a retne tsum uoy';
+$lang['PaypalPaymentsStandard.!error.dev_mode.valid'] = 'Nevig fi "eurt" ot tes eb tsum edom repoleved';
+
