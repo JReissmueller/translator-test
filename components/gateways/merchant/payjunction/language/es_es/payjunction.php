7bb28d0844a62efe33137dd796de251caf3bc7d0
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:14:59 -0800

    Updated es_es language files

--- components/gateways/merchant/payjunction/language/es_es/payjunction.php
+++ components/gateways/merchant/payjunction/language/es_es/payjunction.php
@@ -9,18 +9,18 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['PayJunction.name'] = 'Noitcnujyap';
-$lang['PayJunction.description'] = 'Emulov drac tiderc ylhtnom ni erom ro 000,01$ gnissecorp sessenisub tnemtraped-itlum dna noitacol-itlum rof tif laedi na si ygolonhcet sti .yldipar gniworg si dna yllaunna noillib 4$ sessecorp noitcnujyap';
-$lang['PayJunction.api_key'] = 'Yek ipa';
-$lang['PayJunction.user_name'] = 'Emanresu';
-$lang['PayJunction.password'] = 'Drowssap';
-$lang['PayJunction.test_mode'] = 'Edom repoleved';
-$lang['PayJunction.api_key_note'] = 'Tnuocca noitcnujyap ruoy morf yek ipa eht retne';
-$lang['PayJunction.user_name_note'] = 'Emanresu tnuocca noitcnujyap ruoy retne';
-$lang['PayJunction.password_note'] = 'Drowssap tnuocca noitcnujyap ruoy retne';
-$lang['PayJunction.test_mode_note'] = 'Edom repoleved esu ot ,yek ipa sbal ruoy sa llew sa ,drowssap dna emanresu omed eht esu tsum uoy .tnemnorivne omed sbal noitcnujyap eht ot snoitcasnart tsop lliw edom repoleved gnilbane';
-$lang['PayJunction.!error.api_key.empty'] = 'Yek ipa dilav a retne esaelp';
-$lang['PayJunction.!error.user_name.empty'] = 'Emanresu tnuocca ruoy retne esaelp';
-$lang['PayJunction.!error.password.empty'] = 'Drowssap tnuocca ruoy retne esaelp';
-$lang['PayJunction.!error.test_mode.valid'] = 'Nevig fi \'eurt\' ot tes eb tsum edom repoleved';
+$lang['PayJunction.name'] = 'PayJunction';
+$lang['PayJunction.description'] = 'PayJunction procesa 4.000 millones de dólares al año y está creciendo rápidamente. Su tecnología es ideal para empresas con varias ubicaciones y departamentos que procesan un volumen mensual de tarjetas de crédito igual o superior a 10.000 dólares.';
+$lang['PayJunction.api_key'] = 'Clave API';
+$lang['PayJunction.user_name'] = 'Nombre de usuario';
+$lang['PayJunction.password'] = 'Contraseña';
+$lang['PayJunction.test_mode'] = 'Modo Desarrollador';
+$lang['PayJunction.api_key_note'] = 'Introduzca la clave API de su cuenta PayJunction.';
+$lang['PayJunction.user_name_note'] = 'Introduzca el nombre de usuario de su cuenta PayJunction.';
+$lang['PayJunction.password_note'] = 'Introduzca la contraseña de su cuenta PayJunction.';
+$lang['PayJunction.test_mode_note'] = 'Activando el Modo Desarrollador se publicarán transacciones en el entorno de demostración de PayJunction Labs. Debe utilizar el nombre de usuario y la contraseña de demostración, así como su clave de API de Labs, para utilizar el modo de desarrollador.';
+$lang['PayJunction.!error.api_key.empty'] = 'Introduzca una clave API válida.';
+$lang['PayJunction.!error.user_name.empty'] = 'Introduzca el nombre de usuario de su cuenta.';
+$lang['PayJunction.!error.password.empty'] = 'Introduzca la contraseña de su cuenta.';
+$lang['PayJunction.!error.test_mode.valid'] = 'El Modo Desarrollador debe ser \'true\' si se da.';
 
