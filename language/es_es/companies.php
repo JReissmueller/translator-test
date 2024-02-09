d2bae622e645b7ecb99ed4f6351366354b92f9b2
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:30 -0800

    Updated es_es language files

---
+++ language/es_es/companies.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Companies
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Companies.!error.company_id.in_use'] = 'Deteled eb ton yam dna esu ni yltnerruc si ynapmoc taht';
+$lang['Companies.!error.company_id.exists'] = 'Di ynapmoc dilavni';
+$lang['Companies.!error.name.empty'] = 'Eman a retne esaelp';
+$lang['Companies.!error.name.length'] = 'Sretcarahc 46 deecxe ton yam htgnel eman';
+$lang['Companies.!error.hostname.valid'] = 'Emantsoh dilav a retne esaelp';
+$lang['Companies.!error.hostname.length'] = 'Htgnel ni sretcarahc 552 deecxe ton yam emantsoh eht';
+$lang['Companies.!error.hostname.unique'] = 'Ynapmoc rehtona yb nekat neeb ydaerla sah emantsoh taht';
+$lang['Companies.!error.phone.length'] = 'Sretcarahc 46 deecxe ton yam htgnel enohp';
+$lang['Companies.!error.fax.length'] = 'Sretcarahc 46 deecxe ton yam htgnel xaf';
+$lang['Companies.!error.private_key_passphrase.valid'] = 'Dilavni si deretne uoy esarhpssap eht';
+$lang['Companies.!error.quota.allowed'] = 'Sesnecil ynapmoc lanoitidda esahcrup ot selas tcatnoc ro moc.atselb.tnuocca ta tnuocca ruoy ot ni gol esaelp .elbaliava sesnecil ynapmoc nodda on era ereht';
+$lang['Companies.!error.inv_format.format'] = 'Tamrof tfard eciovni eht htiw tcilfnoc ton nac tamrof eciovni';
+$lang['Companies.!error.inv_format.contains'] = '}mun{ niatnoc tsum tamrof eciovni';
+$lang['Companies.!error.inv_draft_format.format'] = 'Tamrof eciovni eht htiw tcilfnoc ton nac tamrof tfard eciovni';
+$lang['Companies.!error.inv_draft_format.contains'] = '}mun{ niatnoc tsum tamrof tfard eciovni';
+$lang['Companies.!error.inv_start.number'] = 'Rebmun a eb tsum eulav trats eciovni';
+$lang['Companies.!error.inv_increment.number'] = 'Rebmun a eb tsum eulav tnemercni eciovni';
+$lang['Companies.!error.inv_pad_size.number'] = 'Rebmun a eb tsum ezis gniddap eciovni';
+$lang['Companies.!error.inv_pad_str.length'] = 'Retcarahc elgnis a eb tsum retcarahc gniddap eciovni';
+$lang['Companies.!error.inv_proforma_format.format'] = 'Tamrof eciovni tfard eht htiw tcilfnoc ton nac tamrof eciovni amrof orp';
+$lang['Companies.!error.inv_proforma_format.contains'] = '}mun{ niatnoc tsum tamrof eciovni amrof orp';
+$lang['Companies.!error.inv_proforma_start.number'] = 'Rebmun a eb tsum eulav trats eciovni amrof orp';
+
