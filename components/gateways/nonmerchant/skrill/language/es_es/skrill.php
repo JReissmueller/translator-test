2da9a8cc116b74a3a91baabc17014d9030236262
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:38 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/skrill/language/es_es/skrill.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Skrill
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Skrill.name'] = 'Llirks';
+$lang['Skrill.description'] = 'Srefsnart yenom lanoitanretni tsoc-wol no sucof a htiw mroftalp ecremmoc-e desab nodnol a';
+$lang['Skrill.user_email'] = 'Liame';
+$lang['Skrill.secret_word'] = 'Drow terces';
+$lang['Skrill.merchant_id'] = 'Di tnahcrem';
+$lang['Skrill.mqi'] = 'Drowssap iqm';
+$lang['Skrill.secret_word_note'] = 'Drow terces tnuocca llirks ruoy retne';
+$lang['Skrill.user_email_note'] = 'Sserdda liame tnuocca llirks ruoy retne';
+$lang['Skrill.merchant_id_note'] = 'Di tnahcrem tnuocca llirks ruoy retne';
+$lang['Skrill.mqi_note'] = 'Drowssap ipa iqm tnuocca llirks ruoy retne';
+$lang['Skrill.buildprocess.submit'] = 'Llirks htiw yap';
+$lang['Skrill.invoice.description'] = 'Seciovni';
+$lang['Skrill.!error.libxml_required'] = 'Yawetag siht rof deriuqer era snoisnetxe lmxelpmis dna lmxbil eht';
+$lang['Skrill.!error.user_email.valid'] = 'Sserdda liame dilav a retne esaelp';
+$lang['Skrill.!error.secret_word.empty'] = 'Drow terces tnuocca ruoy retne esaelp';
+$lang['Skrill.!error.mqi.empty'] = 'Drowssap iqm ruoy retne esaelp';
+$lang['Skrill.!error.merchant_id.empty'] = 'Di tnahcrem ruoy retne esaelp';
+
