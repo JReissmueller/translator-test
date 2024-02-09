a55c518a88d063702967755fb8634a3e5cfd6608
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:22 -0800

    Updated es_es language files

---
+++ language/es_es/client_main.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.!success.client_updated'] = 'Detadpu yllufsseccus neeb sah noitamrofni tnuocca ruoy';
+$lang['ClientMain.!success.invoice_method_updated'] = '%1$s aiv uoy ot dereviled eb lliw seciovni erutuf lla .detadpu yllufsseccus neeb sah dohtem eciovni ruoy';
+$lang['ClientMain.!info.invoice_due_title'] = '!%1$s ,kcab emoclew';
+$lang['ClientMain.!info.invoice_due_button'] = 'Tnemyap ekam';
+$lang['ClientMain.!info.invoice_past_due_button'] = 'Eud tsap yap';
+$lang['ClientMain.!info.invoice_due_text'] = 'Ecneinevnoc tseilrae ruoy ta tnemyap a ekam esaelp .%1$s fo ecnalab gnidnatstuo na sah tnuocca ruoy';
+$lang['ClientMain.!info.invoice_due_past_due_text'] = 'Ecneinevnoc tseilrae ruoy ta tnemyap a ekam esaelp .eud tsap si %2$s hcihw fo ,%1$s fo ecnalab gnidnatstuo na sah tnuocca ruoy';
+$lang['ClientMain.!info.invoice_due_other_currencies'] = 'Llew sa seicnerruc rehto ni ecnalab gnidnatstuo na evah uoy';
+$lang['ClientMain.!info.service_in_review'] = 'Detavitca eb lliw yeht ,devorppa ecno .ffats yb weiver gnidnep era secivres gniwollof eht';
+$lang['ClientMain.!info.service_name'] = '%2$s - %1$s';
+$lang['ClientMain.!info.additional_service'] = 'Ecivres lanoitidda 1 dna';
+$lang['ClientMain.!info.additional_services'] = 'Secivres lanoitidda %1$s dna';
+$lang['ClientMain.!info.email_pending_verification'] = '%1$s ot tnes neeb sah liame noitacifirev a .deifirevnu yltnerruc si sserdda liame ruoy';
+$lang['ClientMain.!info.email_pending_verification_button'] = 'Liame noitacifirev dneser';
+$lang['ClientMain.!info.no_contacts'] = 'Stcatnoc lanoitidda on era ereht';
+$lang['ClientMain.!info.no_managed_accounts'] = 'Eganam nac uoy taht stnuocca on era ereht';
+$lang['ClientMain.!tooltip.invoice_method_email'] = 'Elif no evah ew sserdda eht ot deliame eb lliw seciovni';
+$lang['ClientMain.!tooltip.invoice_method_paper'] = 'Elif no evah ew sserdda eht ot seciovni liam dna tnirp lliw ew';
+$lang['ClientMain.!tooltip.invoice_method_interfax'] = 'Elif no rebmun xaf dilav a evah ew taht erusne esaelp ,noitpo siht gnitceles fi .elif no evah ew rebmun xaf eht ot seciovni xaf yllacitamotua lliw ew';
+$lang['ClientMain.!tooltip.invoice_method_postalmethods'] = 'Elif no evah ew sserdda eht ot seciovni liam dna tnirp yllacitamotua lliw ew';
+$lang['ClientMain.index.page_title'] = '%1$s# tneilc';
+$lang['ClientMain.myinfo.page_title'] = '%1$s# tneilc';
+$lang['ClientMain.myinfo.client_id'] = '%1$s# tneilc';
+$lang['ClientMain.myinfo.setting_invoices'] = '%1$s yb seciovni gniviecer yltnerruc era uoy';
+$lang['ClientMain.myinfo.setting_autodebit_disabled'] = 'Tnemyap citamotua rof pu tes ton si tnuocca ruoy';
+$lang['ClientMain.myinfo.setting_autodebit_enabled'] = 'Tnemyap citamotua rof pu tes si tnuocca ruoy';
+$lang['ClientMain.myinfo.setting_autodebit_cc_zero_days'] = 'Eud si tnemyap yad eht no %2$sx ni gnidne %1$s ruoy egrahc ll\'ew';
+$lang['ClientMain.myinfo.setting_autodebit_ach_zero_days'] = 'Eud si tnemyap yad eht no %2$sx ni gnidne tnuocca %1$s ruoy egrahc ll\'ew';
+$lang['ClientMain.myinfo.setting_autodebit_cc_one_day'] = 'Eud si tnemyap erofeb yad eht %2$sx ni gnidne %1$s ruoy egrahc ll\'ew';
+$lang['ClientMain.myinfo.setting_autodebit_ach_one_day'] = 'Eud si tnemyap erofeb yad eht %2$sx ni gnidne tnuocca %1$s ruoy egrahc ll\'ew';
+$lang['ClientMain.myinfo.setting_autodebit_cc_days'] = 'Eud si tnemyap erofeb syad %3$s %2$sx ni gnidne %1$s ruoy egrahc ll\'ew';
+$lang['ClientMain.myinfo.setting_autodebit_ach_days'] = 'Eud si tnemyap erofeb syad %3$s %2$sx ni gnidne tnuocca %1$s ruoy egrahc ll\'ew';
+$lang['ClientMain.myinfo.number'] = ')%2$s %1$s(';
+$lang['ClientMain.myinfo.contacts'] = 'Stcatnoc';
+$lang['ClientMain.myinfo.link_addcontact'] = 'Tcatnoc dda';
+$lang['ClientMain.myinfo.managed_accounts'] = 'Stnuocca deganam';
+$lang['ClientMain.myinfo.field_managed_accounts_search'] = ')eman ro ,liame ,ynapmoc( hcraes';
+$lang['ClientMain.myinfo.setting_change'] = '?siht egnahc';
+$lang['ClientMain.myinfo.link_editclient'] = 'Egnahc';
+$lang['ClientMain.edit.heading_contact'] = 'Noitamrofni tcatnoc';
+$lang['ClientMain.edit.page_title'] = 'Noitamrofni ym tide %1$s# tneilc';
+$lang['ClientMain.edit.boxtitle_edit'] = 'Noitamrofni ym tide';
+$lang['ClientMain.edit.heading_billing'] = 'Noitamrofni gnillib';
+$lang['ClientMain.edit.field_taxid'] = 'Nitav/di xat';
+$lang['ClientMain.edit.field_default_currency'] = 'Ycnerruc derreferp';
+$lang['ClientMain.edit.field_invoiceaddress'] = 'Ot seciovni sserdda';
+$lang['ClientMain.edit.heading_settings'] = 'Sgnittes lanoitidda';
+$lang['ClientMain.edit.field_language'] = 'Egaugnal';
+$lang['ClientMain.edit.field_receive_email_marketing'] = 'Sreffo dna ,secivres ,stcudorp wen tuoba sliame eviecer ot tnaw i';
+$lang['ClientMain.edit.heading_authentication'] = 'Noitacitnehtua';
+$lang['ClientMain.edit.field_username'] = 'Emanresu';
+$lang['ClientMain.edit.field_recovery_email'] = ')lanoitpo( liame yrevocer';
+$lang['ClientMain.edit.field_new_password'] = 'Drowssap wen';
+$lang['ClientMain.edit.field_confirm_password'] = 'Drowssap mrifnoc';
+$lang['ClientMain.edit.field_current_password'] = 'Drowssap tnerruc';
+$lang['ClientMain.edit.field_two_factor_mode'] = 'Noitacitnehtua rotcaf-owt elbane';
+$lang['ClientMain.edit.two_factor_desc'] = 'Drowssap emit-eno a retne ot detpmorp eb neht dna drowssap dna emanresu ruoy htiw tnuocca ruoy otni gol ll\'uoy ,delbane fi .tnuocca ruoy otni gol ot noitacifitnedi fo snaem owt edivorp uoy hcihw ni erutaef ytiruces a si noitacitnehtua rotcaf-owt';
+$lang['ClientMain.edit.two_factor_instructions'] = 'Drowssap emit-eno yrassecen eht edivorp dna nekot ruoy sa tca nac enohp ruoy ,ecived elibom swodniw ro ,soi ,diordna na evah uoy fi .nekot ptot a deen uoy ,noitacitnehtua rotcaf-owt esu ot redro ni';
+$lang['ClientMain.edit.two_factor_step1'] = 'Ecived ruoy rof rotacitnehtua na daolnwod';
+$lang['ClientMain.edit.two_factor_step2'] = '%2$s - 23esab>/rb<%1$s - xeh>/rb<:syek gniwollof eht fo eno retne ,ylevitanretla ro ,woleb edoc rq eht nacs dna ,ppa eht nepo';
+$lang['ClientMain.edit.two_factor_step3'] = 'Ni gol uoy emit txen eht drowssap emit-eno ruoy retne ot deriuqer eb ll\'uoy ."noitamrofni ym etadpu" kcilc neht ,drowssap emit-eno ruoy mrifnoc';
+$lang['ClientMain.edit.text_android'] = 'Diordna';
+$lang['ClientMain.edit.text_ios'] = 'Soi';
+$lang['ClientMain.edit.text_windows'] = 'Swodniw';
+$lang['ClientMain.edit.field_otp'] = 'Drowssap emit-eno';
+$lang['ClientMain.edit.field_editsubmit'] = 'Noitamrofni ym etadpu';
+$lang['ClientMain.invoicemethod.page_title'] = 'Dohtem eciovni %1$s# tneilc';
+$lang['ClientMain.invoicemethod.boxtitle_inv_method'] = 'Dohtem eciovni';
+$lang['ClientMain.invoicemethod.text_invoice_method'] = 'Uoy ot seciovni wen reviled ew taht dohtem eht si dohtem eciovni ruoy';
+$lang['Clientmain.invoicemethod.field_methodsubmit'] = 'Etadpu';
+$lang['ClientMain.getcurrencyamounts.text_total_credits'] = 'Stiderc';
+
