5eb0c2a3bf6b8968feb78224e77947efcd84f6e3
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:50 -0800

    Updated es_es language files

---
+++ language/es_es/messages.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Messages
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Messages.!error.message_group_id.valid'] = 'Di puorg egassem dilavni';
+$lang['Messages.!error.company_id.valid'] = 'Di ynapmoc dilavni';
+$lang['Messages.!error.type.valid'] = '"sms" ot tes eb tsum epyt egassem eht';
+$lang['Messages.!error.status.valid'] = '"evitcani" ro "evitca" ot tes eb tsum sutats egassem eht';
+$lang['Messages.!error.content.format'] = 'Yarra na eb tsum tnetnoc egassem eht';
+$lang['Messages.getTypes.sms'] = 'Sms';
+$lang['Messages.getStatusTypes.active'] = 'Evitca';
+$lang['Messages.getStatusTypes.inactive'] = 'Evitcani';
+
