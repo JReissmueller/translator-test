ebd8881424e3b2bd2413ab8c976a1a8d80d79657
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:55 -0800

    Updated es_es language files

---
+++ language/es_es/admin_main.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!error.delete_event.staff_id'] = 'Tneve siht eteled yam rotaerc s\'tneve eht ylno';
+$lang['AdminMain.!error.event_editable'] = 'Tneve taht eteled yam rotaerc s\'tneve eht ylno';
+$lang['AdminMain.!success.event_added'] = '!dedda yllufsseccus neeb sah tneve eht';
+$lang['AdminMain.!success.event_edited'] = '!detadpu yllufsseccus neeb sah tneve eht';
+$lang['AdminMain.!success.event_deleted'] = '!deteled yllufsseccus neeb sah tneve eht';
+$lang['AdminMain.index.page_title'] = 'Draobhsad';
+$lang['AdminMain.index.heading_quicklinks'] = 'Sknil kciuq';
+$lang['AdminMain.index.no_quicklinks'] = 'Tes neeb evah sknil kciuq on';
+$lang['AdminMain.index.manage_widgets'] = 'Stegdiw eganam';
+$lang['AdminMain.index.customize_dashboard'] = 'Draobhsad ezimotsuc';
+$lang['AdminMain.managewidgets.text_widgets'] = 'Meht evomer ot thgir ot tfel morf ro ,draobhsad ruoy ot meht dda ot tfel eht ot thgir eht morf stegdiw gard';
+$lang['AdminMain.managewidgets.text_version'] = '%1$s rev';
+$lang['AdminMain.managewidgets.text_author'] = ':rohtua';
+$lang['AdminMain.updatedashboard.text_layout'] = 'Draobhsad ruoy ezimotsuc ot woleb tuoyal a tceles';
+$lang['AdminMain.calendar.page_title'] = 'Radnelac';
+$lang['AdminMain.calendar.boxtitle_calendar'] = 'Radnelac';
+$lang['AdminMain.calendar.categorylink_addevent'] = 'Tneve etaerc';
+$lang['AdminMain.calendar.category_month'] = 'Htnom';
+$lang['AdminMain.calendar.category_week'] = 'Keew';
+$lang['AdminMain.calendar.category_day'] = 'Yad';
+$lang['AdminMain.getEvents.shared_event_title'] = '%1$s :%3$s %2$s';
+$lang['AdminMain.addevent.boxtitle_addevent'] = 'Tneve etaerc';
+$lang['AdminMain.addevent.field_title'] = 'Eltit';
+$lang['AdminMain.addevent.field_start_date'] = 'Etad trats';
+$lang['AdminMain.addevent.field_end_date'] = 'Etad dne';
+$lang['AdminMain.addevent.field_shared'] = 'Ffats rehto yb elbaweiv tneve siht ekam';
+$lang['AdminMain.addevent.field_all_day'] = 'Yad lla';
+$lang['AdminMain.addevent.field_addeventsubmit'] = 'Tneve etaerc';
+$lang['AdminMain.editevent.boxtitle_editevent'] = 'Tneve tide';
+$lang['AdminMain.editevent.field_title'] = 'Eltit';
+$lang['AdminMain.editevent.field_start_date'] = 'Etad trats';
+$lang['AdminMain.editevent.field_end_date'] = 'Etad dne';
+$lang['AdminMain.editevent.field_shared'] = 'Ffats rehto yb elbaweiv tneve siht ekam';
+$lang['AdminMain.editevent.field_all_day'] = 'Yad lla';
+$lang['AdminMain.editevent.field_editeventsubmit'] = 'Tneve etadpu';
+$lang['AdminMain.editevent.field_deleteeventsubmit'] = 'Tneve eteled';
+$lang['AdminMain.editevent.confirm_delete'] = '?tneve siht eteled yllaer';
+
