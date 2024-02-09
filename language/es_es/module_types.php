af9aab952ef5d9075ff1b16ef76c22af5fd84549
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:53 -0800

    Updated es_es language files

---
+++ language/es_es/module_types.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Module Types
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ModuleTypes.!error.name.valid'] = 'Dilavni si eman';
+$lang['ModuleTypes.!error.type_id.valid'] = 'Dilavni si di epyt eludom eht';
+
