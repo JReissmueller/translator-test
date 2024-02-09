3be8270a4124a9f72270f9de9da2ab6687e3a259
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:30 -0800

    Updated es_es language files

---
+++ plugins/domains/language/es_es/admin_company_emails.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_1_name'] = '1 rednimer noitaripxe niamod';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_1_desc'] = 'Eripxe noos lliw niamod rieht nehw tneilc a ot tnes eciton tsrif';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_2_name'] = '2 rednimer noitaripxe niamod';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_2_desc'] = 'Eripxe noos lliw niamod rieht nehw tneilc a ot tnes eciton dnoces';
+$lang['AdminCompanyEmails.templates.Domains.domain_expiration_name'] = 'Eciton noitaripxe niamod';
+$lang['AdminCompanyEmails.templates.Domains.domain_expiration_desc'] = 'Deripxe sah niamod rieht retfa noos tneilc a ot tnes eciton';
+
