aa0629480e35b5f5bf523c5ca307647d6475a641
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:30 -0800

    Updated es_es language files

---
+++ language/es_es/contacts.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Contacts
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Contacts.!error.client_id.exists'] = 'Di tneilc dilavni';
+$lang['Contacts.!error.user_id.exists'] = 'Di resu dilavni';
+$lang['Contacts.!error.contact_type.format'] = 'Epyt tcatnoc dilavni';
+$lang['Contacts.!error.contact_type.inv_address_to'] = 'Tcatnoc siht gnitadpu erofeb degnahc eb tsum dna tcatnoc siht ot desserdda eb ot tes era seciovni';
+$lang['Contacts.!error.contact_type_id.format'] = 'Di epyt tcatnoc dilavni';
+$lang['Contacts.!error.first_name.empty'] = 'Eman tsrif a retne esaelp';
+$lang['Contacts.!error.last_name.empty'] = 'Eman tsal a retne esaelp';
+$lang['Contacts.!error.title.length'] = 'Sretcarahc 46 deecxe ton yam htgnel eltit';
+$lang['Contacts.!error.title.empty'] = 'Eltit a retne esaelp';
+$lang['Contacts.!error.company.length'] = 'Sretcarahc 821 deecxe ton yam htgnel ynapmoc';
+$lang['Contacts.!error.company.empty'] = 'Ynapmoc a retne esaelp';
+$lang['Contacts.!error.email.format'] = 'Sserdda liame dilavni';
+$lang['Contacts.!error.email.unique'] = 'Nekat ydaerla si sserdda liame';
+$lang['Contacts.!error.city.empty'] = 'Ytic a retne esaelp';
+$lang['Contacts.!error.state.length'] = 'Sretcarahc 3 deecxe ton yam htgnel etats';
+$lang['Contacts.!error.state.country_exists'] = 'Etats detceles eht sehctam taht yrtnuoc eht tceles esaelp';
+$lang['Contacts.!error.state.empty'] = 'Ecnivorp/etats a tceles esaelp';
+$lang['Contacts.!error.country.length'] = 'Sretcarahc 3 deecxe ton yam htgnel yrtnuoc';
+$lang['Contacts.!error.country.empty'] = 'Yrtnuoc a tceles esaelp';
+$lang['Contacts.!error.contact_id.exists'] = 'Di tcatnoc dilavni';
+$lang['Contacts.!error.contact_id.primary'] = 'Deteled eb ton yam tcatnoc tneilc yramirp eht';
+$lang['Contacts.!error.address1.empty'] = 'Sserdda ruoy fo enil tsrif eht retne esaelp';
+$lang['Contacts.!error.address2.empty'] = 'Sserdda ruoy fo enil dnoces eht retne esaelp';
+$lang['Contacts.!error.zip.empty'] = 'Edoc latsop/piz a retne esaelp';
+$lang['Contacts.!error.phone.required'] = 'Rebmun enohp eno tsael ta retne esaelp';
+$lang['Contacts.!error.fax.required'] = 'Rebmun xaf eno tsael ta retne esaelp';
+$lang['Contacts.!error.first_name.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si eman tsrif';
+$lang['Contacts.!error.last_name.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si eman tsal';
+$lang['Contacts.!error.title.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si eltit';
+$lang['Contacts.!error.company.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si ynapmoc';
+$lang['Contacts.!error.email.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si sserdda';
+$lang['Contacts.!error.city.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si ytic';
+$lang['Contacts.!error.state.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si ecnivorp/etats';
+$lang['Contacts.!error.country.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si yrtnuoc';
+$lang['Contacts.!error.address1.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si enil sserdda tsrif eht';
+$lang['Contacts.!error.address2.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si enil sserdda dnoces eht';
+$lang['Contacts.!error.zip.read_only'] = 'Egnahc ot su tcatnoc ,detadpu eb tonnac dna dleif ylno-daer a si edoc latsop/piz';
+$lang['Contacts.!error.number.empty'] = 'Rebmun tcatnoc a retne esaelp';
+$lang['Contacts.!error.type.format'] = 'Epyt dilavni';
+$lang['Contacts.!error.location.format'] = 'Noitacol dilavni';
+$lang['Contacts.!error.id.exists'] = 'Di rebmun tcatnoc dilavni';
+$lang['Contacts.!error.company_id.format'] = 'Di ynapmoc dilavni';
+$lang['Contacts.!error.name.empty'] = 'Eman a retne esaelp';
+$lang['Contacts.!error.is_lang.format'] = 'Rebmun a eb tsum gnal_si';
+$lang['Contacts.!error.is_lang.length'] = 'Retcarahc 1 deecxe ton yam htgnel gnal_si';
+$lang['Contacts.!error.contact_type_id.exists'] = 'Di epyt tcatnoc dilavni';
+$lang['Contacts.getcontacttypes.primary'] = 'Yramirp';
+$lang['Contacts.getcontacttypes.billing'] = 'Gnillib';
+$lang['Contacts.getcontacttypes.other'] = 'Rehto';
+$lang['Contacts.getnumbertypes.phone'] = 'Enohp';
+$lang['Contacts.getnumbertypes.fax'] = 'Xaf';
+$lang['Contacts.getnumberlocations.home'] = 'Emoh';
+$lang['Contacts.getnumberlocations.work'] = 'Krow';
+$lang['Contacts.getnumberlocations.mobile'] = 'Elibom';
+$lang['Contacts.getPermissionOptions.client_invoices'] = 'Seciovni';
+$lang['Contacts.getPermissionOptions.client_quotations'] = 'Snoitatouq';
+$lang['Contacts.getPermissionOptions.client_services'] = 'Secivres';
+$lang['Contacts.getPermissionOptions.client_transactions'] = 'Snoitcasnart';
+$lang['Contacts.getPermissionOptions.client_contacts'] = 'Stcatnoc';
+$lang['Contacts.getPermissionOptions.client_accounts'] = 'Stnuocca tnemyap';
+$lang['Contacts.getPermissionOptions.client_emails'] = 'Yrotsih liame';
+$lang['Contacts.getPermissionOptions._managed'] = 'Stnuocca deganam';
+$lang['Contacts.getPermissionOptions._invoice_delivery'] = 'Yreviled eciovni';
+$lang['Contacts.getPermissionOptions._credits'] = 'Stiderc';
+
