cd6e08c660e0d2eced34dec1060a08b3e651ac55
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:19 -0800

    Updated es_es language files

---
+++ components/modules/virtualmin/language/es_es/virtualmin.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Virtualmin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Virtualmin.name'] = 'Nimlautriv';
+$lang['Virtualmin.description'] = 'Nimbew no desab si ti .snoisrev laicremmoc dna ecruos nepo htob ni elbaliava si dna sniamod ynam eganam dna etaerc ot ytiliba eht sevig hcihw ,lenap lortnoc etisbew dna gnitsoh niamod a si nimlautriv';
+$lang['Virtualmin.module_row'] = 'Revres';
+$lang['Virtualmin.module_row_plural'] = 'Srevres';
+$lang['Virtualmin.module_group'] = 'Puorg revres';
+$lang['Virtualmin.tab_stats'] = 'Scitsitats';
+$lang['Virtualmin.tab_client_actions'] = 'Snoitca';
+$lang['Virtualmin.add_module_row'] = 'Revres dda';
+$lang['Virtualmin.add_module_group'] = 'Puorg revres dda';
+$lang['Virtualmin.manage.module_rows_title'] = 'Srevres';
+$lang['Virtualmin.manage.module_groups_title'] = 'Spuorg revres';
+$lang['Virtualmin.manage.module_rows_heading.name'] = 'Lebal revres';
+$lang['Virtualmin.manage.module_rows_heading.hostname'] = 'Emantsoh';
+$lang['Virtualmin.manage.module_rows_heading.port'] = 'Trop';
+$lang['Virtualmin.manage.module_rows_heading.accounts'] = 'Stnuocca';
+$lang['Virtualmin.manage.module_rows_heading.options'] = 'Snoitpo';
+$lang['Virtualmin.manage.module_groups_heading.name'] = 'Eman puorg';
+$lang['Virtualmin.manage.module_groups_heading.servers'] = 'Tnuoc revres';
+$lang['Virtualmin.manage.module_groups_heading.options'] = 'Snoitpo';
+$lang['Virtualmin.manage.module_rows.count'] = '%2$s / %1$s';
+$lang['Virtualmin.manage.module_rows.edit'] = 'Tide';
+$lang['Virtualmin.manage.module_groups.edit'] = 'Tide';
+$lang['Virtualmin.manage.module_rows.delete'] = 'Eteled';
+$lang['Virtualmin.manage.module_groups.delete'] = 'Eteled';
+$lang['Virtualmin.manage.module_rows.confirm_delete'] = '?revres siht eteled ot tnaw uoy erus uoy era';
+$lang['Virtualmin.manage.module_groups.confirm_delete'] = '?puorg revres siht eteled ot tnaw uoy erus uoy era';
+$lang['Virtualmin.manage.module_rows_no_results'] = 'Srevres on era ereht';
+$lang['Virtualmin.manage.module_groups_no_results'] = 'Spuorg revres on era ereht';
+$lang['Virtualmin.order_options.first'] = 'Revres lluf-non tsrif';
+$lang['Virtualmin.add_row.box_title'] = 'Revres nimlautriv dda';
+$lang['Virtualmin.add_row.basic_title'] = 'Sgnittes cisab';
+$lang['Virtualmin.add_row.add_btn'] = 'Revres dda';
+$lang['Virtualmin.edit_row.box_title'] = 'Revres nimlautriv tide';
+$lang['Virtualmin.edit_row.basic_title'] = 'Sgnittes cisab';
+$lang['Virtualmin.edit_row.add_btn'] = 'Revres tide';
+$lang['Virtualmin.row_meta.server_name'] = 'Lebal revres';
+$lang['Virtualmin.row_meta.host_name'] = 'Emantsoh';
+$lang['Virtualmin.row_meta.user_name'] = 'Eman resu';
+$lang['Virtualmin.row_meta.password'] = 'Drowssap';
+$lang['Virtualmin.row_meta.port'] = 'Trop';
+$lang['Virtualmin.row_meta.use_ssl'] = ')dednemmocer( ipa eht ot gnitcennoc nehw lss esu';
+$lang['Virtualmin.row_meta.account_limit'] = 'Timil tnuocca';
+$lang['Virtualmin.package_fields.plan'] = 'Nalp nimlautriv';
+$lang['Virtualmin.package_fields.template'] = 'Etalpmet nimlautriv';
+$lang['Virtualmin.service_field.domain'] = 'Niamod';
+$lang['Virtualmin.service_field.username'] = 'Emanresu';
+$lang['Virtualmin.service_field.password'] = 'Drowssap';
+$lang['Virtualmin.service_field.confirm_password'] = 'Drowssap mrifnoc';
+$lang['Virtualmin.tab_client_actions.change_password'] = 'Drowssap egnahc';
+$lang['Virtualmin.tab_client_actions.field_virtualmin_password'] = 'Drowssap';
+$lang['Virtualmin.tab_client_actions.field_virtualmin_confirm_password'] = 'Drowssap mrifnoc';
+$lang['Virtualmin.tab_client_actions.field_password_submit'] = 'Drowssap etadpu';
+$lang['Virtualmin.tab_stats.info_title'] = 'Noitamrofni';
+$lang['Virtualmin.tab_stats.info_heading.field'] = 'Dleif';
+$lang['Virtualmin.tab_stats.info_heading.value'] = 'Eulav';
+$lang['Virtualmin.tab_stats.info.disk_used'] = 'Desu ksid';
+$lang['Virtualmin.tab_stats.info.databases_size'] = 'Ezis esabatad';
+$lang['Virtualmin.tab_stats.info.databases_count'] = 'Tnuoc esabatad';
+$lang['Virtualmin.tab_stats.info.maximum_databases'] = 'Sesabatad mumixam';
+$lang['Virtualmin.tab_stats.info.maximum_mailboxes'] = 'Sexobliam mumixam';
+$lang['Virtualmin.service_info.username'] = 'Emanresu';
+$lang['Virtualmin.service_info.password'] = 'Drowssap';
+$lang['Virtualmin.service_info.server'] = 'Revres';
+$lang['Virtualmin.service_info.options'] = 'Snoitpo';
+$lang['Virtualmin.service_info.option_login'] = 'Ni gol';
+$lang['Virtualmin.service_field.tooltip.username'] = 'Eno etareneg yllacitamotua ot knalb emanresu eht evael yam uoy';
+$lang['Virtualmin.service_field.tooltip.password'] = 'Eno etareneg yllacitamotua ot knalb drowssap eht evael yam uoy';
+$lang['Virtualmin.!error.server_name_valid'] = 'Lebal revres a retne tsum uoy';
+$lang['Virtualmin.!error.host_name_valid'] = 'Dilavni eb ot sraeppa emantsoh eht';
+$lang['Virtualmin.!error.user_name_valid'] = 'Dilavni eb ot sraeppa eman resu eht';
+$lang['Virtualmin.!error.port_valid'] = 'Dilavni eb ot sraeppa trop eht';
+$lang['Virtualmin.!error.password_valid'] = 'Dilavni eb ot sraeppa drowssap eht';
+$lang['Virtualmin.!error.password_valid_connection'] = 'Tcerroc era trop dna ,drowssap ,eman resu ,emantsoh eht taht erusne ot kcehc esaelp .dehsilbatse eb ton dluoc revres eht ot noitcennoc a';
+$lang['Virtualmin.!error.account_limit_valid'] = 'Timil tnuocca dilav a retne esaelp';
+$lang['Virtualmin.!error.meta[plan].empty'] = 'Deriuqer si nalp nimlautriv a';
+$lang['Virtualmin.!error.meta[template].empty'] = 'Deriuqer si etalpmet nimlautriv a';
+$lang['Virtualmin.!error.api.internal'] = 'Tseuqer eht ot dnopser ton did revres eht ro ,derrucco rorre lanretni na';
+$lang['Virtualmin.!error.module_row.missing'] = 'Elbaliavanu si wor eludom eht .derrucco rorre lanretni na';
+$lang['Virtualmin.!error.virtualmin_domain.format'] = 'Moc.niamod .g.e ,eman niamod dilav a retne esaelp';
+$lang['Virtualmin.!error.virtualmin_domain.test'] = '\'tset\' htiw trats ton nac eman niamod';
+$lang['Virtualmin.!error.virtualmin_username.format'] = 'Rebmun a htiw trats ton yam dna srebmun dna srettel ylno niatnoc yam emanresu eht';
+$lang['Virtualmin.!error.virtualmin_username.test'] = '\'tset\' htiw nigeb ton yam emanresu eht';
+$lang['Virtualmin.!error.virtualmin_username.length'] = 'Htgnel ni sretcarahc 61 dna 1 neewteb eb tsum emanresu eht';
+$lang['Virtualmin.!error.virtualmin_password.valid'] = 'Htgnel ni sretcarahc 8 tsael ta eb tsum drowssap';
+
