f04d3080d13c8d52b7baad0b2ae6adf591f6bc29
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:45 -0800

    Updated es_es language files

---
+++ components/modules/apnscp/language/es_es/apnscp.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Apnscp
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Apnscp.name'] = 'Pcsipa';
+$lang['Apnscp.description'] = 'Pcsnpa dellac ylremrof';
+$lang['Apnscp.module_row.name'] = 'Revres';
+$lang['Apnscp.module_row_plural.name'] = 'Srevres';
+$lang['Apnscp.module_group.name'] = 'Puorg revres';
+$lang['Apnscp.tab_stats'] = 'Scitsitats';
+$lang['Apnscp.tab_client_stats'] = 'Scitsitats';
+$lang['Apnscp.tab_client_actions'] = 'Snoitca';
+$lang['Apnscp.add_module_row'] = 'Revres dda';
+$lang['Apnscp.add_module_group'] = 'Puorg revres dda';
+$lang['Apnscp.manage.module_rows_title'] = 'Srevres';
+$lang['Apnscp.manage.module_groups_title'] = 'Spuorg revres';
+$lang['Apnscp.manage.module_rows_heading.name'] = 'Lebal revres';
+$lang['Apnscp.manage.module_rows_heading.hostname'] = 'Emantsoh';
+$lang['Apnscp.manage.module_rows_heading.accounts'] = 'Stnuocca';
+$lang['Apnscp.manage.module_rows_heading.options'] = 'Snoitpo';
+$lang['Apnscp.manage.module_groups_heading.name'] = 'Eman puorg';
+$lang['Apnscp.manage.module_groups_heading.servers'] = 'Tnuoc revres';
+$lang['Apnscp.manage.module_groups_heading.options'] = 'Snoitpo';
+$lang['Apnscp.manage.module_rows.count'] = '%2$s / %1$s';
+$lang['Apnscp.manage.module_rows.edit'] = 'Tide';
+$lang['Apnscp.manage.module_groups.edit'] = 'Tide';
+$lang['Apnscp.manage.module_rows.delete'] = 'Eteled';
+$lang['Apnscp.manage.module_groups.delete'] = 'Eteled';
+$lang['Apnscp.manage.module_rows.confirm_delete'] = '?revres siht eteled ot tnaw uoy erus uoy era';
+$lang['Apnscp.manage.module_groups.confirm_delete'] = '?puorg revres siht eteled ot tnaw uoy erus uoy era';
+$lang['Apnscp.manage.module_rows_no_results'] = 'Srevres on era ereht';
+$lang['Apnscp.manage.module_groups_no_results'] = 'Spuorg revres on era ereht';
+$lang['Apnscp.order_options.first'] = 'Revres lluf-non tsrif';
+$lang['Apnscp.order_options.roundrobin'] = 'Srevres gnoma etubirtsid ylneve';
+$lang['Apnscp.add_row.box_title'] = 'Revres pcsipa dda';
+$lang['Apnscp.add_row.basic_title'] = 'Sgnittes cisab';
+$lang['Apnscp.add_row.name_servers_title'] = 'Srevres eman';
+$lang['Apnscp.add_row.notes_title'] = 'Seton';
+$lang['Apnscp.add_row.name_server_btn'] = 'Revres eman lanoitidda dda';
+$lang['Apnscp.add_row.name_server_col'] = 'Revres eman';
+$lang['Apnscp.add_row.name_server_host_col'] = 'Emantsoh';
+$lang['Apnscp.add_row.name_server'] = '%1$s revres eman';
+$lang['Apnscp.add_row.remove_name_server'] = 'Evomer';
+$lang['Apnscp.add_row.add_btn'] = 'Revres dda';
+$lang['Apnscp.edit_row.box_title'] = 'Revres pcsipa tide';
+$lang['Apnscp.edit_row.basic_title'] = 'Sgnittes cisab';
+$lang['Apnscp.edit_row.name_servers_title'] = 'Srevres eman';
+$lang['Apnscp.edit_row.notes_title'] = 'Seton';
+$lang['Apnscp.edit_row.name_server_btn'] = 'Revres eman lanoitidda dda';
+$lang['Apnscp.edit_row.name_server_col'] = 'Revres eman';
+$lang['Apnscp.edit_row.name_server_host_col'] = 'Emantsoh';
+$lang['Apnscp.edit_row.name_server'] = '%1$s revres eman';
+$lang['Apnscp.edit_row.remove_name_server'] = 'Evomer';
+$lang['Apnscp.edit_row.add_btn'] = 'Revres tide';
+$lang['Apnscp.row_meta.server_name'] = 'Lebal revres';
+$lang['Apnscp.row_meta.host_name'] = 'Emantsoh';
+$lang['Apnscp.row_meta.port'] = 'Trop';
+$lang['Apnscp.row_meta.default_port'] = '3802';
+$lang['Apnscp.row_meta.api_key'] = 'Yek ipa';
+$lang['Apnscp.row_meta.use_ssl'] = ')dednemmocer( ipa eht ot gnitcennoc nehw lss esu';
+$lang['Apnscp.row_meta.account_limit'] = 'Timil tnuocca';
+$lang['Apnscp.package_fields.package'] = 'Egakcap pcsipa';
+$lang['Apnscp.service_field.domain'] = 'Niamod';
+$lang['Apnscp.service_field.username'] = 'Emanresu';
+$lang['Apnscp.service_field.password'] = 'Drowssap';
+$lang['Apnscp.tab_stats.info_title'] = 'Noitamrofni';
+$lang['Apnscp.tab_stats.info_heading.field'] = 'Dleif';
+$lang['Apnscp.tab_stats.info_heading.value'] = 'Eulav';
+$lang['Apnscp.tab_stats.info.disk'] = 'Atouq ksid';
+$lang['Apnscp.tab_stats.info.bandwidth'] = 'Atouq htdiwdnab';
+$lang['Apnscp.tab_client_actions.change_password'] = 'Drowssap egnahc';
+$lang['Apnscp.tab_client_actions.field_apnscp_password'] = 'Drowssap';
+$lang['Apnscp.tab_client_actions.field_password_submit'] = 'Drowssap etadpu';
+$lang['Apnscp.tab_client_stats.info_title'] = 'Noitamrofni';
+$lang['Apnscp.tab_client_stats.bandwidth_title'] = ')etad ot htnom( egasu htdiwdnab';
+$lang['Apnscp.tab_client_stats.disk_title'] = 'Egasu ksid';
+$lang['Apnscp.tab_client_stats.usage'] = ')bm %2$s/bm %1$s(';
+$lang['Apnscp.tab_client_stats.usage_unlimited'] = ')';
+$lang['Apnscp.service_info.username'] = 'Emanresu';
+$lang['Apnscp.service_info.password'] = 'Drowssap';
+$lang['Apnscp.service_info.server'] = 'Revres';
+$lang['Apnscp.service_info.options'] = 'Snoitpo';
+$lang['Apnscp.service_info.option_login'] = 'Ni gol';
+$lang['Apnscp.service_field.tooltip.username'] = 'Eno etareneg yllacitamotua ot knalb emanresu eht evael yam uoy';
+$lang['Apnscp.service_field.tooltip.password'] = 'Eno etareneg yllacitamotua ot knalb drowssap eht evael yam uoy';
+$lang['Apnscp.!error.server_name_valid'] = 'Lebal revres a retne tsum uoy';
+$lang['Apnscp.!error.host_name_valid'] = 'Dilavni eb ot sraeppa emantsoh eht';
+$lang['Apnscp.!error.port_format'] = 'Rebmun a eb tsum trop eht';
+$lang['Apnscp.!error.api_key_valid'] = 'Dilavni eb ot sraeppa yek ipa eht';
+$lang['Apnscp.!error.api_key_valid_connection'] = 'Tcerroc era trop dna ,yek ipa ,emantsoh eht taht erusne ot kcehc esaelp .dehsilbatse eb ton dluoc revres eht ot noitcennoc a';
+$lang['Apnscp.!error.account_limit_valid'] = 'Eulav regetni emos ot tes ro )stnuocca detimilnu rof( knalb tfel eb tsum timil tnuocca';
+$lang['Apnscp.!error.name_servers_valid'] = 'Dilavni era deretne srevres eman eht fo erom ro eno';
+$lang['Apnscp.!error.name_servers_count'] = 'Srevres eman 2 tsael ta enifed tsum uoy';
+$lang['Apnscp.!error.meta[package].empty'] = 'Deriuqer si egakcap pcsipa na';
+$lang['Apnscp.!error.api.internal'] = 'Tseuqer eht ot dnopser ton did revres eht ro ,derrucco rorre lanretni na';
+$lang['Apnscp.!error.module_row.missing'] = 'Elbaliavanu si wor eludom eht .derrucco rorre lanretni na';
+$lang['Apnscp.!error.apnscp_domain.format'] = 'Moc.niamod .g.e ,eman niamod dilav a retne esaelp';
+$lang['Apnscp.!error.apnscp_domain.test'] = '\'tset\' htiw trats ton nac eman niamod';
+$lang['Apnscp.!error.apnscp_username.format'] = 'Rebmun a htiw trats ton yam dna srebmun dna srettel ylno niatnoc yam emanresu eht';
+$lang['Apnscp.!error.apnscp_username.test'] = '\'tset\' htiw nigeb ton yam emanresu eht';
+$lang['Apnscp.!error.apnscp_username.length'] = 'Htgnel ni sretcarahc 61 dna 1 neewteb eb tsum emanresu eht';
+$lang['Apnscp.!error.apnscp_password.valid'] = 'Htgnel ni sretcarahc 8 tsael ta eb tsum drowssap';
+$lang['Apnscp.!error.apnscp_password.matches'] = 'Hctam ton od drowssap mrifnoc dna drowssap';
+
