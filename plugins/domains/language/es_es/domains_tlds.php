740ca7e1528de8f53efdd274cd21de00b1dee09d
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:37 -0800

    Updated es_es language files

---
+++ plugins/domains/language/es_es/domains_tlds.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Domains Tlds
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsTlds.!error.tld.empty'] = 'Dlt a retne esaelp';
+$lang['DomainsTlds.!error.tld.exists'] = 'Ynapmoc eht ni stsixe ydaerla dlt eht';
+$lang['DomainsTlds.!error.tld.not_exists'] = 'Ynapmoc eht ni tsixe ton seod dlt eht';
+$lang['DomainsTlds.!error.tld.length'] = 'Gnol sretcarahc 3 tsael ta eb tsum dlt eht';
+$lang['DomainsTlds.!error.tld_id.exists'] = 'Di dlt dilavni';
+$lang['DomainsTlds.!error.package_id.exists'] = 'Di egakcap dilavni';
+$lang['DomainsTlds.!error.package_group_id.exists'] = 'Di puorg egakcap dilavni';
+$lang['DomainsTlds.!error.module_id.exists'] = 'Di eludom dilavni';
+$lang['DomainsTlds.!error.commpany_id.exists'] = 'Di ynapmoc dilavni';
+$lang['DomainsTlds.!error.dns_management.valid'] = '1 ro 0 eb tsum ,tnemeganam snd rof eulav dilavni';
+$lang['DomainsTlds.!error.email_forwarding.valid'] = '1 ro 0 eb tsum ,gnidrawrof liame rof eulav dilavni';
+$lang['DomainsTlds.!error.id_protection.valid'] = '1 ro 0 eb tsum ,noitcetorp di rof eulav dilavni';
+$lang['DomainsTlds.!error.epp_code.valid'] = '1 ro 0 eb tsum ,edoc ppe rof eulav dilavni';
+$lang['DomainsTlds.!error.package_pricing.count'] = 'Gnicirp eno tsael ta evah tsum dlt eht ,delbasid eb tonnac gnicirp eht';
+$lang['DomainsTlds.!error.package_pricing.service'] = 'Ti gnisu yltnerruc si ecivres a esuaceb delbasid eb ton dluoc gnicirp eht';
+$lang['DomainsTlds.!error.tld.supported'] = 'Eludom rartsiger detceles eht yb detroppusnu si dlt dedivorp eht';
+$lang['DomainsTlds.!error.feature.unsupported'] = 'Eludom rartsiger detceles eht yb detroppusnu si erutaef %1$s eht';
+$lang['DomainsTlds.getTldFields.group'] = 'Puorg';
+
