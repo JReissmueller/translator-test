3721fad5b3480c350a42fbd9b57f35db52f0bbb0
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:05:59 -0800

    Updated es_es language files

--- @es_es/language/es_es/actions.php
+++ @es_es/language/es_es/actions.php
@@ -9,28 +9,28 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Actions.!error.location.valid'] = 'Ubicación válida no válida.';
-$lang['Actions.!error.location.unique'] = 'La ubicación y la URL deben ser únicas para esta empresa.';
-$lang['Actions.!error.url.empty'] = 'Por favor, introduzca una URL.';
-$lang['Actions.!error.name.action_empty'] = 'Introduzca un nombre para la acción.';
-$lang['Actions.!error.plugin_id.valid'] = 'Proporcione un ID de plugin válido.';
-$lang['Actions.!error.company_id.valid'] = 'ID de empresa no válida';
-$lang['Actions.!error.editable.valid'] = 'La opción editable debe ser 1 o 0.';
-$lang['Actions.!error.enabled.valid'] = 'La opción activado debe ser 1 o 0.';
-$lang['Actions.getLocations.nav_staff'] = 'Menú de navegación del personal';
-$lang['Actions.getLocations.nav_client'] = 'Menú de navegación del cliente';
-$lang['Actions.getLocations.nav_public'] = 'Menú de navegación pública';
-$lang['Actions.getLocations.widget_client_home'] = 'Widget del panel de control del cliente';
-$lang['Actions.getLocations.widget_staff_home'] = 'Widget del cuadro de mandos del personal';
-$lang['Actions.getLocations.widget_staff_client'] = 'Widget de perfil de cliente';
-$lang['Actions.getLocations.widget_staff_billing'] = 'Widget de facturación del personal';
-$lang['Actions.getLocations.action_staff_client'] = 'Enlace "Acciones de la cuenta" del cliente';
-$lang['Actions.getLocationDescriptions.nav_staff'] = 'Pone un enlace a disposición de la navegación de la interfaz de administración.';
-$lang['Actions.getLocationDescriptions.nav_client'] = 'Pone un enlace a disposición de la navegación de la interfaz cliente.';
-$lang['Actions.getLocationDescriptions.nav_public'] = 'Pone un enlace a disposición de la navegación de la interfaz pública.';
-$lang['Actions.getLocationDescriptions.widget_client_home'] = '	Muestra un widget en el panel de control del cliente.';
-$lang['Actions.getLocationDescriptions.widget_staff_home'] = 'Muestra un widget en el panel de control del personal.';
-$lang['Actions.getLocationDescriptions.widget_staff_client'] = 'Muestra un widget en la página de perfil del cliente.';
-$lang['Actions.getLocationDescriptions.widget_staff_billing'] = 'Muestra un widget en la página de facturación del personal.';
-$lang['Actions.getLocationDescriptions.action_staff_client'] = 'Crea un enlace en la sección "Acciones de la cuenta" de la página de perfil del cliente del personal.';
+$lang['Actions.!error.location.valid'] = 'Noitacol dilav dilavni';
+$lang['Actions.!error.location.unique'] = 'Ynapmoc siht rof euqinu eb tsum lru dna noitacol eht';
+$lang['Actions.!error.url.empty'] = 'Lru a retne esaelp';
+$lang['Actions.!error.name.action_empty'] = 'Eman noitca na retne esaelp';
+$lang['Actions.!error.plugin_id.valid'] = 'Di nigulp dilav a edivorp esaelp';
+$lang['Actions.!error.company_id.valid'] = 'Di ynapmoc dilavni';
+$lang['Actions.!error.editable.valid'] = '0 ro 1 ot tes eb tsum noitpo elbatide eht';
+$lang['Actions.!error.enabled.valid'] = '0 ro 1 ot tes eb tsum noitpo delbane eht';
+$lang['Actions.getLocations.nav_staff'] = 'Unem noitagivan ffats';
+$lang['Actions.getLocations.nav_client'] = 'Unem noitagivan tneilc';
+$lang['Actions.getLocations.nav_public'] = 'Unem noitagivan cilbup';
+$lang['Actions.getLocations.widget_client_home'] = 'Tegdiw draobhsad tneilc';
+$lang['Actions.getLocations.widget_staff_home'] = 'Tegdiw draobhsad ffats';
+$lang['Actions.getLocations.widget_staff_client'] = 'Tegdiw eliforp tneilc ffats';
+$lang['Actions.getLocations.widget_staff_billing'] = 'Tegdiw gnillib ffats';
+$lang['Actions.getLocations.action_staff_client'] = 'Knil "snoitca tnuocca" tneilc';
+$lang['Actions.getLocationDescriptions.nav_staff'] = 'Ecafretni nimda eht fo noitagivan eht ot elbaliava knil a sekam';
+$lang['Actions.getLocationDescriptions.nav_client'] = 'Ecafretni tneilc eht fo noitagivan eht ot elbaliava knil a sekam';
+$lang['Actions.getLocationDescriptions.nav_public'] = 'Ecafretni cilbup eht fo noitagivan eht ot elbaliava knil a sekam';
+$lang['Actions.getLocationDescriptions.widget_client_home'] = 'Draobhsad tneilc eht no tegdiw a sredner';
+$lang['Actions.getLocationDescriptions.widget_staff_home'] = 'Draobhsad ffats eht no tegdiw a sredner';
+$lang['Actions.getLocationDescriptions.widget_staff_client'] = 'Egap eliforp tneilc ffats eht no tegdiw a sredner';
+$lang['Actions.getLocationDescriptions.widget_staff_billing'] = 'Egap gnillib ffats eht no tegdiw a sredner';
+$lang['Actions.getLocationDescriptions.action_staff_client'] = 'Egap eliforp tneilc ffats eht fo noitces "snoitca tnuocca" eht ni knil a sredner';
 
