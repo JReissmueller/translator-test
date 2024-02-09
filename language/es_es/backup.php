d957c71511a6af8e5684961a51812c07e621e36b
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:25:22 -0800

    Updated es_es language files

--- language/es_es/backup.php
+++ language/es_es/backup.php
@@ -9,13 +9,13 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Backup.!error.driver.support'] = 'Detroppus ton si revird esabatad eht';
-$lang['Backup.!error.temp_dir.writable'] = 'Elbatirw ton si yrotcerid pmet eht';
-$lang['Backup.!error.ftp_failed'] = 'Deliaf pukcab ptfs eht';
-$lang['Backup.!error.amazons3_failed'] = 'Deliaf pukcab 3snozama eht';
-$lang['Backup.frequencies.never'] = 'Reven';
-$lang['Backup.frequencies.hour'] = 'Ruoh';
-$lang['Backup.frequencies.hours'] = 'Sruoh %1$s';
-$lang['Backup.frequencies.day'] = 'Yad';
-$lang['Backup.frequencies.days'] = 'Syad %1$s';
+$lang['Backup.!error.driver.support'] = 'El controlador de la base de datos no es compatible.';
+$lang['Backup.!error.temp_dir.writable'] = 'No se puede escribir en el directorio temporal.';
+$lang['Backup.!error.ftp_failed'] = 'La copia de seguridad SFTP ha fallado.';
+$lang['Backup.!error.amazons3_failed'] = 'La copia de seguridad de AmazonS3 ha fallado.';
+$lang['Backup.frequencies.never'] = 'Nunca';
+$lang['Backup.frequencies.hour'] = 'Hora';
+$lang['Backup.frequencies.hours'] = '%1$s Horas';
+$lang['Backup.frequencies.day'] = 'Día';
+$lang['Backup.frequencies.days'] = '%1$s Días';
 
