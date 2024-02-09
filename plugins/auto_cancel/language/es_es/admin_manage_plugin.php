e9a6334a6f4f55907433ed6c3f000e333cfaa6ee
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:17 -0800

    Updated es_es language files

---
+++ plugins/auto_cancel/language/es_es/admin_manage_plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Admin Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminManagePlugin.!success.settings_saved'] = 'Devas neeb evah sgnittes eht';
+$lang['AdminManagePlugin.index.page_title'] = 'Lecnac otua';
+$lang['AdminManagePlugin.index.boxtitle_auto_cancel'] = 'Lecnac otua';
+$lang['AdminManagePlugin.index.heading_settings'] = 'Sgnittes';
+$lang['AdminManagePlugin.index.field.schedule_days'] = 'Dednepsus retfa syad noitallecnac eludehcs';
+$lang['AdminManagePlugin.index.field.cancel_days'] = 'Dednepsus retfa syad secivres lecnac';
+$lang['AdminManagePlugin.index.field.submit'] = 'Evas';
+$lang['AdminManagePlugin.getDays.never'] = 'Reven';
+$lang['AdminManagePlugin.getDays.text_day'] = 'Yad %1$s';
+$lang['AdminManagePlugin.getDays.text_days'] = 'Syad %1$s';
+
