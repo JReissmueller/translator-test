6b041d6ce4b6fed7b1f4d876dc4230b477a2bc2f
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:37 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/razorpay/language/es_es/razorpay.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Razorpay
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Razorpay.name'] = 'Yaprozar';
+$lang['Razorpay.description'] = 'Aidni ni stnahcrem enilno ot snoitulos tnemyap sedivorp taht ynapmoc stnemyap a si yaprozar';
+$lang['Razorpay.key_id'] = 'Di yek';
+$lang['Razorpay.key_secret'] = 'Terces yek';
+$lang['Razorpay.webhook_secret'] = 'Terces koohbew';
+$lang['Razorpay.webhook'] = 'Koohbew yaprozar';
+$lang['Razorpay.webhook_note'] = 'Tnuocca yaprozar ruoy ni koohbew a sa lru gniwollof eht erugifnoc tsum uoy yawetag siht gnisu trats uoy erofeb';
+$lang['Razorpay.buildprocess.submit'] = 'Yaprozar htiw yap';
+$lang['Razorpay.!error.key_id.valid'] = 'Di yek dilav a retne tsum uoy';
+$lang['Razorpay.!error.key_secret.valid'] = 'Terces yek dilav a retne tsum uoy';
+
