7e369c983e1715105bcd129ba6fcddf3712b910e
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:21 -0800

    Updated es_es language files

---
+++ plugins/billing_overview/language/es_es/billing_overview_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Billing Overview Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BillingOverviewPlugin.name'] = 'Ecnalg a ta gnillib';
+$lang['BillingOverviewPlugin.description'] = 'Egap weivrevo gnillib eht no tegdiw a ni scitsitats gnillib syalpsid';
+
