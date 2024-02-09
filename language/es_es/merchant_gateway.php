55b6c9089e4a7193bc1bce5d87d3e1ffcd4aa5c2
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:48 -0800

    Updated es_es language files

---
+++ language/es_es/merchant_gateway.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Merchant Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MerchantGateway.!error.card_number_invalid'] = 'Dilavni si rebmun drac eht';
+$lang['MerchantGateway.!error.card_expired'] = 'Deripxe sah drac eht';
+$lang['MerchantGateway.!error.routing_number_invalid'] = 'Dilavni si rebmun gnituor eht';
+$lang['MerchantGateway.!error.account_number_invalid'] = 'Dilavni si rebmun tnuocca eht';
+$lang['MerchantGateway.!error.duplicate_transaction'] = 'Detceted neeb sah noitcasnart etacilpud a';
+$lang['MerchantGateway.!error.card_not_accepted'] = 'Detpecca ton era epyt taht fo sdrac';
+$lang['MerchantGateway.!error.invalid_security_code'] = 'Dilavni eb ot sraeppa edoc ytiruces eht';
+$lang['MerchantGateway.!error.address_verification_failed'] = 'Dilavni eb ot sraeppa sserdda eht';
+$lang['MerchantGateway.!error.transaction_not_found'] = 'Noitca taht gnissecorp rof elbaliava regnol on ro dilavni si noitcasnart eht';
+$lang['MerchantGateway.!error.unsupported'] = 'Noitca taht troppus ton seod yawetag eht';
+$lang['MerchantGateway.!error.general'] = 'Yawetag eht htiw tseuqer eht gnissecorp nehw derucco rorre na';
+
