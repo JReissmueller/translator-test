7e5aefbbe59b1091950be823a69396ee9aada76d
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:05 -0800

    Updated es_es language files

---
+++ language/es_es/service_changes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Service Changes
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ServiceChanges.!error.id.exists'] = 'Di egnahc ecivres dilavni';
+$lang['ServiceChanges.!error.service_id.exists'] = 'Di ecivres dilavni';
+$lang['ServiceChanges.!error.invoice_id.exists'] = 'Di eciovni dilavni';
+$lang['ServiceChanges.!error.invoice_id.unique'] = 'Niaga tes eb ton yam dna egnahc ecivres a rof tes ydaerla si di eciovni eht';
+$lang['ServiceChanges.!error.status.valid'] = 'Sutats dilav a tceles esaelp';
+$lang['ServiceChanges.status.pending'] = 'Gnidnep';
+$lang['ServiceChanges.status.completed'] = 'Detelpmoc';
+$lang['ServiceChanges.status.error'] = 'Rorre';
+$lang['ServiceChanges.status.canceled'] = 'Delecnac';
+
