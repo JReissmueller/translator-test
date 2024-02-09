6113554ba403df72a59f04a9d07e12348e6cb883
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:23 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/coin_payments/language/es_es/coin_payments.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Coin Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CoinPayments.name'] = 'Ten.stnemyapnioc';
+$lang['CoinPayments.description'] = 'Seef wol htiw niocetil dna nioctib sa hcus seicnerrucotpyrc rof metsys tuokcehc a';
+$lang['CoinPayments.merchant_id'] = 'Di tnahcrem stnemyapnioc';
+$lang['CoinPayments.ipn_secret'] = 'Terces npi';
+$lang['CoinPayments.buildprocess.submit'] = 'Stnemyapnioc htiw yap';
+
