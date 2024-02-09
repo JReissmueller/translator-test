45489a755b1b5bd6774403c08af82a15316e96e4
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:52 -0800

    Updated es_es language files

---
+++ language/es_es/module_manager.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Module Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ModuleManager.!error.company_id.valid'] = 'Dilavni si di ynapmoc';
+$lang['ModuleManager.!error.class.valid'] = 'Dilavni si ssalc';
+$lang['ModuleManager.!error.name.valid'] = 'Dilavni si eman';
+$lang['ModuleManager.!error.version.valid'] = 'Dilavni si noisrev';
+$lang['ModuleManager.!error.type_id.valid'] = 'Dilavni si di epyt';
+$lang['ModuleManager.!error.name.empty'] = 'Puorg eht rof eman a yficeps tsum uoy';
+$lang['ModuleManager.!error.module_rows[].valid'] = 'Eludom siht ot gnoleb ton od deificeps swor eht fo erom ro eno';
+$lang['ModuleManager.!error.force_limits.valid'] = '1 ro 0 ot tes eb tsum stimil tcirts ecrofne ot rehtehw';
+$lang['ModuleManager.!error.module_row_id.assigned_service'] = 'Secivres erom ro eno ot sgnoleb ti esuaceb deteled eb ton nac wor eht';
+$lang['ModuleManager.!error.module_row_id.assigned_package'] = 'Segakcap erom ro eno ot sgnoleb ti esuaceb deteled eb ton nac wor eht';
+$lang['ModuleManager.!error.module_group_id.assigned_package'] = 'Segakcap erom ro eno ot sgnoleb ti esuaceb deteled eb ton nac puorg eht';
+$lang['ModuleManager.!error.module_id.assigned_package'] = 'Segakcap evitca erom ro eno yb desu si ti esuaceb dellatsninu eb ton nac eludom eht';
+$lang['ModuleManager.!error.module_id.assigned_service'] = 'Secivres delecnacnu erom ro eno yb desu si ti esuaceb dellatsninu eb ton nac eludom eht';
+
