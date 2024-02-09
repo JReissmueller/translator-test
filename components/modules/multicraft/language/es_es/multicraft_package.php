40edd9ac9cf9c7a43094f75dcc1542a7ca425336
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:02 -0800

    Updated es_es language files

---
+++ components/modules/multicraft/language/es_es/multicraft_package.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Multicraft Package
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MulticraftPackage.!error.meta[server_name].format'] = 'Revres eht rof eman a tes esaelp';
+$lang['MulticraftPackage.!error.meta[players].format'] = 'Wolla ot stols reyalp fo rebmun mumixam eht tes esaelp';
+$lang['MulticraftPackage.!error.meta[memory].format'] = 'Bm ni regetni na sa yromem fo tnuoma mumixam eht tes esaelp';
+$lang['MulticraftPackage.!error.meta[jardir].format'] = 'Yrotcerid raj dilav a tceles esaelp';
+$lang['MulticraftPackage.!error.meta[user_jar].format'] = '0 ro 1 ot tes eb tsum raj eht tceles nac renwo eht rehtehw';
+$lang['MulticraftPackage.!error.meta[user_name].format'] = '0 ro 1 ot tes eb tsum eman a tes nac renwo eht rehtehw';
+$lang['MulticraftPackage.!error.meta[user_schedule].format'] = '0 ro 1 ot tes eb tsum sksat eludehcs nac renwo eht rehtehw';
+$lang['MulticraftPackage.!error.meta[user_ftp].format'] = '0 ro 1 ot tes eb tsum srehto ot ssecca ptf evig nac renwo eht rehtehw';
+$lang['MulticraftPackage.!error.meta[user_visibility].format'] = '0 ro 1 ot tes eb tsum ytilibisiv revres eht egnahc nac renwo eht rehtehw';
+$lang['MulticraftPackage.!error.meta[default_level].format'] = 'Elor tluafed dilav a tceles esaelp';
+$lang['MulticraftPackage.!error.meta[autostart].format'] = '0 ro 1 ot tes eb tsum trats yllacitamotua dluohs revres eht rehtehw';
+$lang['MulticraftPackage.!error.meta[create_ftp].format'] = '0 ro 1 ot tes eb tsum tnuocca ptf na etaerc ot rehtehw';
+$lang['MulticraftPackage.!error.meta[server_visibility].format'] = 'Ytilibisiv revres dilav a tceles esaelp';
+$lang['MulticraftPackage.package_fields.server_name'] = 'Eman revres';
+$lang['MulticraftPackage.package_fields.players'] = 'Stols reyalp';
+$lang['MulticraftPackage.package_fields.memory'] = 'Mar revres';
+$lang['MulticraftPackage.package_fields.tooltip.memory'] = 'Tnuoma tluafed eht esu ot knalb evael ro ,bm ni yromem tes';
+$lang['MulticraftPackage.package_fields.jarfile'] = 'Elif raj';
+$lang['MulticraftPackage.package_fields.tooltip.jarfile'] = 'Elif raj tluafed eht esu ot knalb evael';
+$lang['MulticraftPackage.package_fields.jardir'] = 'Yrotcerid gniwollof eht ni sraj rof kool';
+$lang['MulticraftPackage.package_fields.tooltip.jardir'] = 'Edom resu-itlum ni gninnur eb dluohs tfarcitlum ,yrotcerid raj nomead eht gnisu ton fi';
+$lang['MulticraftPackage.package_fields.jardir_daemon'] = 'Yrotcerid raj nomead';
+$lang['MulticraftPackage.package_fields.jardir_server'] = 'Yrotcerid raj revres';
+$lang['MulticraftPackage.package_fields.jardir_server_base'] = 'Yrotcerid esab revres';
+$lang['MulticraftPackage.package_fields.user_jar'] = 'Raj elbatceles renwo';
+$lang['MulticraftPackage.package_fields.user_name'] = 'Eman tes nac renwo';
+$lang['MulticraftPackage.package_fields.tooltip.user_name'] = 'Eman s\'revres eht egnahc ro enifed nac renwo eht rehtehw stes';
+$lang['MulticraftPackage.package_fields.user_schedule'] = 'Sksat eludehcs nac renwo';
+$lang['MulticraftPackage.package_fields.tooltip.user_schedule'] = 'Gnittes evasotua eht egnahc dna sksat deludehcs etaerc nac renwo eht rehtehw stes';
+$lang['MulticraftPackage.package_fields.user_ftp'] = 'Ptf eganam nac renwo';
+$lang['MulticraftPackage.package_fields.tooltip.user_ftp'] = 'Sresu rehto ot ssecca ptf evig nac renwo eht rehtehw stes';
+$lang['MulticraftPackage.package_fields.user_visibility'] = 'Ytilibisiv tes nac renwo';
+$lang['MulticraftPackage.package_fields.tooltip.user_visibility'] = 'Elor tluafed eht dna ytilibisiv revres eht egnahc nac renwo eht rehtehw stes';
+$lang['MulticraftPackage.package_fields.default_level'] = 'Elor tluafed';
+$lang['MulticraftPackage.package_fields.tooltip.default_level'] = 'Gnitsil-etihw rof ssecca on esu .tcennoc tsrif yeht nehw dengissa eb lliw sreyalp elor hcihw tceles';
+$lang['MulticraftPackage.package_fields.default_level_0'] = 'Ssecca on';
+$lang['MulticraftPackage.package_fields.default_level_10'] = 'Tseug';
+$lang['MulticraftPackage.package_fields.default_level_20'] = 'Resu';
+$lang['MulticraftPackage.package_fields.default_level_30'] = 'Rotaredom';
+$lang['MulticraftPackage.package_fields.autostart'] = 'Revres trats';
+$lang['MulticraftPackage.package_fields.tooltip.autostart'] = 'Stratser tfarcitlum nehw revres eht strats yllacitamotua';
+$lang['MulticraftPackage.package_fields.create_ftp'] = 'Tnuocca ptf etaerc';
+$lang['MulticraftPackage.package_fields.tooltip.create_ftp'] = 'Tnuocca ptf na setaerc yllacitamotua';
+$lang['MulticraftPackage.package_fields.server_visibility'] = 'Ytilibisiv revres';
+$lang['MulticraftPackage.package_fields.tooltip.server_visibility'] = 'Tsil revres tfarcitlum eht ni revres eht fo ytilibisiv eht stes';
+$lang['MulticraftPackage.package_fields.server_visibility_0'] = 'Ylno renwo';
+$lang['MulticraftPackage.package_fields.server_visibility_1'] = 'Elor tluafed yb';
+$lang['MulticraftPackage.package_fields.server_visibility_2'] = 'Ylno selor htiw sresu';
+
