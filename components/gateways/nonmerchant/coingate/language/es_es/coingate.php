a4c8a4b90d16862270fc2d808f6fc6b609722d11
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:25 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/coingate/language/es_es/coingate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Coingate
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Coingate.name'] = 'Moc.etagnioc';
+$lang['Coingate.description'] = 'Moc.etagnioc aiv snioctla ro nioctib htiw yap';
+$lang['Coingate.app_id'] = 'Di ppa';
+$lang['Coingate.api_key'] = 'Yek ipa';
+$lang['Coingate.api_secret'] = 'Terces ipa';
+$lang['Coingate.coingate_environment'] = 'Tnemnorivne etagnioc';
+$lang['Coingate.environment.sandbox'] = 'Xobdnas';
+$lang['Coingate.environment.live'] = 'Evil';
+$lang['Coingate.receive_currency'] = 'Ycnerruc tuoyap';
+$lang['Coingate.receive_currency_note'] = 'Etagnioc ta lawardhtiw a gnikam erofeb ssenisub ruoy   										
+yfirev ot deksa eb lliw uoy dsu ro rue esoohc uoy fi taht eton esaelp   										
+.etagnioc ta lawardhtiw gnikam nehw eviecer ot tnaw uoy ycnerruc';
+$lang['Coingate.environment_note'] = 'Edom xobdnas rof krow ton lliw moc.etagnioc no detareneg slaitnederc ipa .moc.etagnioc.xobdnas no slaitnederc ipa etarapes etareneg tsum uoy edom xobdnas rof taht ,eton esaelp .moc.etagnioc.xobdnas no tset ot "xobdnas" elbane';
+$lang['Coingate.buildprocess.submit'] = 'Moc.etagnioc';
+$lang['Coingate.receive_currency.usd'] = '$ srallod su';
+$lang['Coingate.receive_currency.btc'] = '';
+$lang['Coingate.receive_currency.eur'] = '';
+$lang['Coingate.!error.app_id.empty'] = 'Ytpme eb ton nac di ppa';
+$lang['Coingate.!error.api_key.empty'] = 'Ytpme eb ton nac yek ipa';
+$lang['Coingate.!error.api_secret.empty'] = 'Ytpme eb ton nac terces ipa';
+$lang['Coingate.!error.payment.invalid'] = 'Dessecorp eb ton dluoc dna dilavni si noitcasnart eht';
+$lang['Coingate.!error.payment.canceled'] = 'Dessecorp eb ton dluoc dna delecnac si noitcasnart eht';
+$lang['Coingate.!error.payment.expired'] = 'Dessecorp eb ton dluoc dna deripxe sah noitcasnart eht';
+$lang['Coingate.!error.failed.response'] = 'Dessecorp eb ton dluoc noitcasnart eht';
+
