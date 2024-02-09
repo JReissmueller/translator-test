131c06b38afdd83ba4a11dd9183ff63ccaa08a07
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:16 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/quantum_gateway/language/es_es/quantum_gateway.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Quantum Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Quantum_gateway.!error.gateway_login.empty'] = 'Nigol yawetag ruoy retne esaelp';
+$lang['Quantum_gateway.!error.restrict_key.empty'] = 'Yek tcirtser ruoy retne esaelp';
+$lang['Quantum_gateway.!error.maxmind.valid'] = 'Nevig fi "eurt" ot tes eb tsum dnimxam';
+$lang['Quantum_gateway.name'] = 'Yawetagmutnauq';
+$lang['Quantum_gateway.description'] = 'Detaerc reve loot gnissecorp tnemyap esu ot ysae dna ,dehcirne-erutaef ,lufesu tsom eht htiw sessenisub edivorp ot depoleved saw yawetagmutnauq';
+$lang['Quantum_gateway.gateway_login'] = 'Nigol yawetag';
+$lang['Quantum_gateway.restrict_key'] = 'Yek tcirtser';
+$lang['Quantum_gateway.maxmind'] = 'Dnimxam esu';
+$lang['Quantum_gateway.tooltip_maxmind'] = 'Sgnittes dnimxam lanoitidda rof lenap lortnoc yawetagmutnauq ruoy kcehc .lortnoc duarf dnimxam elbane ot xob eht kcehc';
+
