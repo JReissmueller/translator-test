4a566a5b3c333c0442444da8f223dd86802df105
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:25 -0800

    Updated es_es language files

---
+++ plugins/client_documents/language/es_es/client_documents_files.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Client Documents Files
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDocumentsFiles.!error.client_id.exists'] = 'Tsixe ton seod tneilc eht';
+$lang['ClientDocumentsFiles.!error.name.valid'] = 'Deriuqer si eman tnemucod a';
+$lang['ClientDocumentsFiles.!error.date_added.valid'] = 'Deriuqer si dedda etad dilav a';
+
