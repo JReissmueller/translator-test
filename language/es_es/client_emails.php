b5fa2971adeab8a3bc724a368af7b0fb6fe988a9
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:19 -0800

    Updated es_es language files

---
+++ language/es_es/client_emails.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Client Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientEmails.index.page_title'] = 'Yrotsih liame %1$s# tneilc';
+$lang['ClientEmails.index.boxtitle_emails'] = 'Yrotsih liame';
+$lang['ClientEmails.index.heading_date'] = 'Etad';
+$lang['ClientEmails.index.heading_subject'] = 'Tcejbus';
+$lang['ClientEmails.index.heading_summary'] = 'Yrammus';
+$lang['ClientEmails.index.text_to'] = 'Ot';
+$lang['ClientEmails.index.text_cc'] = 'Cc';
+$lang['ClientEmails.index.text_from'] = 'Morf';
+$lang['ClientEmails.index.no_results'] = 'Yrotsih liame on evah uoy';
+
