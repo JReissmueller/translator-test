77a7d77e46b32ac4861fb70fc684099a89bd993f
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:27:19 -0800

    Updated es_es language files

--- plugins/download_manager/language/es_es/client_main.php
+++ plugins/download_manager/language/es_es/client_main.php
@@ -9,9 +9,9 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['ClientMain.!text.root_directory'] = 'Emoh';
-$lang['ClientMain.index.page_title'] = 'Sdaolnwod';
-$lang['ClientMain.index.boxtitle_downloads'] = 'Sdaolnwod';
-$lang['ClientMain.index.login'] = 'Noitces siht ni sdaolnwod weiv ot ni gol tsum uoy';
-$lang['ClientMain.index.no_downloads'] = 'Noitces siht ni elbaliava sdaolnwod on era ereht';
+$lang['ClientMain.!text.root_directory'] = 'Inicio';
+$lang['ClientMain.index.boxtitle_downloads'] = 'Descargas';
+$lang['ClientMain.index.login'] = 'Debe iniciar sesión para ver las descargas de esta sección.';
+$lang['ClientMain.index.no_downloads'] = 'No hay descargas disponibles en esta sección.';
+$lang['ClientMain.index.page_title'] = 'Descargas';
 
