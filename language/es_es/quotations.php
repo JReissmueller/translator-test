19906627fae7294354942b8fbbb2556359a12167
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:03 -0800

    Updated es_es language files

---
+++ language/es_es/quotations.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Quotations
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Quotations.!error.quotation_add.failed'] = 'Niaga yrt esaelp .detaerc eb ton dluoc noitatouq siht';
+$lang['Quotations.!error.id.quotation_invoiced'] = 'Detide eb tonnac dna deciovni neeb sah etouq siht';
+$lang['Quotations.!error.id_format.empty'] = 'Snoitatouq rof tes tamrof di on';
+$lang['Quotations.!error.id_format.length'] = 'Sretcarahc 46 deecxe ton yam snoitatouq rof tamrof di eht';
+$lang['Quotations.!error.id_value.valid'] = 'Eulav di noitatouq enimreted ot elbanu';
+$lang['Quotations.!error.client_id.exists'] = 'Di tneilc dilavni';
+$lang['Quotations.!error.staff_id.exists'] = 'Di ffats dilavni';
+$lang['Quotations.!error.title.empty'] = 'Eltit a retne esaelp';
+$lang['Quotations.!error.title.length'] = 'Sretcarahc 552 deecxe ton yam snoitatouq rof eltit eht';
+$lang['Quotations.!error.date_created.format'] = 'Tamrof etad dilavni na ni si etad noitaerc eht';
+$lang['Quotations.!error.date_expires.format'] = 'Tamrof etad dilavni na ni si etad noitaripxe eht';
+$lang['Quotations.!error.date_expires.after_created'] = 'Dellib noitaerc eht retfa ro no eb tsum eud noitaripxe eht';
+$lang['Quotations.!error.status.format'] = 'Sutats dilavni';
+$lang['Quotations.!error.currency.length'] = 'Htgnel ni sretcarahc 3 eb tsum edoc ycnerruc eht';
+$lang['Quotations.!error.status.valid'] = 'Deciovni eb ot gnidnep ro devorppa rehtie eb tsum noitatouq eht';
+$lang['Quotations.!error.first_due_date.format'] = 'Tamrof etad dilavni na ni si etad eud tsrif eht';
+$lang['Quotations.!error.second_due_date.format'] = 'Tamrof etad dilavni na ni si etad eud dnoces eht';
+$lang['Quotations.!error.percentage_due.format'] = 'Rebmun a eb tsum eud egatnecrep eht';
+$lang['Quotations.!error.percentage_due.valid'] = '001 ot lauqe ro ssel dna orez naht retaerg rebmun a eb tsum eud egatnecrep eht';
+$lang['Quotations.!error.lines[][id].exists'] = 'Di meti enil dilavni';
+$lang['Quotations.!error.lines[][description].empty'] = 'Noitpircsed meti enil a retne esaelp';
+$lang['Quotations.!error.lines[][qty].minimum'] = 'Erom ro 1 fo ytitnauq a retne esaelp';
+$lang['Quotations.!error.lines[][amount].format'] = 'Rebmun a eb tsum tsoc tinu eht';
+$lang['Quotations.!error.lines[][tax].format'] = '\'eslaf\' ro \'eurt\' a eb tsum xat meti enil';
+$lang['Quotations.getstatuses.draft'] = 'Tfard';
+$lang['Quotations.getstatuses.approved'] = 'Devorppa';
+$lang['Quotations.getstatuses.pending'] = 'Gnidnep';
+$lang['Quotations.getstatuses.expired'] = 'Deripxe';
+$lang['Quotations.getstatuses.invoiced'] = 'Deciovni';
+$lang['Quotations.getstatuses.dead'] = 'Daed';
+$lang['Quotations.getstatuses.lost'] = 'Tsol';
+
