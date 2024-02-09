2ca380515437a1cc0f46ebdf48c3beda6a28bab6
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:28 -0800

    Updated es_es language files

---
+++ components/reports/invoice_creation/language/es_es/invoice_creation.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Invoice Creation
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['InvoiceCreation.name'] = 'Noitaerc eciovni';
+$lang['InvoiceCreation.options.field_start_date'] = 'Etad trats';
+$lang['InvoiceCreation.options.field_end_date'] = 'Etad dne';
+$lang['InvoiceCreation.options.field_status'] = 'Sutats';
+$lang['InvoiceCreation.option.any'] = 'Yna';
+$lang['InvoiceCreation.heading.id_code'] = '# eciovni';
+$lang['InvoiceCreation.heading.client_id_code'] = '# tneilc';
+$lang['InvoiceCreation.heading.subtotal'] = 'Latotbus';
+$lang['InvoiceCreation.heading.total'] = 'Latot';
+$lang['InvoiceCreation.heading.paid'] = 'Diap';
+$lang['InvoiceCreation.heading.currency'] = 'Ycnerruc';
+$lang['InvoiceCreation.heading.status'] = 'Sutats';
+$lang['InvoiceCreation.heading.date_billed'] = 'Dellib etad';
+$lang['InvoiceCreation.heading.date_due'] = 'Eud etad';
+$lang['InvoiceCreation.heading.date_closed'] = 'Desolc etad';
+
