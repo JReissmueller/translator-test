33ca7bcaf55be352ef85732dae2f362e630f9a32
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:56 -0800

    Updated es_es language files

---
+++ language/es_es/admin_myinfo.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Admin Myinfo
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMyinfo.!success.updated'] = 'Detadpu yllufsseccus erew sgnittes tnuocca ruoy';
+$lang['AdminMyinfo.!success.notices_updated'] = 'Detadpu yllufsseccus erew sgnittes eciton ruoy';
+$lang['AdminMyinfo.gettabnames.text_index'] = 'Tnuocca';
+$lang['AdminMyinfo.gettabnames.text_notices'] = 'Seciton';
+$lang['AdminMyinfo.index.page_title'] = 'Noitamrofni ym';
+$lang['AdminMyinfo.index.boxtitle_myinfo'] = 'Noitamrofni ym';
+$lang['AdminMyinfo.index.field_username'] = 'Emanresu';
+$lang['AdminMyinfo.index.field_firstname'] = 'Eman tsrif';
+$lang['AdminMyinfo.index.field_lastname'] = 'Eman tsal';
+$lang['AdminMyinfo.index.field_email'] = 'Liam-e';
+$lang['AdminMyinfo.index.field_mobileemail'] = 'Liam-e elibom';
+$lang['AdminMyinfo.index.field_mobilenumber'] = 'Rebmun elibom';
+$lang['AdminMyinfo.index.field_newpass'] = 'Drowssap wen';
+$lang['AdminMyinfo.index.field_confirmpass'] = 'Drowssap mrifnoc';
+$lang['AdminMyinfo.index.field_recovery_email'] = ')lanoitpo( liame yrevocer';
+$lang['AdminMyinfo.index.field_twofactormode'] = 'Noitacitnehtua rotcaf owt';
+$lang['AdminMyinfo.index.field_twofactorkey'] = 'Yek rotcaf owt';
+$lang['AdminMyinfo.index.field_twofactorpin'] = 'Nip rotcaf owt';
+$lang['AdminMyinfo.index.field_language'] = 'Egaugnal';
+$lang['AdminMyinfo.index.field_otp'] = 'Drowssap emit-eno';
+$lang['AdminMyinfo.index.note_otp'] = 'Drowssap emit-eno detareneg ruoy retne ,yltcerroc derugifnoc si noitacitnehtua rotcaf owt ruoy mrifnoc ot';
+$lang['AdminMyinfo.index.field_groups'] = 'Spuorg ffats';
+$lang['AdminMyinfo.index.field_currentpass'] = 'Drowssap tnerruc';
+$lang['AdminMyinfo.index.field_infosubmit'] = 'Tnuocca etadpu';
+$lang['AdminMyinfo.notices.page_title'] = 'Seciton > noitamrofni ym';
+$lang['AdminMyinfo.notices.heading_notices'] = 'Seciton ccb liame';
+
