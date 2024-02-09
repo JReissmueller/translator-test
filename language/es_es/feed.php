3f95e8189bb958d0d3bbed64ee1b29a686ac7385
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:41 -0800

    Updated es_es language files

---
+++ language/es_es/feed.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Feed.!error.disabled'] = 'Aera ffats eht ni sdeef > ynapmoc > sgnittes tisiv elbane ot ,delbasid si deef atad siht';
+$lang['Feed.!error.invalid'] = 'Dilavni s\'ti ro stsixe ton seod ecruoser detseuqer eht';
+
