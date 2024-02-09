5658cc999d8e05eaf252592fad1ec6671d832c30
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:58 -0800

    Updated es_es language files

---
+++ language/es_es/package_option_groups.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Package Option Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageOptionGroups.!error.company_id.exists'] = 'Di ynapmoc dilav a yficeps esaelp';
+$lang['PackageOptionGroups.!error.name.empty'] = 'Eman puorg a yficeps esaelp';
+$lang['PackageOptionGroups.!error.name.length'] = 'Htgnel ni sretcarahc 821 deecxe ton yam eman puorg eht';
+$lang['PackageOptionGroups.!error.group_id.exists'] = 'Di puorg noitpo egakcap dilavni';
+$lang['PackageOptionGroups.!error.hide_options.valid'] = '1 ro 0 ot tes eb tsum snoitpo edih ot rehtehw';
+$lang['PackageOptionGroups.!error.hidden.valid'] = '1 ro 0 ot tes eb tsum puorg noitpo siht edih ot rehtehw';
+
