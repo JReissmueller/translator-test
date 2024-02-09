2d906010c095febe36052ba1184550642fcda8f6
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:54 -0800

    Updated es_es language files

---
+++ language/es_es/navigation.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Navigation
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Navigation.!error.action_id.valid'] = 'Di noitca dilavni';
+$lang['Navigation.!error.order.valid'] = 'Eulav ciremun a eb tsum xedni redro eht';
+$lang['Navigation.!error.parent_id.valid'] = 'Di meti noitagivan tnerap dilavni';
+$lang['Navigation.getprimary.nav_home'] = 'Emoh';
+$lang['Navigation.getprimary.nav_home_dashboard'] = 'Draobhsad';
+$lang['Navigation.getprimary.nav_clients'] = 'Stneilc';
+$lang['Navigation.getprimary.nav_clients_browse'] = 'Stneilc esworb';
+$lang['Navigation.getprimary.nav_billing'] = 'Gnillib';
+$lang['Navigation.getprimary.nav_billing_overview'] = 'Weivrevo';
+$lang['Navigation.getprimary.nav_billing_invoices'] = 'Seciovni';
+$lang['Navigation.getprimary.nav_billing_quotations'] = 'Snoitatouq';
+$lang['Navigation.getprimary.nav_billing_transactions'] = 'Snoitcasnart';
+$lang['Navigation.getprimary.nav_billing_services'] = 'Secivres';
+$lang['Navigation.getprimary.nav_billing_reports'] = 'Stroper';
+$lang['Navigation.getprimary.nav_billing_printqueue'] = 'Eueuq tnirp';
+$lang['Navigation.getprimary.nav_billing_batch'] = 'Hctab';
+$lang['Navigation.getprimary.nav_packages'] = 'Segakcap';
+$lang['Navigation.getprimary.nav_packages_browse'] = 'Segakcap esworb';
+$lang['Navigation.getprimary.nav_packages_groups'] = 'Spuorg egakcap';
+$lang['Navigation.getprimary.nav_package_options'] = 'Snoitpo elbarugifnoc';
+$lang['Navigation.getprimary.nav_tools'] = 'Sloot';
+$lang['Navigation.getprimary.nav_tools_logs'] = 'Sgol';
+$lang['Navigation.getprimary.nav_tools_currency'] = 'Ycnerruc trevnoc';
+$lang['Navigation.getprimary.nav_tools_utilities'] = 'Seitilitu';
+$lang['Navigation.getprimary.nav_tools_renewals'] = 'Eueuq lawener';
+$lang['Navigation.getprimary.nav_tools_blacklist'] = 'Tsilkcalb';
+$lang['Navigation.getprimary.nav_settings_company'] = 'Ynapmoc';
+$lang['Navigation.getprimary.nav_settings_system'] = 'Metsys';
+$lang['Navigation.getprimaryclient.nav_dashboard'] = 'Draobhsad';
+$lang['Navigation.getprimaryclient.nav_services'] = 'Secivres';
+$lang['Navigation.getprimaryclient.nav_paymentaccounts'] = 'Stnuocca tnemyap';
+$lang['Navigation.getprimaryclient.nav_paymentaccounts_add'] = 'Tnuocca tnemyap dda';
+$lang['Navigation.getprimaryclient.nav_contacts'] = 'Stcatnoc';
+$lang['Navigation.getprimaryclient.nav_contacts_add'] = 'Tcatnoc dda';
+$lang['Navigation.getprimaryclient.nav_return'] = 'Draobhsad ot nruter';
+$lang['Navigation.getprimarypublic.nav_dashboard'] = 'Emoh';
+$lang['Navigation.getcompany.nav_general'] = 'Lareneg';
+$lang['Navigation.getcompany.nav_general_localization'] = 'Noitazilacol';
+$lang['Navigation.getcompany.nav_general_international'] = 'Noitazilanoitanretni';
+$lang['Navigation.getcompany.nav_general_encryption'] = 'Noitpyrcne';
+$lang['Navigation.getcompany.nav_general_contacttypes'] = 'Sepyt tcatnoc';
+$lang['Navigation.getcompany.nav_general_marketing'] = 'Gnitekram';
+$lang['Navigation.getcompany.nav_general_smart_search'] = 'Hcraes trams';
+$lang['Navigation.getcompany.nav_general_humanverification'] = 'Noitacifirev namuh';
+$lang['Navigation.getcompany.nav_lookandfeel'] = 'Leef dna kool';
+$lang['Navigation.getcompany.nav_lookandfeel_themes'] = 'Semeht';
+$lang['Navigation.getcompany.nav_lookandfeel_template'] = 'Etalpmet';
+$lang['Navigation.getcompany.nav_lookandfeel_layout'] = 'Tuoyal';
+$lang['Navigation.getcompany.nav_lookandfeel_navigation'] = 'Noitagivan';
+$lang['Navigation.getcompany.nav_lookandfeel_actions'] = 'Snoitca';
+$lang['Navigation.getcompany.nav_lookandfeel_customize'] = 'Ezimotsuc';
+$lang['Navigation.getcompany.nav_automation'] = 'Noitamotua';
+$lang['Navigation.getcompany.nav_billing'] = 'Tnemyap/gnillib';
+$lang['Navigation.getcompany.nav_billing_invoices'] = 'Snoitpo egrahc dna eciovni';
+$lang['Navigation.getcompany.nav_billing_custominvoice'] = 'Noitazimotsuc eciovni';
+$lang['Navigation.getcompany.nav_billing_deliverymethods'] = 'Yreviled eciovni';
+$lang['Navigation.getcompany.nav_billing_latefees'] = 'Seef etal';
+$lang['Navigation.getcompany.nav_billing_notices'] = 'Seciton';
+$lang['Navigation.getcompany.nav_billing_coupons'] = 'Snopuoc';
+$lang['Navigation.getcompany.nav_billing_acceptedtypes'] = 'Sepyt tnemyap detpecca';
+$lang['Navigation.getcompany.nav_modules'] = 'Seludom';
+$lang['Navigation.getcompany.nav_messengers'] = 'Sregnessem';
+$lang['Navigation.getcompany.nav_messengers_messengers'] = 'Sregnessem';
+$lang['Navigation.getcompany.nav_messengers_configuration'] = 'Noitarugifnoc regnessem';
+$lang['Navigation.getcompany.nav_messengers_templates'] = 'Setalpmet egassem';
+$lang['Navigation.getcompany.nav_gateways'] = 'Syawetag tnemyap';
+$lang['Navigation.getcompany.nav_taxes'] = 'Sexat';
+$lang['Navigation.getcompany.nav_taxes_basictax'] = 'Sgnittes xat cisab';
+$lang['Navigation.getcompany.nav_taxes_taxrules'] = 'Selur xat';
+$lang['Navigation.getcompany.nav_emails'] = 'Sliame';
+$lang['Navigation.getcompany.nav_emails_mail'] = 'Sgnittes liam';
+$lang['Navigation.getcompany.nav_emails_templates'] = 'Setalpmet liame';
+$lang['Navigation.getcompany.nav_emails_signatures'] = 'Serutangis';
+$lang['Navigation.getcompany.nav_clientoptions'] = 'Snoitpo tneilc';
+$lang['Navigation.getcompany.nav_clientoptions_general'] = 'Sgnittes tneilc lareneg';
+$lang['Navigation.getcompany.nav_clientoptions_customfields'] = 'Sdleif motsuc tneilc';
+$lang['Navigation.getcompany.nav_clientoptions_requiredfields'] = 'Sdleif tneilc deriuqer';
+$lang['Navigation.getcompany.nav_currencies'] = 'Seicnerruc';
+$lang['Navigation.getcompany.nav_currency_currencysetup'] = 'Putes ycnerruc';
+$lang['Navigation.getcompany.nav_currency_active'] = 'Seicnerruc evitca';
+$lang['Navigation.getcompany.nav_plugins'] = 'Snigulp';
+$lang['Navigation.getcompany.nav_groups'] = 'Spuorg tneilc';
+$lang['Navigation.getcompany.nav_feeds'] = 'Sdeef atad';
+$lang['Navigation.getcompany.nav_feeds_general'] = 'Lareneg';
+$lang['Navigation.getcompany.nav_feeds_settings'] = 'Sgnittes deef';
+$lang['Navigation.getsystem.nav_general'] = 'Lareneg';
+$lang['Navigation.getsystem.nav_general_basic'] = 'Putes cisab';
+$lang['Navigation.getsystem.nav_general_geoip'] = 'Sgnittes pioeg';
+$lang['Navigation.getsystem.nav_general_maintenance'] = 'Ecnanetniam';
+$lang['Navigation.getsystem.nav_general_license'] = 'Yek esnecil';
+$lang['Navigation.getsystem.nav_general_paymenttypes'] = 'Sepyt tnemyap';
+$lang['Navigation.getsystem.nav_automation'] = 'Noitamotua';
+$lang['Navigation.getsystem.nav_companies'] = 'Seinapmoc';
+$lang['Navigation.getsystem.nav_backup'] = 'Pukcab';
+$lang['Navigation.getsystem.nav_backup_ftp'] = 'Ptf eruces';
+$lang['Navigation.getsystem.nav_backup_index'] = 'Dnamed no';
+$lang['Navigation.getsystem.nav_backup_amazon'] = '3s nozama';
+$lang['Navigation.getsystem.nav_staff'] = 'Ffats';
+$lang['Navigation.getsystem.nav_staff_manage'] = 'Ffats eganam';
+$lang['Navigation.getsystem.nav_staff_groups'] = 'Spuorg ffats';
+$lang['Navigation.getsystem.nav_api'] = 'Ssecca ipa';
+$lang['Navigation.getsystem.nav_upgrade'] = 'Snoitpo edargpu';
+$lang['Navigation.getsystem.nav_help'] = 'Pleh';
+$lang['Navigation.getsystem.nav_help_index'] = 'Secruoser';
+$lang['Navigation.getsystem.nav_help_notes'] = 'Seton esaeler';
+$lang['Navigation.getsystem.nav_help_about'] = 'Atselb tuoba';
+$lang['Navigation.getsystem.nav_marketplace'] = 'Ecalptekram';
+$lang['Navigation.getsearchoptions.smart'] = 'Hcraes trams';
+$lang['Navigation.getsearchoptions.clients'] = 'Hcraes tneilc';
+$lang['Navigation.getsearchoptions.invoices'] = 'Hcraes eciovni';
+$lang['Navigation.getsearchoptions.transactions'] = 'Hcraes noitcasnart';
+$lang['Navigation.getsearchoptions.services'] = 'Hcraes ecivres';
+$lang['Navigation.getsearchoptions.packages'] = 'Hcraes egakcap';
+
