6d1f954b67b6f8aaaef97b0c2de58621f3bf1405
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:44 -0800

    Updated es_es language files

---
+++ language/es_es/admin_company_feeds.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Admin Company Feeds
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyFeeds.!success.datafeeds_updated'] = 'Detadpu yllufsseccus neeb evah sgnittes sdeef atad eht';
+$lang['AdminCompanyFeeds.!success.settings_updated'] = 'Detadpu yllufsseccus neeb evah sgnittes deef eht';
+$lang['AdminCompanyFeeds.index.page_title'] = 'Sdeef atad > ynapmoc > sgnittes';
+$lang['AdminCompanyFeeds.index.boxtitle_data_feeds'] = 'Sdeef atad';
+$lang['AdminCompanyFeeds.index.data_feeds_description'] = 'Erom dna ,secirp ,seman egakcap ekil ,setisbew lanretxe no deddebme eb ot atselb morf tnetnoc wolla sdeef atad';
+$lang['AdminCompanyFeeds.index.field_enable_data_feeds'] = 'Sdeef atad elbane';
+$lang['AdminCompanyFeeds.index.field_submit'] = 'Sgnittes etadpu';
+$lang['AdminCompanyFeeds.settings.page_title'] = 'Sgnittes deef > sdeef atad > ynapmoc > sgnittes';
+$lang['AdminCompanyFeeds.settings.boxtitle_feed_settings'] = 'Sgnittes deef';
+$lang['AdminCompanyFeeds.settings.header_endpoints'] = 'Stniopdne';
+$lang['AdminCompanyFeeds.settings.field_submit'] = 'Sgnittes etadpu';
+
