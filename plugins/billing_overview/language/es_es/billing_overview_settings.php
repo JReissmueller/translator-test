53b0ea85e058d690d77bdff3beff7cd1513b3923
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:22 -0800

    Updated es_es language files

---
+++ plugins/billing_overview/language/es_es/billing_overview_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Billing Overview Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BillingOverviewSettings.!error.staff_id.exists'] = 'Di ffats dilavni';
+$lang['BillingOverviewSettings.!error.company_id.exists'] = 'Di ynapmoc dilavni';
+$lang['BillingOverviewSettings.!error.settings[][key].empty'] = 'Ytpme eb yam yek gnittes on';
+$lang['BillingOverviewSettings.!error.settings[][value].length'] = 'Sretcarahc 552 deecxe ton yam htgnel eulav gnittes laudividni hcae';
+
