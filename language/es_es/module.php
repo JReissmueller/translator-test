dcba59e0faf1988cae370e5a4c3f6566cc294a6a
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:51 -0800

    Updated es_es language files

---
+++ language/es_es/module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Module.!error.invalid'] = 'Deviecer saw esnopser ipa dezingocernu ro dilavni na';
+$lang['Module.!error.unsupported'] = 'Noitca taht troppus ton seod eludom eht';
+$lang['Module.!error.general'] = 'Eludom eht htiw tseuqer eht gnissecorp nehw derucco rorre na';
+
