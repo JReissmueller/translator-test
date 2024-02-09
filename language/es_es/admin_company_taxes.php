a63ad9cdfa94d2faa77820f06f2bfc531c6eb6ca
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:51 -0800

    Updated es_es language files

---
+++ language/es_es/admin_company_taxes.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Admin Company Taxes
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyTaxes.!success.basic_updated'] = '!detadpu yllufsseccus erew sgnittes xat cisab eht';
+$lang['AdminCompanyTaxes.!success.taxrule_created'] = '!detaerc yllufsseccus neeb sah elur xat eht';
+$lang['AdminCompanyTaxes.!success.taxrule_updated'] = '!detadpu yllufsseccus neeb sah elur xat eht';
+$lang['AdminCompanyTaxes.!success.rule_deleted'] = 'Deteled yllufsseccus neeb sah elur xat eht';
+$lang['AdminCompanyTaxes.countries.all'] = '-- lla --';
+$lang['AdminCompanyTaxes.states.all'] = '-- lla --';
+$lang['AdminCompanyTaxes.!tooltip.type'] = 'Yalpsid latot redro eht ni ti edulcni ton lliw tub ,tes uoy secirp meti eht ot noitidda ni xat etaluclac lliw evisulcxe
+>/rb<.tes uoy secirp meti eht ot noitidda ni xat etaluclac lliw )evitidda( evisulcni
+>/rb<.sresu tpmexe xat rof ecirp meti eht morf detcartbus eb lliw dna tes uoy secirp meti eht fo trap a sa xat etaluclac lliw evisulcni';
+$lang['AdminCompanyTaxes.!tooltip.level'] = 'Ylppa selur xat elpitlum fi redro xat eht tes ot uoy swolla level xat eht';
+$lang['AdminCompanyTaxes.!tooltip.name'] = ')xat selas .g.e( xat eht fo eman deyalpsid eht';
+$lang['AdminCompanyTaxes.!tooltip.amount'] = 'Egatnecrep a sa tnuoma xat eht';
+$lang['AdminCompanyTaxes.!tooltip.country'] = 'Ot seilppa elur xat siht taht yrtnuoc eht tceles';
+$lang['AdminCompanyTaxes.!tooltip.state'] = 'Ot seilppa elur xat siht taht ecnivorp/etats eht tceles';
+$lang['AdminCompanyTaxes.basic.page_title'] = 'Sgnittes xat cisab > sexat > ynapmoc > sgnittes';
+$lang['AdminCompanyTaxes.basic.boxtitle_basic'] = 'Sgnittes xat cisab';
+$lang['AdminCompanyTaxes.basic.heading_general'] = 'Sgnittes lareneg';
+$lang['AdminCompanyTaxes.basic.field_enable_tax'] = 'Xat elbane';
+$lang['AdminCompanyTaxes.basic.note_enable_tax'] = 'Ynapmoc siht rof xat elbane ot noitpo siht kcehc';
+$lang['AdminCompanyTaxes.basic.field_cascade_tax'] = 'Xat edacsac';
+$lang['AdminCompanyTaxes.basic.note_cascade_tax'] = 'Latot eciovni xat-erp eht no ylno dessessa era 2 level xat dna 1 level xat esiwehto .xat no xat a ni stluser siht .1 level xat gnidulcni latot wen siht no dessessa eb dluow 2 level xat dna ,latot eciovni eht no dessessa eb tsrif lliw 1 level xat ,delbane fi';
+$lang['AdminCompanyTaxes.basic.field_setup_fee_tax'] = 'Seef putes xat';
+$lang['AdminCompanyTaxes.basic.note_setup_fee_tax'] = 'Dexat eb lliw seef putes yna ,delbane fi';
+$lang['AdminCompanyTaxes.basic.field_cancelation_fee_tax'] = 'Seef noitalecnac xat';
+$lang['AdminCompanyTaxes.basic.note_cancelation_fee_tax'] = 'Dexat eb lliw seef noitalecnac yna ,delbane fi';
+$lang['AdminCompanyTaxes.basic.field_taxid'] = 'Nitav/di xat';
+$lang['AdminCompanyTaxes.basic.heading_tax_provider'] = 'Sgnittes %1$s';
+$lang['AdminCompanyTaxes.basic.field_addsubmit'] = 'Sgnittes etadpu';
+$lang['AdminCompanyTaxes.rules.page_title'] = 'Selur xat > sexat > ynapmoc > sgnittes';
+$lang['AdminCompanyTaxes.rules.no_results'] = 'Selur xat %1$s level on era ereht';
+$lang['AdminCompanyTaxes.rules.categorylink_addrule'] = 'Elur xat dda';
+$lang['AdminCompanyTaxes.rules.boxtitle_rules'] = 'Selur xat';
+$lang['AdminCompanyTaxes.rules.heading_level1'] = 'Selur 1 level';
+$lang['AdminCompanyTaxes.rules.heading_level2'] = 'Selur 2 level';
+$lang['AdminCompanyTaxes.rules.text_name'] = 'Eman';
+$lang['AdminCompanyTaxes.rules.text_type'] = 'Epyt';
+$lang['AdminCompanyTaxes.rules.text_amount'] = 'Tnuoma';
+$lang['AdminCompanyTaxes.rules.text_country'] = 'Yrtnuoc';
+$lang['AdminCompanyTaxes.rules.text_state'] = 'Ecnivorp/etats';
+$lang['AdminCompanyTaxes.rules.text_options'] = 'Snoitpo';
+$lang['AdminCompanyTaxes.rules.text_all'] = 'Lla';
+$lang['AdminCompanyTaxes.rules.option_edit'] = 'Tide';
+$lang['AdminCompanyTaxes.rules.option_delete'] = 'Eteled';
+$lang['AdminCompanyTaxes.rules.confirm_delete'] = '?elur xat siht eteled ot tnaw uoy erus uoy era';
+$lang['AdminCompanyTaxes.add.page_title'] = 'Elur xat dda > sexat > ynapmoc > sgnittes';
+$lang['AdminCompanyTaxes.add.boxtitle_add'] = 'Elur xat dda';
+$lang['AdminCompanyTaxes.add.field.type'] = 'Epyt xat';
+$lang['AdminCompanyTaxes.add.field.level'] = 'Level xat';
+$lang['AdminCompanyTaxes.add.field.level1'] = '1 level';
+$lang['AdminCompanyTaxes.add.field.level2'] = '2 level';
+$lang['AdminCompanyTaxes.add.field.name'] = 'Xat fo eman';
+$lang['AdminCompanyTaxes.add.field.amount'] = 'Tnuoma';
+$lang['AdminCompanyTaxes.add.field.country'] = 'Yrtnuoc';
+$lang['AdminCompanyTaxes.add.field.state'] = 'Ecnivorp/etats';
+$lang['AdminCompanyTaxes.add.field.addsubmit'] = 'Elur etaerc';
+$lang['AdminCompanyTaxes.edit.page_title'] = 'Elur xat tide > sexat > ynapmoc > sgnittes';
+$lang['AdminCompanyTaxes.edit.boxtitle_edit'] = 'Elur xat tide';
+$lang['AdminCompanyTaxes.edit.field.type'] = 'Epyt xat';
+$lang['AdminCompanyTaxes.edit.field.level'] = 'Level xat';
+$lang['AdminCompanyTaxes.edit.field.level1'] = '1 level';
+$lang['AdminCompanyTaxes.edit.field.level2'] = '2 level';
+$lang['AdminCompanyTaxes.edit.field.name'] = 'Xat fo eman';
+$lang['AdminCompanyTaxes.edit.field.amount'] = 'Tnuoma';
+$lang['AdminCompanyTaxes.edit.field.country'] = 'Yrtnuoc';
+$lang['AdminCompanyTaxes.edit.field.state'] = 'Ecnivorp/etats';
+$lang['AdminCompanyTaxes.edit.field.editsubmit'] = 'Elur tide';
+
