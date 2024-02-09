5363f40e85f7bf2b911df721f91e0857bbeb56d7
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:38 -0800

    Updated es_es language files

---
+++ language/es_es/email_verifications.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Email Verifications
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['EmailVerifications.!error.contact_id.exists'] = 'Di tcatnoc dilavni';
+$lang['EmailVerifications.!error.email.valid'] = 'Sserdda liame dilav a retne esaelp';
+$lang['EmailVerifications.!error.token.format'] = 'Nekot dilav a retne esaelp';
+$lang['EmailVerifications.!error.verified.valid'] = '"0" ro "1" eb tsum dleif deifirev eht';
+$lang['EmailVerifications.!error.redirect_url.valid'] = 'Lru dilav a retne esaelp';
+
