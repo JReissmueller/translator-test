cc1ad75c1f95b065091d700a05d0c513aea010b9
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:04:35 -0800

    Updated es_es language files

--- @es_es/components/gateways/nonmerchant/payumoney/language/es_es/payumoney.php
+++ @es_es/components/gateways/nonmerchant/payumoney/language/es_es/payumoney.php
@@ -9,21 +9,21 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Payumoney.name'] = 'Payumoney';
-$lang['Payumoney.description'] = 'En la India, PayU da servicio a más de 4.50.000 comercios con más de 100 métodos de pago y es el socio de pagos preferido por casi el 60% de los comercios electrónicos.';
-$lang['Payumoney.merchant_key'] = 'Clave de comerciante';
-$lang['Payumoney.merchant_salt'] = 'Sal mercantil';
-$lang['Payumoney.auth_header'] = 'Cabecera de autorización';
-$lang['Payumoney.auth_header_note'] = 'La cabecera de autorización se puede encontrar en el salpicadero de Payumoney en Gestionar cuenta -> Mi cuenta -> Clave de vendedor-Salt.';
-$lang['Payumoney.merchant_key_note'] = 'La clave de comerciante se puede encontrar en el salpicadero de Payumoney en Gestionar cuenta -> Mi cuenta -> Clave de comerciante-Salt.';
-$lang['Payumoney.merchant_salt_note'] = 'La sal de comerciante se puede encontrar en el salpicadero de Payumoney en Gestionar cuenta -> Mi cuenta -> Clave-Sal de comerciante.';
-$lang['Payumoney.test_mode'] = 'Modo Sandbox';
-$lang['Payumoney.test_mode_note'] = 'Habilitando esta opción utilizará el sandbox de Payumoney. Sólo active esta opción si está probando con una cuenta Payumoney sandbox.';
-$lang['Payumoney.buildprocess.submit'] = 'Pagar con Payumoney';
-$lang['Payumoney.!error.merchant_key.valid'] = 'Introduzca una clave de comerciante válida.';
-$lang['Payumoney.!error.merchant_salt.valid'] = 'Por favor, introduzca una Sal válida.';
-$lang['Payumoney.!error.auth_header.valid'] = 'Por favor, introduzca un encabezado de autorización válido.';
-$lang['Payumoney.!error.key.valid'] = 'Clave de comerciante no válida.';
-$lang['Payumoney.!error.invalid_token.response'] = 'Payumoney respondió con un token inválido.';
-$lang['Payumoney.!error.hash.valid'] = 'Hash no válido.';
+$lang['Payumoney.name'] = 'Yenomuyap';
+$lang['Payumoney.description'] = 'Stnahcrem ecremmoc-e eht fo %06 ylraen rof rentrap stnemyap derreferp eht si dna sdohtem tnemyap +001 revo htiw stnahcrem +000,05,4 naht erom sevres uyap ,aidni ni';
+$lang['Payumoney.merchant_key'] = 'Yek tnahcrem';
+$lang['Payumoney.merchant_salt'] = 'Tlas tnahcrem';
+$lang['Payumoney.auth_header'] = 'Redaeh noitazirohtua';
+$lang['Payumoney.auth_header_note'] = 'Tlas-yek tnahcrem >- tnuocca ym >- tnuocca eganam rednu draobhsad yenomuyap eht no dnuof eb nac redaeh noitazirohtua';
+$lang['Payumoney.merchant_key_note'] = 'Tlas-yek tnahcrem >- tnuocca ym >- tnuocca eganam rednu draobhsad yenomuyap eht no dnuof eb nac yek tnahcrem';
+$lang['Payumoney.merchant_salt_note'] = 'Tlas-yek tnahcrem >- tnuocca ym >- tnuocca eganam rednu draobhsad yenomuyap eht no dnuof eb nac tlas tnahcrem';
+$lang['Payumoney.test_mode'] = 'Edom xobdnas';
+$lang['Payumoney.test_mode_note'] = 'Tnuocca xobdnas yenomuyap a htiw gnitset era uoy fi noitpo siht elbane ylno .xobdnas yenomuyap eht ezilitu lliw noitpo siht gnilbane';
+$lang['Payumoney.buildprocess.submit'] = 'Yenomuyap aiv yap';
+$lang['Payumoney.!error.merchant_key.valid'] = 'Yek tnahcrem dilav a retne esaelp';
+$lang['Payumoney.!error.merchant_salt.valid'] = 'Tlas dilav a retne esaelp';
+$lang['Payumoney.!error.auth_header.valid'] = 'Redaeh noitazirohtua dilav a retne esaelp';
+$lang['Payumoney.!error.key.valid'] = 'Yek tnahcrem dilavni';
+$lang['Payumoney.!error.hash.valid'] = 'Hsah dilavni';
+$lang['Payumoney.!error.invalid_token.response'] = 'Nekot dilavni na htiw dednopser yenomuyap';
 
