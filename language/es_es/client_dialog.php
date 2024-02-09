f4ed4abf27eb95ab254f16fb2a15c2b550bb8695
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:18 -0800

    Updated es_es language files

---
+++ language/es_es/client_dialog.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Client Dialog
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDialog.confirm.text_yes'] = 'Sey';
+$lang['ClientDialog.confirm.text_no'] = 'On';
+$lang['ClientDialog.password.title'] = 'Drowssap etareneg';
+$lang['ClientDialog.password.field_modal_password'] = 'Drowssap';
+$lang['ClientDialog.password.field_modal_password_copied'] = 'Ecalp efas a ot drowssap siht deipoc evah i';
+$lang['ClientDialog.password.btn_generate'] = 'Etareneg';
+$lang['ClientDialog.password.btn_cancel'] = 'Lecnac';
+$lang['ClientDialog.password.btn_use_password'] = 'Drowssap esu';
+
