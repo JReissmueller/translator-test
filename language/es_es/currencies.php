97658d836906099cdc26301443a9a0522c329846
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:36 -0800

    Updated es_es language files

---
+++ language/es_es/currencies.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Currencies
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Currencies.!error.code.length'] = '7124 osi ni denifed sa sretcarahc ahpla 3 eb tsum edoc ycnerruc eht';
+$lang['Currencies.!error.code.exists'] = 'Nekat ydaerla si %2$s di ynapmoc dna %1$s edoc htiw ycnerruc a';
+$lang['Currencies.!error.company_id.exists'] = 'Di ynapmoc dilavni';
+$lang['Currencies.!error.format.format'] = 'Tamrof ycnerruc dilavni';
+$lang['Currencies.!error.precision.format'] = 'Evisulcni ,4 dna 0 neewteb rebmun a eb tsum noisicerp ycnerruc eht';
+$lang['Currencies.!error.prefix.length'] = 'Htgnel ni sretcarahc 01 deecxe ton yam xiferp ycnerruc eht';
+$lang['Currencies.!error.suffix.length'] = 'Htgnel ni sretcarahc 01 deecxe ton yam xiffus ycnerruc eht';
+$lang['Currencies.!error.exchange_rate.format'] = 'Rebmun a eb tsum etar egnahcxe eht';
+$lang['Currencies.!error.exchange_updated.format'] = 'Tamrof etad dilavni na ni si etad detadpu egnahcxe eht';
+$lang['Currencies.!error.currency_code.in_use'] = 'Deteled eb tonnac dna esu ni yltnerruc si %1$s ycnerruc eht';
+$lang['Currencies.!error.currency_code.is_default'] = 'Deteled eb tonnac dna ycnerruc tluafed eht si %1$s ycnerruc eht';
+$lang['Currencies.!error.processor.invalid'] = 'Rossecorp etar egnahcxe dilavni';
+$lang['Currencies.!error.processor.empty'] = 'Tes neeb sah rossecorp etar egnahcxe on';
+
