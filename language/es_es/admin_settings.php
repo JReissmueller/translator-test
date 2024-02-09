af4770c9e4e33692c6574f5c3c0321132caa4927
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:02 -0800

    Updated es_es language files

---
+++ language/es_es/admin_settings.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Admin Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSettings.company.page_title'] = 'Sgnittes';
+$lang['AdminSettings.company.category_company'] = 'Ynapmoc';
+$lang['AdminSettings.company.category_system'] = 'Metsys';
+$lang['AdminSettings.company.boxtitle_settings'] = 'Sgnittes';
+$lang['AdminSettings.company.heading_general'] = 'Lareneg';
+$lang['AdminSettings.company.text_general'] = 'Sepyt tcatnoc tneilc dna ,snoitpo noitazilanoitanretni ,noitazilacol erugifnoc';
+$lang['AdminSettings.company.heading_billing'] = 'Tnemyap/gnillib';
+$lang['AdminSettings.company.text_billing'] = 'Snopuoc dna ,seciton tnemyap ,stnemyap citamotua ,noitazimotsuc dna snoitpo eciovni erugifnoc';
+$lang['AdminSettings.company.heading_emails'] = 'Sliame';
+$lang['AdminSettings.company.text_emails'] = 'Serutangis liame eganam dna etaerc dna ,setalpmet liame tide ,sgnittes liam erugifnoc';
+$lang['AdminSettings.company.heading_messengers'] = 'Sregnessem';
+$lang['AdminSettings.company.text_messengers'] = 'Noitacinummoc dna snoitacifiton rof desu era hcihw ,sregnessem eganam dna llatsni';
+$lang['AdminSettings.company.heading_currencies'] = 'Seicnerruc';
+$lang['AdminSettings.company.text_currencies'] = 'Setar egnahcxe erugifnoc dna ,ycnerruc-itlum erugifnoc ,ycnerruc tluafed eht tes ,seicnerruc dda dna etadpu';
+$lang['AdminSettings.company.heading_clientgroups'] = 'Spuorg tneilc';
+$lang['AdminSettings.company.text_clientgroups'] = 'Sgnittes dna spuorg tneilc eganam dna etaerc .erom dna ,degrahc ,deciovni si tneilc taht nehw senimreted hcihw ,puorg a ot gnoleb stneilc lla';
+$lang['AdminSettings.company.heading_automation'] = 'Noitamotua';
+$lang['AdminSettings.company.text_automation'] = 'Atselb ni sksat detamotua lortnoc dna ssecca';
+$lang['AdminSettings.company.heading_modules'] = 'Seludom';
+$lang['AdminSettings.company.text_modules'] = 'Secivres fo tnemeganam dna gninoisivorp eht rof desu era hcihw ,seludom eganam dna llatsni';
+$lang['AdminSettings.company.heading_gateways'] = 'Syawetag tnemyap';
+$lang['AdminSettings.company.text_gateways'] = 'Tnemyap fo ecnatpecca eht rof srossecorp tnemyap tnahcrem-non dna tnahcrem elbasid dna elbane';
+$lang['AdminSettings.company.heading_taxes'] = 'Sexat';
+$lang['AdminSettings.company.text_taxes'] = 'Selur xat level-itlum ro elgnis etaerc dna ,nitav/di xat a tes ,xat elbasid ro elbane';
+$lang['AdminSettings.company.heading_clientoptions'] = 'Snoitpo tneilc';
+$lang['AdminSettings.company.text_clientoptions'] = 'Sdleif tneilc deriuqer tceles dna sdleif tneilc motsuc eganam dna etaerc';
+$lang['AdminSettings.company.heading_plugins'] = 'Snigulp';
+$lang['AdminSettings.company.text_plugins'] = 'Atselb fo ytilanoitcnuf eroc eht ezimotsuc dna dnetxe hcihw ,snigulp eganam dna ,llatsninu ,llatsni';
+$lang['AdminSettings.company.heading_lookandfeel'] = 'Leef dna kool';
+$lang['AdminSettings.company.text_lookandfeel'] = 'Etalpmet ecafretni tneilc eht gnitadpu ro ,semeht roloc gniganam yb atselb fo leef dna kool eht ezimotsuc';
+$lang['AdminSettings.company.heading_feeds'] = 'Sdeef atad';
+$lang['AdminSettings.company.text_feeds'] = 'Etisbew lanretxe na ni deddebme ylisae eb ot ,snoitpircsed dna seman ,gnicirp egakcap wolla sdeef atad';
+$lang['AdminSettings.system.page_title'] = 'Sgnittes';
+$lang['AdminSettings.system.category_company'] = 'Ynapmoc';
+$lang['AdminSettings.system.category_system'] = 'Metsys';
+$lang['AdminSettings.system.boxtitle_settings'] = 'Sgnittes';
+$lang['AdminSettings.system.heading_general'] = 'Lareneg';
+$lang['AdminSettings.system.text_general'] = 'Yek esnecil ruoy gnitadpu ro ,esabatad pioeg eht ,edom ecnanetniam gnilbane gnidulcni ,sgnittes ediw-metsys erugifnoc';
+$lang['AdminSettings.system.heading_companies'] = 'Seinapmoc';
+$lang['AdminSettings.system.text_companies'] = 'Seinapmoc etadpu ro dda';
+$lang['AdminSettings.system.heading_backup'] = 'Pukcab';
+$lang['AdminSettings.system.text_backup'] = 'Retupmoc lacol ruoy ot pukcab a daolnwod ro ,3s nozama ro ,ptfs gnisu spukcab citamotua erugifnoc';
+$lang['AdminSettings.system.heading_api'] = 'Ssecca ipa';
+$lang['AdminSettings.system.text_api'] = 'Ipa s\'noitallatsni siht ot ssecca lortnoc';
+$lang['AdminSettings.system.heading_marketplace'] = 'Ecalptekram';
+$lang['AdminSettings.system.text_marketplace'] = 'Ecalptekram atselb eht morf erom dna ,syawetag ,seludom ,snigulp daolnwod dna esworb';
+$lang['AdminSettings.system.heading_automation'] = 'Noitamotua';
+$lang['AdminSettings.system.text_automation'] = 'Noitamrofni ksat deludehcs ro norc ruoy enimreted';
+$lang['AdminSettings.system.heading_staff'] = 'Ffats';
+$lang['AdminSettings.system.text_staff'] = 'Aera nimda atselb eht ot ssecca evah ffats .snoissimrep dna stnuocca ffats yfidom ro etaerc';
+$lang['AdminSettings.system.heading_upgrade'] = 'Snoitpo edargpu';
+$lang['AdminSettings.system.text_upgrade'] = 'Sdaolnwod dna noitamrofni edargpu weiv';
+$lang['AdminSettings.system.heading_help'] = 'Pleh';
+$lang['AdminSettings.system.text_help'] = 'Atselb fo noitallatsni ruoy rof secruoser pleh weiv';
+
