c45a1d9cc8f98b8737da4234349e1ffb9a18dc86
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:27:01 -0800

    Updated es_es language files

--- plugins/client_documents/language/es_es/client_main.php
+++ plugins/client_documents/language/es_es/client_main.php
@@ -9,12 +9,12 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['ClientMain.index.page_title'] = 'Stnemucod';
-$lang['ClientMain.index.boxtitle_documents'] = 'Stnemucod';
-$lang['ClientMain.index.no_results'] = 'Daolnwod rof elbaliava stnemucod on era ereht';
-$lang['ClientMain.index.heading_name'] = 'Eman';
-$lang['ClientMain.index.heading_description'] = 'Noitpircsed';
-$lang['ClientMain.index.heading_date_added'] = 'Dedda etad';
-$lang['ClientMain.index.heading_options'] = 'Snoitpo';
-$lang['ClientMain.index.option_download'] = 'Daolnwod';
+$lang['ClientMain.index.page_title'] = 'Documentos';
+$lang['ClientMain.index.boxtitle_documents'] = 'Documentos';
+$lang['ClientMain.index.no_results'] = 'No hay documentos disponibles para descargar.';
+$lang['ClientMain.index.heading_name'] = 'Nombre';
+$lang['ClientMain.index.heading_description'] = 'Descripción';
+$lang['ClientMain.index.heading_date_added'] = 'Fecha de adición';
+$lang['ClientMain.index.heading_options'] = 'Opciones';
+$lang['ClientMain.index.option_download'] = 'Descargar';
 
