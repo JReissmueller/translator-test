f2b852bca108bab3e1a94633e19cc75f641b4e35
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:29 -0800

    Updated es_es language files

---
+++ components/reports/package_revenue/language/es_es/package_revenue.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Package Revenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageRevenue.name'] = 'Eunever egakcap';
+$lang['PackageRevenue.description'] = 'Egakcap eht htiw detaicossa smeti enil eciovni diap eht gnilatot yb egakcap hcae rof ycnerruc a morf eunever setaluclac';
+$lang['PackageRevenue.option.any'] = 'Yna';
+$lang['PackageRevenue.options.currency'] = 'Ycnerruc';
+$lang['PackageRevenue.options.field_start_date'] = 'Etad trats desolc eciovni';
+$lang['PackageRevenue.options.field_end_date'] = 'Etad dne desolc eciovni';
+$lang['PackageRevenue.heading.package_name'] = 'Egakcap';
+$lang['PackageRevenue.heading.status'] = 'Sutats';
+$lang['PackageRevenue.heading.module_name'] = 'Eludom';
+$lang['PackageRevenue.heading.service_count'] = 'Stinu';
+$lang['PackageRevenue.heading.package_revenue'] = 'Eunever';
+$lang['PackageRevenue.heading.tax'] = 'Xat';
+
