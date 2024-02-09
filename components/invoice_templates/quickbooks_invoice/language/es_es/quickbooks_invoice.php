13a653d47f56fc6c36a2c2d361de232ac3548b4a
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:43 -0800

    Updated es_es language files

---
+++ components/invoice_templates/quickbooks_invoice/language/es_es/quickbooks_invoice.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Quickbooks Invoice
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['QuickbooksInvoice.name'] = 'Skoobkciuq';
+$lang['QuickbooksInvoice.page_of'] = '%2$s fo %1$s egap';
+$lang['QuickbooksInvoice.terms_heading'] = 'Smret';
+$lang['QuickbooksInvoice.type_active'] = 'Eciovni';
+$lang['QuickbooksInvoice.type_draft'] = 'Tfard - eciovni';
+$lang['QuickbooksInvoice.type_void'] = 'Diov - eciovni';
+$lang['QuickbooksInvoice.type_proforma'] = 'Eciovni amrof orp';
+$lang['QuickbooksInvoice.type_quotation'] = 'Noitatouq';
+$lang['QuickbooksInvoice.type_quotation_draft'] = 'Tfard noitatouq';
+$lang['QuickbooksInvoice.invoice_id_code'] = 'On eciovni';
+$lang['QuickbooksInvoice.proforma_id_code'] = 'On amrof orp';
+$lang['QuickbooksInvoice.draft_id_code'] = 'On tfard';
+$lang['QuickbooksInvoice.quotation_id_code'] = 'On noitatouq';
+$lang['QuickbooksInvoice.client_id_code'] = 'On remotsuc';
+$lang['QuickbooksInvoice.date_created'] = 'Etad noitaerc';
+$lang['QuickbooksInvoice.date_billed'] = 'Etad';
+$lang['QuickbooksInvoice.date_expires'] = 'Etad noitaripxe';
+$lang['QuickbooksInvoice.date_due'] = 'Etad eud';
+$lang['QuickbooksInvoice.notes_heading'] = 'Seton';
+$lang['QuickbooksInvoice.subtotal_heading'] = 'Latotbus';
+$lang['QuickbooksInvoice.total_heading'] = 'Latot';
+$lang['QuickbooksInvoice.lines_description'] = 'Noitpircsed';
+$lang['QuickbooksInvoice.lines_quantity'] = 'Ytitnauq';
+$lang['QuickbooksInvoice.lines_unit_price'] = 'Ecirp tinu';
+$lang['QuickbooksInvoice.lines_cost'] = 'Tsoc';
+$lang['QuickbooksInvoice.address_heading'] = 'Ot llib';
+$lang['QuickbooksInvoice.address_city_state'] = '%4$s %3$s %2$s ,%1$s';
+$lang['QuickbooksInvoice.tax_id'] = '%1$s :nitav/di xat';
+$lang['QuickbooksInvoice.client_tax_id'] = '%1$s :nitav/di xat remotsuc';
+$lang['QuickbooksInvoice.watermark_paid'] = 'Diap';
+$lang['QuickbooksInvoice.payments_heading'] = 'Stnemyap';
+$lang['QuickbooksInvoice.payments_applied_date'] = 'Deilppa etad';
+$lang['QuickbooksInvoice.payments_type_name'] = 'Epyt';
+$lang['QuickbooksInvoice.payments_transaction_id'] = 'Rebmun noitcasnart';
+$lang['QuickbooksInvoice.payments_applied_amount'] = 'Deilppa tnuoma';
+$lang['QuickbooksInvoice.balance_heading'] = 'Ecnalab';
+
