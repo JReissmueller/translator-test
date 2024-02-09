f3c5a0d5f0d4be1f178c8597ad337b8b62098f94
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:34 -0800

    Updated es_es language files

---
+++ language/es_es/_custom.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Custom
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['_PaymentTypes.cash'] = 'Hsac';
+$lang['_PaymentTypes.check'] = 'Kcehc';
+$lang['_PaymentTypes.money_order'] = 'Redro yenom';
+$lang['_PaymentTypes.in_house_credit'] = 'Tiderc esuoh ni';
+
