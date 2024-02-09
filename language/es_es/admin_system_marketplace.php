46af9c269320910d1f50511882663ab33161c3bd
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:08 -0800

    Updated es_es language files

---
+++ language/es_es/admin_system_marketplace.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Admin System Marketplace
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemMarketplace.index.page_title'] = 'Ecalptekram > metsys > sgnittes';
+$lang['AdminSystemMarketplace.index.boxtitle_marketplace'] = 'Ecalptekram';
+$lang['AdminSystem.marketplace.category_all'] = 'Lla';
+$lang['AdminSystemMarketplace.index.text_unavailable'] = 'Elbaliavanu yltnerruc si ecalptekram eht';
+$lang['AdminSystemMarketplace.listing.boxtitle_listing'] = '%1$s noisnetxe';
+
