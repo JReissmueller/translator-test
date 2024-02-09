1611f07fbb82784e0c1f7e727052defc160569bd
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:07:11 -0800

    Updated es_es language files

--- @es_es/language/es_es/client_quotations.php
+++ @es_es/language/es_es/client_quotations.php
@@ -9,30 +9,30 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['ClientQuotations.!error.password_mismatch'] = 'La contraseña no coincide con la contraseña de inicio de sesión.';
-$lang['ClientQuotations.!success.approved_quotation'] = 'El presupuesto se ha aprobado correctamente.';
-$lang['ClientQuotations.index.page_title'] = 'Cliente #%1$s Presupuestos';
-$lang['ClientQuotations.index.category_pending'] = 'Pendiente';
-$lang['ClientQuotations.index.category_approved'] = 'Aprobado';
-$lang['ClientQuotations.index.category_expired'] = 'Caducado';
-$lang['ClientQuotations.index.boxtitle_quotations'] = 'Citas';
-$lang['ClientQuotations.index.heading_quotation'] = 'Presupuesto';
-$lang['ClientQuotations.index.heading_title'] = 'Título';
-$lang['ClientQuotations.index.heading_subtotal'] = 'Subtotal';
-$lang['ClientQuotations.index.heading_total'] = 'Importe';
-$lang['ClientQuotations.index.heading_date_created'] = 'Fecha de creación';
-$lang['ClientQuotations.index.heading_date_expires'] = 'Fecha de expiración';
-$lang['ClientQuotations.index.heading_options'] = 'Opciones';
-$lang['ClientQuotations.index.option_view'] = 'Ver';
-$lang['ClientQuotations.index.option_approve'] = 'Aprobar';
-$lang['ClientQuotations.index.no_results'] = 'No tiene %1$s Citas.';
-$lang['ClientQuotations.approve.heading_approve'] = 'Aprobar';
-$lang['ClientQuotations.approve.field_password'] = 'Iniciar sesión Contraseña para aprobar';
-$lang['ClientQuotations.approve.field_cancel'] = 'Cancelar, no aprobar';
-$lang['ClientQuotations.approve.field_submit'] = 'Aprobar';
-$lang['ClientQuotations.invoices.heading_invoice'] = 'Factura #';
-$lang['ClientQuotations.invoices.heading_amount'] = 'Importe';
-$lang['ClientQuotations.invoices.heading_paid'] = 'Pagado';
-$lang['ClientQuotations.invoices.heading_date_billed'] = 'Fecha de facturación';
-$lang['ClientQuotations.invoices.no_results'] = 'Este presupuesto no tiene facturas asociadas.';
+$lang['ClientQuotations.!error.password_mismatch'] = 'Drowssap ni gol ruoy hctam ton seod drowssap eht';
+$lang['ClientQuotations.!success.approved_quotation'] = 'Yllufsseccus devorppa neeb sah noitatouq eht';
+$lang['ClientQuotations.index.page_title'] = 'Snoitatouq %1$s# tneilc';
+$lang['ClientQuotations.index.category_pending'] = 'Gnidnep';
+$lang['ClientQuotations.index.category_approved'] = 'Devorppa';
+$lang['ClientQuotations.index.category_expired'] = 'Deripxe';
+$lang['ClientQuotations.index.boxtitle_quotations'] = 'Snoitatouq';
+$lang['ClientQuotations.index.heading_quotation'] = '# noitatouq';
+$lang['ClientQuotations.index.heading_title'] = 'Eltit';
+$lang['ClientQuotations.index.heading_subtotal'] = 'Latotbus';
+$lang['ClientQuotations.index.heading_total'] = 'Tnuoma';
+$lang['ClientQuotations.index.heading_date_created'] = 'Detaerc etad';
+$lang['ClientQuotations.index.heading_date_expires'] = 'Seripxe etad';
+$lang['ClientQuotations.index.heading_options'] = 'Snoitpo';
+$lang['ClientQuotations.index.option_view'] = 'Weiv';
+$lang['ClientQuotations.index.option_approve'] = 'Evorppa';
+$lang['ClientQuotations.index.no_results'] = 'Snoitatouq %1$s on evah uoy';
+$lang['ClientQuotations.approve.heading_approve'] = 'Evorppa';
+$lang['ClientQuotations.approve.field_password'] = 'Evorppa ot drowssap ni gol';
+$lang['ClientQuotations.approve.field_cancel'] = 'Evorppa ton od ,lecnac';
+$lang['ClientQuotations.approve.field_submit'] = 'Evorppa';
+$lang['ClientQuotations.invoices.heading_invoice'] = '# eciovni';
+$lang['ClientQuotations.invoices.heading_amount'] = 'Tnuoma';
+$lang['ClientQuotations.invoices.heading_paid'] = 'Diap';
+$lang['ClientQuotations.invoices.heading_date_billed'] = 'Dellib etad';
+$lang['ClientQuotations.invoices.no_results'] = 'Ti ot detaicossa seciovni on sah noitatouq siht';
 
