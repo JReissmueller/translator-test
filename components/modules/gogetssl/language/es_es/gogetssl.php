fa5091391216f94b2eb1f6e7411696d673bd1269
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:55 -0800

    Updated es_es language files

---
+++ components/modules/gogetssl/language/es_es/gogetssl.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Gogetssl
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GoGetSSL.!error.api_username.empty'] = 'Emanresu ipa na retne esaelp';
+$lang['GoGetSSL.!error.api_username.valid'] = 'Tcerroc era slaitnederc ipa eht taht erusne ot kcehc esaelp .dehsilbatse eb ton dluoc revres eht ot noitcennoc a';
+$lang['GoGetSSL.!error.api_password.empty'] = 'Drowssap ipa na retne esaelp';
+$lang['GoGetSSL.!error.gogetssl_name.empty'] = 'Eman relleser a retne esaelp';
+$lang['GoGetSSL.!error.gogetssl_validity.empty'] = 'Doirep ytidilav a retne esaelp';
+$lang['GoGetSSL.!error.gogetssl_approver_email.format'] = 'Liam-e revorppa dilav a retne esaelp';
+$lang['GoGetSSL.!error.gogetssl_csr.format'] = 'Rsc eulav a retne esaelp';
+$lang['GoGetSSL.!error.gogetssl_fqdn.format'] = 'Niamod dilav a retne esaelp';
+$lang['GoGetSSL.!error.gogetssl_webserver_type.format'] = 'Epyt revresbew dilav a retne esaelp';
+$lang['GoGetSSL.!error.meta[product].valid'] = 'Tcudorp a tceles esaelp';
+$lang['GoGetSSL.!error.meta[gogetssl_name].empty'] = 'Relleser eno tsael ta tceles esaelp';
+$lang['GoGetSSL.!error.api.internal'] = 'Tseuqer eht ot dnopser ton did revres eht ro ,derrucco rorre lanretni na';
+$lang['GoGetSSL.please_select'] = '-- tceles esaelp --';
+$lang['GoGetSSL.name'] = 'Lsstegog';
+$lang['GoGetSSL.description'] = '9002 ecnis setacifitrec lss gnivres neeb sah lsstegog';
+$lang['GoGetSSL.module_row'] = 'Relleser lsstegog';
+$lang['GoGetSSL.module_row_plural'] = 'Srelleser';
+$lang['GoGetSSL.add_module_row'] = 'Relleser dda';
+$lang['GoGetSSL.manage.module_rows_title'] = 'Srelleser lsstegog';
+$lang['GoGetSSL.manage.module_rows.edit'] = 'Tide';
+$lang['GoGetSSL.manage.module_rows.delete'] = 'Eteled';
+$lang['GoGetSSL.manage.module_rows.confirm_delete'] = '?relleser siht eteled ot tnaw uoy erus uoy era';
+$lang['GoGetSSL.manage.module_rows_no_results'] = 'Srelleser on era ereht';
+$lang['GoGetSSL.manage.module_rows_heading.gogetssl_name'] = 'Eman relleser';
+$lang['GoGetSSL.manage.module_rows_heading.options'] = 'Snoitpo';
+$lang['GoGetSSL.row_meta.gogetssl_name'] = 'Eman relleser';
+$lang['GoGetSSL.row_meta.api_username'] = 'Emanresu ipa';
+$lang['GoGetSSL.row_meta.api_password'] = 'Drowssap ipa';
+$lang['GoGetSSL.row_meta.sandbox'] = 'Xobdnas';
+$lang['GoGetSSL.add_row.box_title'] = 'Relleser lsstegog dda';
+$lang['GoGetSSL.add_row.basic_title'] = 'Sgnittes cisab';
+$lang['GoGetSSL.add_row.add_btn'] = 'Relleser dda';
+$lang['GoGetSSL.edit_row.box_title'] = 'Relleser lsstegog tide';
+$lang['GoGetSSL.edit_row.basic_title'] = 'Sgnittes cisab';
+$lang['GoGetSSL.edit_row.add_btn'] = 'Relleser etadpu';
+$lang['GoGetSSL.package_fields.product'] = 'Tcudorp';
+$lang['GoGetSSL.service_field.gogetssl_fqdn'] = 'Niamod / tsoh';
+$lang['GoGetSSL.service_field.gogetssl_csr'] = 'Tnetnoc rsc';
+$lang['GoGetSSL.service_field.gogetssl_webserver_type'] = 'Epyt revresbew';
+$lang['GoGetSSL.service_field.gogetssl_approver_email'] = 'Liam-e revorppa';
+$lang['GoGetSSL.service_field.gogetssl_title'] = 'Eltit';
+$lang['GoGetSSL.service_field.gogetssl_firstname'] = 'Eman tsrif';
+$lang['GoGetSSL.service_field.gogetssl_lastname'] = 'Eman tsal';
+$lang['GoGetSSL.service_field.gogetssl_address1'] = '1 enil sserdda';
+$lang['GoGetSSL.service_field.gogetssl_address2'] = '2 enil sserdda';
+$lang['GoGetSSL.service_field.gogetssl_city'] = 'Ytic';
+$lang['GoGetSSL.service_field.gogetssl_zip'] = 'Piz';
+$lang['GoGetSSL.service_field.gogetssl_state'] = 'Etats';
+$lang['GoGetSSL.service_field.gogetssl_country'] = 'Yrtnuoc';
+$lang['GoGetSSL.service_field.gogetssl_email'] = 'Liam-e';
+$lang['GoGetSSL.service_field.gogetssl_number'] = ')tamrof lanoitanretni( rebmun enohpelet';
+$lang['GoGetSSL.service_field.gogetssl_organization'] = ')elbacilppa ton fi an retne( noitazinagro';
+$lang['GoGetSSL.service_field.gogetssl_organization_unit'] = ')elbacilppa ton fi an retne( tinu noitazinagro';
+$lang['GoGetSSL.service_field.gogetssl_fax'] = ')tamrof lanoitanretni( rebmun xaf';
+$lang['GoGetSSL.tab_reissue'] = 'Eussier';
+$lang['GoGetSSL.tab_reissue.heading_reissue'] = 'Eussier';
+
