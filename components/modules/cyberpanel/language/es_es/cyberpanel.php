c7e6a379306d595f9a95ca3f7897469b261b1569
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:51 -0800

    Updated es_es language files

---
+++ components/modules/cyberpanel/language/es_es/cyberpanel.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Cyberpanel
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cyberpanel.name'] = 'Lenaprebyc';
+$lang['Cyberpanel.description'] = 'Ytilibailer dna ,ytiruces ,deeps rof tliub .deepsetilnepo yb derewop lenap lortnoc gnitsoh noitareneg txen a si lenaprebyc';
+$lang['Cyberpanel.module_row'] = 'Revres';
+$lang['Cyberpanel.module_row_plural'] = 'Srevres';
+$lang['Cyberpanel.module_group'] = 'Puorg revres';
+$lang['Cyberpanel.tab_client_actions'] = 'Snoitca';
+$lang['Cyberpanel.add_module_row'] = 'Revres dda';
+$lang['Cyberpanel.add_module_group'] = 'Puorg revres dda';
+$lang['Cyberpanel.manage.module_rows_title'] = 'Srevres';
+$lang['Cyberpanel.manage.module_groups_title'] = 'Spuorg revres';
+$lang['Cyberpanel.manage.module_rows_heading.name'] = 'Lebal revres';
+$lang['Cyberpanel.manage.module_rows_heading.hostname'] = 'Emantsoh';
+$lang['Cyberpanel.manage.module_rows_heading.options'] = 'Snoitpo';
+$lang['Cyberpanel.manage.module_groups_heading.name'] = 'Eman puorg';
+$lang['Cyberpanel.manage.module_groups_heading.servers'] = 'Tnuoc revres';
+$lang['Cyberpanel.manage.module_groups_heading.options'] = 'Snoitpo';
+$lang['Cyberpanel.manage.module_rows.edit'] = 'Tide';
+$lang['Cyberpanel.manage.module_groups.edit'] = 'Tide';
+$lang['Cyberpanel.manage.module_rows.delete'] = 'Eteled';
+$lang['Cyberpanel.manage.module_groups.delete'] = 'Eteled';
+$lang['Cyberpanel.manage.module_rows.confirm_delete'] = '?revres siht eteled ot tnaw uoy erus uoy era';
+$lang['Cyberpanel.manage.module_groups.confirm_delete'] = '?puorg revres siht eteled ot tnaw uoy erus uoy era';
+$lang['Cyberpanel.manage.module_rows_no_results'] = 'Srevres on era ereht';
+$lang['Cyberpanel.manage.module_groups_no_results'] = 'Spuorg revres on era ereht';
+$lang['Cyberpanel.order_options.first'] = 'Revres lluf-non tsrif';
+$lang['Cyberpanel.order_options.roundrobin'] = 'Srevres gnoma etubirtsid ylneve';
+$lang['Cyberpanel.add_row.box_title'] = 'Revres lenaprebyc dda';
+$lang['Cyberpanel.add_row.basic_title'] = 'Sgnittes cisab';
+$lang['Cyberpanel.add_row.name_servers_title'] = 'Srevres eman';
+$lang['Cyberpanel.add_row.notes_title'] = 'Seton';
+$lang['Cyberpanel.add_row.name_server_btn'] = 'Revres eman lanoitidda dda';
+$lang['Cyberpanel.add_row.name_server_col'] = 'Revres eman';
+$lang['Cyberpanel.add_row.name_server_host_col'] = 'Emantsoh';
+$lang['Cyberpanel.add_row.name_server'] = '%1$s revres eman';
+$lang['Cyberpanel.add_row.remove_name_server'] = 'Evomer';
+$lang['Cyberpanel.add_row.add_btn'] = 'Revres dda';
+$lang['Cyberpanel.edit_row.box_title'] = 'Revres lenaprebyc tide';
+$lang['Cyberpanel.edit_row.basic_title'] = 'Sgnittes cisab';
+$lang['Cyberpanel.edit_row.name_servers_title'] = 'Srevres eman';
+$lang['Cyberpanel.edit_row.notes_title'] = 'Seton';
+$lang['Cyberpanel.edit_row.name_server_btn'] = 'Revres eman lanoitidda dda';
+$lang['Cyberpanel.edit_row.name_server_col'] = 'Revres eman';
+$lang['Cyberpanel.edit_row.name_server_host_col'] = 'Emantsoh';
+$lang['Cyberpanel.edit_row.name_server'] = '%1$s revres eman';
+$lang['Cyberpanel.edit_row.remove_name_server'] = 'Evomer';
+$lang['Cyberpanel.edit_row.add_btn'] = 'Revres tide';
+$lang['Cyberpanel.row_meta.server_name'] = 'Lebal revres';
+$lang['Cyberpanel.row_meta.host_name'] = 'Emantsoh';
+$lang['Cyberpanel.row_meta.admin_username'] = 'Emanresu nimda';
+$lang['Cyberpanel.row_meta.admin_password'] = 'Drowssap nimda';
+$lang['Cyberpanel.row_meta.use_ssl'] = ')dednemmocer( ipa eht ot gnitcennoc nehw lss esu';
+$lang['Cyberpanel.package_fields.package'] = 'Eman egakcap';
+$lang['Cyberpanel.service_field.domain'] = 'Niamod';
+$lang['Cyberpanel.service_field.username'] = 'Emanresu';
+$lang['Cyberpanel.service_field.password'] = 'Drowssap';
+$lang['Cyberpanel.tab_client_actions.change_password'] = 'Drowssap egnahc';
+$lang['Cyberpanel.tab_client_actions.field_cyberpanel_password'] = 'Drowssap';
+$lang['Cyberpanel.tab_client_actions.field_cyberpanel_confirm_password'] = 'Drowssap mrifnoc';
+$lang['Cyberpanel.tab_client_actions.field_password_submit'] = 'Drowssap etadpu';
+$lang['Cyberpanel.service_info.username'] = 'Emanresu';
+$lang['Cyberpanel.service_info.password'] = 'Drowssap';
+$lang['Cyberpanel.service_info.server'] = 'Revres';
+$lang['Cyberpanel.service_info.options'] = 'Snoitpo';
+$lang['Cyberpanel.service_info.option_login'] = 'Ni gol';
+$lang['Cyberpanel.service_field.tooltip.username'] = 'Eno etareneg yllacitamotua ot knalb emanresu eht evael yam uoy';
+$lang['Cyberpanel.service_field.tooltip.password'] = 'Eno etareneg yllacitamotua ot knalb drowssap eht evael yam uoy';
+$lang['Cyberpanel.!error.server_name_valid'] = 'Lebal revres a retne tsum uoy';
+$lang['Cyberpanel.!error.host_name_valid'] = 'Dilavni eb ot sraeppa emantsoh eht';
+$lang['Cyberpanel.!error.remote_admin_username_valid'] = 'Dilavni eb ot sraeppa emanresu nimda eht';
+$lang['Cyberpanel.!error.remote_admin_password_valid'] = 'Dilavni eb ot sraeppa drowssap nimda eht';
+$lang['Cyberpanel.!error.remote_admin_password_valid_connection'] = 'Tcerroc era drowssap nimda eht dna emantsoh eht taht erusne ot kcehc esaelp .dehsilbatse eb ton dluoc revres eht ot noitcennoc a';
+$lang['Cyberpanel.!error.name_servers_valid'] = 'Dilavni era deretne srevres eman eht fo erom ro eno';
+$lang['Cyberpanel.!error.name_servers_count'] = 'Srevres eman 2 tsael ta enifed tsum uoy';
+$lang['Cyberpanel.!error.meta[package].empty'] = 'Deriuqer si egakcap lenaprebyc a';
+$lang['Cyberpanel.!error.api.internal'] = 'Tseuqer eht ot dnopser ton did revres eht ro ,derrucco rorre lanretni na';
+$lang['Cyberpanel.!error.module_row.missing'] = 'Elbaliavanu si wor eludom eht .derrucco rorre lanretni na';
+$lang['Cyberpanel.!error.cyberpanel_domain.format'] = 'Moc.niamod .g.e ,eman niamod dilav a retne esaelp';
+$lang['Cyberpanel.!error.cyberpanel_domain.test'] = '\'tset\' htiw trats ton nac eman niamod';
+$lang['Cyberpanel.!error.cyberpanel_username.format'] = 'Rebmun a htiw trats ton yam dna srebmun dna srettel ylno niatnoc yam emanresu eht';
+$lang['Cyberpanel.!error.cyberpanel_username.test'] = '\'tset\' htiw nigeb ton yam emanresu eht';
+$lang['Cyberpanel.!error.cyberpanel_username.length'] = 'Htgnel ni sretcarahc 61 dna 1 neewteb eb tsum emanresu eht';
+$lang['Cyberpanel.!error.cyberpanel_password.valid'] = 'Htgnel ni sretcarahc 8 tsael ta eb tsum drowssap';
+$lang['Cyberpanel.!error.cyberpanel_password.matches'] = 'Hctam ton od drowssap mrifnoc dna drowssap';
+
