068b04e407814c2a2e1888bb3f18acf01ccab6be
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:08:02 -0800

    Updated es_es language files

--- @es_es/language/es_es/pricings.php
+++ @es_es/language/es_es/pricings.php
@@ -9,28 +9,28 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Pricings.!error.term.format'] = 'El término debe ser un número.';
-$lang['Pricings.!error.term.length'] = 'La longitud del término no puede superar los 5 caracteres.';
-$lang['Pricings.!error.term.valid'] = 'El término debe ser mayor que 0.';
-$lang['Pricings.!error.period.format'] = 'Tipo de período no válido.';
-$lang['Pricings.!error.price.format'] = 'El precio debe ser un número.';
-$lang['Pricings.!error.price_renews.format'] = 'El precio de renovación debe ser un número.';
-$lang['Pricings.!error.price_renews.valid'] = 'El precio de renovación no puede fijarse para un periodo de tiempo único.';
-$lang['Pricings.!error.price_transfer.format'] = 'El precio de renovación debe ser un número.';
-$lang['Pricings.!error.setup_fee.format'] = 'La cuota de instalación debe ser un número.';
-$lang['Pricings.!error.cancel_fee.format'] = 'La tasa de cancelación debe ser un número.';
-$lang['Pricings.!error.currency.format'] = 'El código de moneda debe tener 3 caracteres.';
-$lang['Pricings.!error.term.in_use'] = 'El término no puede actualizarse en una tarificación que esté en uso.';
-$lang['Pricings.!error.period.in_use'] = 'El periodo no puede actualizarse en una tarificación que esté en uso.';
-$lang['Pricings.!error.currency.in_use'] = 'La moneda no puede actualizarse en una tarificación que esté en uso.';
-$lang['Pricings.getPeriods.day'] = 'Día';
-$lang['Pricings.getPeriods.week'] = 'Semana';
-$lang['Pricings.getPeriods.month'] = 'Mes';
-$lang['Pricings.getPeriods.year'] = 'Año';
-$lang['Pricings.getPeriods.onetime'] = 'Una vez';
-$lang['Pricings.getPeriods.day_plural'] = 'Días';
-$lang['Pricings.getPeriods.week_plural'] = 'Semanas';
-$lang['Pricings.getPeriods.month_plural'] = 'Meses';
-$lang['Pricings.getPeriods.year_plural'] = 'Años';
-$lang['Pricings.getPeriods.onetime_plural'] = 'Una vez';
+$lang['Pricings.!error.term.format'] = 'Rebmun a eb tsum mret';
+$lang['Pricings.!error.term.length'] = 'Sretcarahc 5 deecxe ton yam htgnel mret';
+$lang['Pricings.!error.term.valid'] = '0 naht retaerg eb tsum mret eht';
+$lang['Pricings.!error.period.format'] = 'Epyt doirep dilavni';
+$lang['Pricings.!error.price.format'] = 'Rebmun a eb tsum ecirp';
+$lang['Pricings.!error.price_renews.format'] = 'Rebmun a eb tsum ecirp lawener';
+$lang['Pricings.!error.price_renews.valid'] = 'Doirep emit eno a rof tes eb tonnac ecirp lawener';
+$lang['Pricings.!error.price_transfer.format'] = 'Rebmun a eb tsum ecirp lawener';
+$lang['Pricings.!error.setup_fee.format'] = 'Rebmun a eb tsum eef putes';
+$lang['Pricings.!error.cancel_fee.format'] = 'Rebmun a eb tsum eef lecnac';
+$lang['Pricings.!error.currency.format'] = 'Sretcarahc 3 eb tsum edoc ycnerruc';
+$lang['Pricings.!error.term.in_use'] = 'Esu ni si taht gnicirp a ni detadpu eb tonnac mret';
+$lang['Pricings.!error.period.in_use'] = 'Esu ni si taht gnicirp a ni detadpu eb tonnac doirep';
+$lang['Pricings.!error.currency.in_use'] = 'Esu ni si taht gnicirp a ni detadpu eb tonnac ycnerruc';
+$lang['Pricings.getPeriods.day'] = 'Yad';
+$lang['Pricings.getPeriods.week'] = 'Keew';
+$lang['Pricings.getPeriods.month'] = 'Htnom';
+$lang['Pricings.getPeriods.year'] = 'Raey';
+$lang['Pricings.getPeriods.onetime'] = 'Emit eno';
+$lang['Pricings.getPeriods.day_plural'] = 'Syad';
+$lang['Pricings.getPeriods.week_plural'] = 'Skeew';
+$lang['Pricings.getPeriods.month_plural'] = 'Shtnom';
+$lang['Pricings.getPeriods.year_plural'] = 'Sraey';
+$lang['Pricings.getPeriods.onetime_plural'] = 'Emit eno';
 
