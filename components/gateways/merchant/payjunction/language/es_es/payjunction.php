523b0fcf1a68a4f5283b5547e1ba49bf5fab075d
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:15 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/payjunction/language/es_es/payjunction.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Payjunction
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PayJunction.name'] = 'Noitcnujyap';
+$lang['PayJunction.description'] = 'Emulov drac tiderc ylhtnom ni erom ro 000,01$ gnissecorp sessenisub tnemtraped-itlum dna noitacol-itlum rof tif laedi na si ygolonhcet sti .yldipar gniworg si dna yllaunna noillib 4$ sessecorp noitcnujyap';
+$lang['PayJunction.api_key'] = 'Yek ipa';
+$lang['PayJunction.user_name'] = 'Emanresu';
+$lang['PayJunction.password'] = 'Drowssap';
+$lang['PayJunction.test_mode'] = 'Edom repoleved';
+$lang['PayJunction.api_key_note'] = 'Tnuocca noitcnujyap ruoy morf yek ipa eht retne';
+$lang['PayJunction.user_name_note'] = 'Emanresu tnuocca noitcnujyap ruoy retne';
+$lang['PayJunction.password_note'] = 'Drowssap tnuocca noitcnujyap ruoy retne';
+$lang['PayJunction.test_mode_note'] = 'Edom repoleved esu ot ,yek ipa sbal ruoy sa llew sa ,drowssap dna emanresu omed eht esu tsum uoy .tnemnorivne omed sbal noitcnujyap eht ot snoitcasnart tsop lliw edom repoleved gnilbane';
+$lang['PayJunction.!error.api_key.empty'] = 'Yek ipa dilav a retne esaelp';
+$lang['PayJunction.!error.user_name.empty'] = 'Emanresu tnuocca ruoy retne esaelp';
+$lang['PayJunction.!error.password.empty'] = 'Drowssap tnuocca ruoy retne esaelp';
+$lang['PayJunction.!error.test_mode.valid'] = 'Nevig fi \'eurt\' ot tes eb tsum edom repoleved';
+
