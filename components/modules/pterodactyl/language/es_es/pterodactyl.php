cc5f74752087e4ff9e92d567603c12b155eb76fb
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:11 -0800

    Updated es_es language files

---
+++ components/modules/pterodactyl/language/es_es/pterodactyl.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Pterodactyl
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pterodactyl.name'] = 'Lytcadoretp';
+$lang['Pterodactyl.description'] = 'Lenap lytcadoretp eht aiv srevres emag seganam dna snoisivorp';
+$lang['Pterodactyl.module_row'] = 'Revres';
+$lang['Pterodactyl.module_row_plural'] = 'Srevres';
+$lang['Pterodactyl.module_group'] = 'Puorg revres';
+$lang['Pterodactyl.!error.server_name.empty'] = 'Eman revres a retne tsum uoy';
+$lang['Pterodactyl.!error.host_name.valid'] = 'Dilavni eb ot sraeppa emantsoh eht';
+$lang['Pterodactyl.!error.account_api_key.empty'] = 'Yek ipa tnuocca na retne tsum uoy';
+$lang['Pterodactyl.!error.account_api_key.valid'] = 'Yek ipa tnuocca dna lru nevig eht htiw tcennoc ot elbanu';
+$lang['Pterodactyl.!error.application_api_key.empty'] = 'Yek ipa noitacilppa na retne tsum uoy';
+$lang['Pterodactyl.!error.application_api_key.valid'] = 'Yek ipa noitacilppa dna lru nevig eht htiw tcennoc ot elbanu';
+$lang['Pterodactyl.!error.module_row.missing'] = 'Elbaliavanu si wor eludom eht .derrucco rorre lanretni na';
+$lang['Pterodactyl.!success.start'] = 'Gnitoob won si revres eht';
+$lang['Pterodactyl.!success.stop'] = 'Nwod gnittuhs won si revres eht';
+$lang['Pterodactyl.!success.restart'] = 'Gnitoober won si revres eht';
+$lang['Pterodactyl.add_row.box_title'] = 'Revres lytcadoretp dda';
+$lang['Pterodactyl.add_row.basic_title'] = 'Sgnittes cisab';
+$lang['Pterodactyl.add_row.add_btn'] = 'Revres dda';
+$lang['Pterodactyl.edit_row.box_title'] = 'Revres lytcadoretp tide';
+$lang['Pterodactyl.edit_row.basic_title'] = 'Sgnittes cisab';
+$lang['Pterodactyl.edit_row.add_btn'] = 'Revres tide';
+$lang['Pterodactyl.row_meta.server_name'] = 'Lebal revres';
+$lang['Pterodactyl.row_meta.host_name'] = 'Emantsoh';
+$lang['Pterodactyl.row_meta.use_ssl'] = ')dednemmocer( ipa eht ot gnitcennoc nehw lss esu';
+$lang['Pterodactyl.row_meta.account_api_key'] = 'Yek ipa tnuocca';
+$lang['Pterodactyl.row_meta.application_api_key'] = 'Yek ipa noitacilppa';
+$lang['Pterodactyl.!tooltip.host_name'] = 'Moc.niamod.lenap .g.e .lenap lytcadoretp ruoy fo pi ro emantsoh eht retne';
+$lang['Pterodactyl.!tooltip.account_api_key'] = 'Lenap lytcadoretp ruoy rof detareneg yek ipa tnuocca eht retne';
+$lang['Pterodactyl.!tooltip.application_api_key'] = 'Lenap lytcadoretp ruoy rof detareneg yek ipa noitacilppa eht retne';
+$lang['Pterodactyl.order_options.first'] = 'Revres lluf-non tsrif';
+$lang['Pterodactyl.add_module_row'] = 'Revres dda';
+$lang['Pterodactyl.add_module_group'] = 'Puorg revres dda';
+$lang['Pterodactyl.manage.module_rows_title'] = 'Srevres';
+$lang['Pterodactyl.manage.module_groups_title'] = 'Spuorg revres';
+$lang['Pterodactyl.manage.module_rows_heading.name'] = 'Lebal revres';
+$lang['Pterodactyl.manage.module_rows_heading.host_name'] = 'Emantsoh';
+$lang['Pterodactyl.manage.module_rows_heading.options'] = 'Snoitpo';
+$lang['Pterodactyl.manage.module_groups_heading.name'] = 'Eman puorg';
+$lang['Pterodactyl.manage.module_groups_heading.servers'] = 'Tnuoc revres';
+$lang['Pterodactyl.manage.module_groups_heading.options'] = 'Snoitpo';
+$lang['Pterodactyl.manage.module_rows.edit'] = 'Tide';
+$lang['Pterodactyl.manage.module_groups.edit'] = 'Tide';
+$lang['Pterodactyl.manage.module_rows.delete'] = 'Eteled';
+$lang['Pterodactyl.manage.module_groups.delete'] = 'Eteled';
+$lang['Pterodactyl.manage.module_rows.confirm_delete'] = '?revres siht eteled ot tnaw uoy erus uoy era';
+$lang['Pterodactyl.manage.module_groups.confirm_delete'] = '?puorg revres siht eteled ot tnaw uoy erus uoy era';
+$lang['Pterodactyl.manage.module_rows_no_results'] = 'Srevres on era ereht';
+$lang['Pterodactyl.manage.module_groups_no_results'] = 'Spuorg revres on era ereht';
+$lang['Pterodactyl.service_info.username'] = 'Emanresu';
+$lang['Pterodactyl.service_info.password'] = 'Drowssap';
+$lang['Pterodactyl.service_info.login'] = 'Lru nigol';
+$lang['Pterodactyl.tab_actions'] = 'Snoitca revres';
+$lang['Pterodactyl.tab_client_actions'] = 'Snoitca revres';
+$lang['Pterodactyl.tab_actions.heading_actions'] = 'Snoitca';
+$lang['Pterodactyl.tab_actions.server_status'] = 'Sutats revres';
+$lang['Pterodactyl.tab_actions.status_running'] = 'Enilno';
+$lang['Pterodactyl.tab_actions.status_offline'] = 'Enilffo';
+$lang['Pterodactyl.tab_actions.status_starting'] = 'Gnitrats';
+$lang['Pterodactyl.tab_actions.status_stopping'] = 'Gnippots';
+$lang['Pterodactyl.tab_client_actions.heading_actions'] = 'Snoitca';
+$lang['Pterodactyl.tab_client_actions.server_status'] = 'Sutats revres';
+$lang['Pterodactyl.tab_client_actions.status_running'] = 'Enilno';
+$lang['Pterodactyl.tab_client_actions.status_offline'] = 'Enilffo';
+$lang['Pterodactyl.tab_client_actions.status_starting'] = 'Gnitrats';
+$lang['Pterodactyl.tab_client_actions.status_stopping'] = 'Gnippots';
+$lang['Pterodactyl.!actions.start'] = 'Trats';
+$lang['Pterodactyl.!actions.stop'] = 'Pots';
+$lang['Pterodactyl.!actions.restart'] = 'Tratser';
+
