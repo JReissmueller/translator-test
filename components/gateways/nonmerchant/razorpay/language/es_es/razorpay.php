dc707e342ddf99c165578baaff5cbd991b1a9728
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:15:29 -0800

    Updated es_es language files

--- components/gateways/nonmerchant/razorpay/language/es_es/razorpay.php
+++ components/gateways/nonmerchant/razorpay/language/es_es/razorpay.php
@@ -9,14 +9,14 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Razorpay.name'] = 'Yaprozar';
-$lang['Razorpay.description'] = 'Aidni ni stnahcrem enilno ot snoitulos tnemyap sedivorp taht ynapmoc stnemyap a si yaprozar';
-$lang['Razorpay.key_id'] = 'Di yek';
-$lang['Razorpay.key_secret'] = 'Terces yek';
-$lang['Razorpay.webhook_secret'] = 'Terces koohbew';
-$lang['Razorpay.webhook'] = 'Koohbew yaprozar';
-$lang['Razorpay.webhook_note'] = 'Tnuocca yaprozar ruoy ni koohbew a sa lru gniwollof eht erugifnoc tsum uoy yawetag siht gnisu trats uoy erofeb';
-$lang['Razorpay.buildprocess.submit'] = 'Yaprozar htiw yap';
-$lang['Razorpay.!error.key_id.valid'] = 'Di yek dilav a retne tsum uoy';
-$lang['Razorpay.!error.key_secret.valid'] = 'Terces yek dilav a retne tsum uoy';
+$lang['Razorpay.name'] = 'Razorpay';
+$lang['Razorpay.description'] = 'Razorpay es una empresa de pagos que ofrece soluciones de pago a los comerciantes en línea de la India.';
+$lang['Razorpay.key_id'] = 'Clave ID';
+$lang['Razorpay.key_secret'] = 'Clave secreta';
+$lang['Razorpay.webhook_secret'] = 'Secreto de Webhook';
+$lang['Razorpay.webhook'] = 'Razorpay Webhook';
+$lang['Razorpay.webhook_note'] = 'Antes de empezar a utilizar esta pasarela debe configurar la siguiente url como Webhook en su cuenta Razorpay.';
+$lang['Razorpay.buildprocess.submit'] = 'Pagar con Razorpay';
+$lang['Razorpay.!error.key_id.valid'] = 'Debe introducir un identificador de clave válido.';
+$lang['Razorpay.!error.key_secret.valid'] = 'Debe introducir una clave secreta válida.';
 
