cfbe2ab71eb7d70c2ddcac0e723933339de42356
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:40 -0800

    Updated es_es language files

---
+++ plugins/download_manager/language/es_es/download_manager_categories.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Download Manager Categories
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerCategories.!error.parent_id.exists'] = 'Tsixe ton seod yrogetac tnerap detceles eht';
+$lang['DownloadManagerCategories.!error.parent_id.loop'] = 'Flesti yrogetac eht eb ton yam yrogetac tnerap detceles eht';
+$lang['DownloadManagerCategories.!error.company_id.exists'] = 'Di ynapmoc dilavni';
+$lang['DownloadManagerCategories.!error.name.empty'] = 'Eman yrogetac a retne esaelp';
+$lang['DownloadManagerCategories.!error.description.empty'] = 'Noitpircsed yrogetac a retne esaelp';
+$lang['DownloadManagerCategories.!error.category_id.exists'] = 'Tsixe ton seod detadpu gnieb yrogetac eht';
+
