7c5ab09ed2132fa08b6db40b333d2fd166056470
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:30 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/offline/language/es_es/offline.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Offline
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Offline.name'] = 'Tnemyap enilffo';
+$lang['Offline.description'] = 'Enilffo stnemyap gnikat rof snoitcurtsni laiceps yalpsid ot uoy swolla';
+$lang['Offline.instructions'] = 'Snoitcurtsni';
+$lang['Offline.!error.instructions.valid'] = 'Snoitcurtsni emos retne tsum uoy';
+
