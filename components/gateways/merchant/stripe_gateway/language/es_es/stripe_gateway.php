e5c9cd585c60ed3bb4116e52365040ac6006b989
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:22:04 -0800

    Updated es_es language files

--- components/gateways/merchant/stripe_gateway/language/es_es/stripe_gateway.php
+++ components/gateways/merchant/stripe_gateway/language/es_es/stripe_gateway.php
@@ -9,14 +9,14 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Stripe_gateway.!error.auth'] = 'Etacitnehtua ton dluoc yawetag eht';
-$lang['Stripe_gateway.!error.api_key.empty'] = 'Yek ipa na retne esaelp';
-$lang['Stripe_gateway.!error.json_required'] = 'Yawetag siht rof deriuqer si noisnetxe nosj eht';
-$lang['Stripe_gateway.name'] = 'Epirts';
-$lang['Stripe_gateway.description'] = 'Daetsni atselb ni yawetag stnemyap epirts eht gnisu dnemmocer ew ."ylefasnu stnemyap ssecorp" ot detadpu eb tnuocca epirts ruoy seriuqer ti .tuokcehc epirts sesu taht yawetag epirts ycagel eht si siht';
-$lang['Stripe_gateway.api_key'] = 'Yek terces ipa';
-$lang['Stripe_gateway.tooltip_api'] = 'Yek tcerroc eht gnisu era uoy erus eb .edom tset ro evil rehtie ot cificeps si yek terces ipa ruoy';
-$lang['Stripe_gateway.stored'] = 'Etisffo noitamrofni drac erots';
-$lang['Stripe_gateway.tooltip_stored'] = 'Atselb nihtiw naht rehtar epirts htiw noitamrofni drac tnuocca tnemyap erots ot xob siht kcehc';
-$lang['Stripe_gateway.charge_description'] = '%1$s rof egrahc';
+$lang['Stripe_gateway.!error.auth'] = 'La puerta de enlace no ha podido autenticarse.';
+$lang['Stripe_gateway.!error.api_key.empty'] = 'Introduzca una clave API.';
+$lang['Stripe_gateway.!error.json_required'] = 'La extensión JSON es necesaria para esta pasarela.';
+$lang['Stripe_gateway.name'] = 'Raya';
+$lang['Stripe_gateway.description'] = 'Esta es la antigua pasarela de Stripe que utiliza Stripe Checkout. Requiere que tu cuenta de Stripe esté actualizada para "Procesar pagos de forma insegura". Le recomendamos que utilice la pasarela de pagos de Stripe en Blesta.';
+$lang['Stripe_gateway.api_key'] = 'Clave secreta API';
+$lang['Stripe_gateway.tooltip_api'] = 'Su clave secreta de la API es específica para el modo activo o de prueba. Asegúrese de que está utilizando la clave correcta.';
+$lang['Stripe_gateway.stored'] = 'Almacenar la información de la tarjeta fuera del sitio';
+$lang['Stripe_gateway.tooltip_stored'] = 'Marque esta casilla para almacenar la información de la tarjeta de la cuenta de pago con Stripe en lugar de dentro de Blesta.';
+$lang['Stripe_gateway.charge_description'] = 'Cargo por %1$s';
 
