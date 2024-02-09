0c76e2dc7b32a79b16f00fa1cbfa89155668f961
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:05:01 -0800

    Updated es_es language files

--- @es_es/components/modules/generic_domains/language/es_es/generic_domains.php
+++ @es_es/components/modules/generic_domains/language/es_es/generic_domains.php
@@ -9,13 +9,13 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['GenericDomains.name'] = 'Dominios genéricos';
-$lang['GenericDomains.description'] = 'Un módulo genérico para el registro de dominios.';
-$lang['GenericDomains.module_row'] = 'Módulo Fila';
-$lang['GenericDomains.module_row_plural'] = 'Módulo Filas';
-$lang['GenericDomains.domain.domain'] = 'Dominio';
-$lang['GenericDomains.transfer.domain'] = 'Dominio';
-$lang['GenericDomains.transfer.transfer_key'] = 'Código PPE';
-$lang['GenericDomains.package_fields.tld_options'] = 'TLDs';
-$lang['GenericDomains.manage.text'] = 'El personal puede recibir copias CCO de los correos electrónicos de creación, suspensión y cancelación de servicios si su grupo de personal tiene configuradas estas notificaciones CCO y el miembro del personal se suscribe a ellas en su sección [Mi información] -> [Notificaciones].';
+$lang['GenericDomains.name'] = 'Sniamod cireneg';
+$lang['GenericDomains.description'] = 'Noitartsiger niamod rof eludom cireneg a';
+$lang['GenericDomains.module_row'] = 'Wor eludom';
+$lang['GenericDomains.module_row_plural'] = 'Swor eludom';
+$lang['GenericDomains.domain.domain'] = 'Niamod';
+$lang['GenericDomains.transfer.domain'] = 'Niamod';
+$lang['GenericDomains.transfer.transfer_key'] = 'Edoc ppe';
+$lang['GenericDomains.package_fields.tld_options'] = 'Sdlt';
+$lang['GenericDomains.manage.text'] = 'Noitces ]seciton[ >- ]ofni ym[ rieht rednu meht ot sebircsbus rebmem ffats eht dna ,tes seciton ccb eseht sah puorg ffats rieht fi sliame noitallecnac dna noisnepsus ,noitaerc ecivres fo seipoc ccb eviecer yam ffats';
 
