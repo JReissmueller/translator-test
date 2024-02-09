753a180ccbd145883c3279e1d36a2e365c5e9bdc
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:43 -0800

    Updated es_es language files

---
+++ plugins/download_manager/language/es_es/download_manager_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Download Manager Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerPlugin.name'] = 'Reganam daolnwod';
+$lang['DownloadManagerPlugin.description'] = 'Daolnwod ot stneilc rof elbaliava edam dna dedaolpu eb ot selif swolla';
+$lang['DownloadManagerPlugin.client_main'] = 'Sdaolnwod';
+$lang['DownloadManagerPlugin.admin_main'] = 'Reganam daolnwod';
+
