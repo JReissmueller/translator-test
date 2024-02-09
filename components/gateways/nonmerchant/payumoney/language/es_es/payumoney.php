070614fa1342bc370a3edeb04473e9506765f701
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:35 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/payumoney/language/es_es/payumoney.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Payumoney
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payumoney.name'] = 'Yenomuyap';
+$lang['Payumoney.description'] = 'Stnahcrem ecremmoc-e eht fo %06 ylraen rof rentrap stnemyap derreferp eht si dna sdohtem tnemyap +001 revo htiw stnahcrem +000,05,4 naht erom sevres uyap ,aidni ni';
+$lang['Payumoney.merchant_key'] = 'Yek tnahcrem';
+$lang['Payumoney.merchant_salt'] = 'Tlas tnahcrem';
+$lang['Payumoney.auth_header'] = 'Redaeh noitazirohtua';
+$lang['Payumoney.auth_header_note'] = 'Tlas-yek tnahcrem >- tnuocca ym >- tnuocca eganam rednu draobhsad yenomuyap eht no dnuof eb nac redaeh noitazirohtua';
+$lang['Payumoney.merchant_key_note'] = 'Tlas-yek tnahcrem >- tnuocca ym >- tnuocca eganam rednu draobhsad yenomuyap eht no dnuof eb nac yek tnahcrem';
+$lang['Payumoney.merchant_salt_note'] = 'Tlas-yek tnahcrem >- tnuocca ym >- tnuocca eganam rednu draobhsad yenomuyap eht no dnuof eb nac tlas tnahcrem';
+$lang['Payumoney.test_mode'] = 'Edom xobdnas';
+$lang['Payumoney.test_mode_note'] = 'Tnuocca xobdnas yenomuyap a htiw gnitset era uoy fi noitpo siht elbane ylno .xobdnas yenomuyap eht ezilitu lliw noitpo siht gnilbane';
+$lang['Payumoney.buildprocess.submit'] = 'Yenomuyap aiv yap';
+$lang['Payumoney.!error.merchant_key.valid'] = 'Yek tnahcrem dilav a retne esaelp';
+$lang['Payumoney.!error.merchant_salt.valid'] = 'Tlas dilav a retne esaelp';
+$lang['Payumoney.!error.auth_header.valid'] = 'Redaeh noitazirohtua dilav a retne esaelp';
+$lang['Payumoney.!error.key.valid'] = 'Yek tnahcrem dilavni';
+$lang['Payumoney.!error.hash.valid'] = 'Hsah dilavni';
+$lang['Payumoney.!error.invalid_token.response'] = 'Nekot dilavni na htiw dednopser yenomuyap';
+
