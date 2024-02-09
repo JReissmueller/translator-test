a67116ae483adfa5a2c495c09e6ffd6b7559f7da
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:55 -0800

    Updated es_es language files

---
+++ language/es_es/nonmerchant_gateway.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Nonmerchant Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['NonmerchantGateway.!error.invalid'] = 'Deviecer saw esnopser tnemyap dezingocernu ro dilavni na';
+$lang['NonmerchantGateway.!error.transaction_not_found'] = 'Noitca taht gnissecorp rof elbaliava regnol on ro dilavni si noitcasnart eht';
+$lang['NonmerchantGateway.!error.unsupported'] = 'Noitca taht troppus ton seod yawetag eht';
+$lang['NonmerchantGateway.!error.general'] = 'Yawetag eht htiw tseuqer eht gnissecorp nehw derucco rorre na';
+
