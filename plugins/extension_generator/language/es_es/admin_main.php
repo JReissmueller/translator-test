b79ac0ae86ee6d182dfde1b00464cedc7bd8937b
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:44 -0800

    Updated es_es language files

---
+++ plugins/extension_generator/language/es_es/admin_main.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!error.name_taken'] = 'Yrotcerid noisnetxe gnitsixe na htiw stcilfnoc eman siht';
+$lang['AdminMain.!error.generation_failed'] = '%1$s :detareneg noitpecxe  .noisnetxe ruoy etareneg ot elbanu';
+$lang['AdminMain.!notice.type_warning'] = 'Noisnetxe siht rof devas sgnittes lla esare ylbisriverri lliw epyt noisnetxe eht gnignahc';
+$lang['AdminMain.!notice.file_overwrite'] = 'Yrotcerid atselb eht esu uoy fi nettirwrevo eb lliw dna selif atselb eht ni tsixe ydaerla noisnetxe siht rof selif eht';
+$lang['AdminMain.!notice.updating_installed_extension'] = 'Esabatad uoy fo ycnetsisisnoc eht ot egamad ni tluser yam ti ro selif eht gnitareneger erofeb ti llatsinu uoy taht dednemmocer ylhgih si ti .dellatsni yltnerruc si noisnetxe siht';
+$lang['AdminMain.!success.package_deleted'] = 'Deteled yllufsseccus neeb sah noisnetxe eht';
+$lang['AdminMain.!success.module_created'] = '%1$s ta dnuof eb nac dna detareneg yllufsseccus neeb sah eludom wen eht';
+$lang['AdminMain.!success.plugin_created'] = '%1$s ta dnuof eb nac dna detareneg yllufsseccus neeb sah nigulp wen eht';
+$lang['AdminMain.!success.merchant_created'] = '%1$s ta dnuof eb nac dna detareneg yllufsseccus neeb sah yawetag wen eht';
+$lang['AdminMain.!success.nonmerchant_created'] = '%1$s ta dnuof eb nac dna detareneg yllufsseccus neeb sah yawetag wen eht';
+$lang['AdminMain.getfilelocations.module'] = 'Yrotcerid seludom atselb';
+$lang['AdminMain.getfilelocations.plugin'] = 'Yrotcerid snigulp atselb';
+$lang['AdminMain.getfilelocations.merchant'] = 'Yrotcerid yawetag tnahcrem atselb';
+$lang['AdminMain.getfilelocations.nonmerchant'] = 'Yrotcerid yawetag tnahcrem-non atselb';
+$lang['AdminMain.getfilelocations.upload'] = 'Yrotcerid sdaolpu atselb';
+$lang['AdminMain.getfilelocations.custom'] = 'Motsuc';
+$lang['AdminMain.index.page_title'] = 'Rotareneg noisnetxe';
+$lang['AdminMain.index.boxtitle_extension_generator'] = 'Rotareneg noisnetxe';
+$lang['AdminMain.index.boxtitle_extensions'] = 'Snoisnetxe - rotareneg noisnetxe';
+$lang['AdminMain.index.extension_add'] = 'Noisnetxe dda';
+$lang['AdminMain.index.heading_id'] = 'Di';
+$lang['AdminMain.index.heading_name'] = 'Eman';
+$lang['AdminMain.index.heading_type'] = 'Epyt';
+$lang['AdminMain.index.heading_form_type'] = 'Epyt mrof';
+$lang['AdminMain.index.heading_code_examples'] = 'Selpmaxe edoc';
+$lang['AdminMain.index.heading_date_updated'] = 'Detadpu etad';
+$lang['AdminMain.index.heading_options'] = 'Snoitpo';
+$lang['AdminMain.index.option_edit'] = 'Tide';
+$lang['AdminMain.index.option_delete'] = 'Eteled';
+$lang['AdminMain.index.text_confirm_delete'] = '?noisnetxe siht eteled ot tnaw uoy erus uoy era';
+$lang['AdminMain.index.extensions_no_results'] = 'Emit siht ta snoisnetxe on era ereht';
+$lang['AdminMain.general.heading_general_settings'] = 'Sgnittes lareneg';
+$lang['AdminMain.general.name'] = 'Eman';
+$lang['AdminMain.general.type'] = 'Epyt noisnetxe';
+$lang['AdminMain.general.form_type'] = 'Epyt mrof';
+$lang['AdminMain.general.code_examples'] = 'Edoc elpmaxe edulcni';
+$lang['AdminMain.general.basic_info'] = 'Ofni cisab - txen';
+$lang['AdminMain.general.tooltip_name'] = 'Noitareneg edoc htiw seussi emos sesuac ti sa eman eht ni "nigulp" drow eht edulcni ton od snigulp rof  .eludomym eb lliw eman ssalc eht dna ,eludom_ym eb lliw yrotcerid eludom eht neht ,eludom ym si eman eht fi ,elpmaxe rof .edoc suoirav gnitaerc rof desu osla si siht .noisnetxe eht fo eman yalpsid eht';
+$lang['AdminMain.general.tooltip_type'] = 'Tnemyap etelpmoc ot etis rossecorp tnemyap a ot stneilc dnes syawetag tnahcrem-non  .ecafretni atselb eht ni stneilc gnipeek elihw stnemyap ssecorp syawetag tnahcrem .atselb ni gnihtyna tsom gniod fo elbapac noitargetni lufrewop era snigulp .srevres etomer no gninoisivorp ecivres gnidulcni serutaef ecivres ynam eldnah seludom';
+$lang['AdminMain.general.tooltip_form_type'] = 'Noisnetxe eht etareneg ot yrassecen muminim eht ylno gnidulcni ,mrof noisnetxe eht fo noisrev detacnurt ylhgih a esu ot cisab';
+$lang['AdminMain.general.tooltip_code_examples'] = 'Cte ,sni-eit dna stsil tneve ,sksat norc sa hcus serutaef rof edoc elpmas fo senil tuo detnemmoc edulcni ot kcehc';
+$lang['AdminMain.general.placeholder_name'] = 'Eman noisnetxe';
+$lang['AdminMain.confirm.heading_confirm'] = 'Noitamrifnoc';
+$lang['AdminMain.confirm.text_generation'] = 'Noisnetxe ruoy rof selif eht etareneg yllacitamotua dna hsinif ot "noisnetxe etareneg" kcilc  .rab ssergorp eht ni sedon eht no gnikcilc yb noitces yna weiver yam uoy  .etelpmoc era sgnittes noisnetxe';
+$lang['AdminMain.confirm.location'] = 'Noitacol noisnetxe';
+$lang['AdminMain.confirm.tooltip_location'] = 'Selif noisnetxe detareneg daolpu ot hcihw ni yrotcerid eht';
+$lang['AdminMain.confirm.custom_path'] = 'Htap motsuc';
+$lang['AdminMain.confirm.generate'] = 'Noisnetxe etareneg';
+
