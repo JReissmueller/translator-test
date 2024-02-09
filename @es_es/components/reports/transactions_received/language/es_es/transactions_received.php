93b8f79816b5aa2f13b6c759db4de34596399794
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:05:53 -0800

    Updated es_es language files

--- @es_es/components/reports/transactions_received/language/es_es/transactions_received.php
+++ @es_es/components/reports/transactions_received/language/es_es/transactions_received.php
@@ -9,22 +9,22 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['TransactionsReceived.name'] = 'Transacciones recibidas';
-$lang['TransactionsReceived.option.any'] = 'Cualquier';
-$lang['TransactionsReceived.options.field_start_date'] = 'Fecha de inicio';
-$lang['TransactionsReceived.options.field_end_date'] = 'Fecha final';
-$lang['TransactionsReceived.options.field_gateway'] = 'Pasarela';
-$lang['TransactionsReceived.options.field_status'] = 'Estado';
-$lang['TransactionsReceived.options.field_payment_type'] = 'Tipo de pago';
-$lang['TransactionsReceived.heading.id'] = 'Transacción #';
-$lang['TransactionsReceived.heading.transaction_id'] = 'ID de transacción';
-$lang['TransactionsReceived.heading.reference_id'] = 'ID de referencia';
-$lang['TransactionsReceived.heading.client_id_code'] = 'Cliente';
-$lang['TransactionsReceived.heading.gateway_name'] = 'Pasarela de pagos';
-$lang['TransactionsReceived.heading.type_name'] = 'Tipo de pago';
-$lang['TransactionsReceived.heading.income_type'] = 'Tipo de transacción';
-$lang['TransactionsReceived.heading.amount'] = 'Importe del pago';
-$lang['TransactionsReceived.heading.currency'] = 'Moneda';
-$lang['TransactionsReceived.heading.status'] = 'Estado';
-$lang['TransactionsReceived.heading.date_added'] = 'Fecha de adición';
+$lang['TransactionsReceived.name'] = 'Deviecer snoitcasnart';
+$lang['TransactionsReceived.option.any'] = 'Yna';
+$lang['TransactionsReceived.options.field_start_date'] = 'Etad trats';
+$lang['TransactionsReceived.options.field_end_date'] = 'Etad dne';
+$lang['TransactionsReceived.options.field_gateway'] = 'Yawetag';
+$lang['TransactionsReceived.options.field_status'] = 'Sutats';
+$lang['TransactionsReceived.options.field_payment_type'] = 'Epyt tnemyap';
+$lang['TransactionsReceived.heading.id'] = '# noitcasnart';
+$lang['TransactionsReceived.heading.transaction_id'] = 'Di noitcasnart';
+$lang['TransactionsReceived.heading.reference_id'] = 'Di ecnerefer';
+$lang['TransactionsReceived.heading.client_id_code'] = '# tneilc';
+$lang['TransactionsReceived.heading.gateway_name'] = 'Yawetag tnemyap';
+$lang['TransactionsReceived.heading.type_name'] = 'Epyt tnemyap';
+$lang['TransactionsReceived.heading.income_type'] = 'Epyt noitcasnart';
+$lang['TransactionsReceived.heading.amount'] = 'Tnuoma tnemyap';
+$lang['TransactionsReceived.heading.currency'] = 'Ycnerruc';
+$lang['TransactionsReceived.heading.status'] = 'Sutats';
+$lang['TransactionsReceived.heading.date_added'] = 'Dedda etad';
 
