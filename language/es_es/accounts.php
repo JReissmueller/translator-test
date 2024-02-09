fa8647f2ac7d764b083d06703ee156847b885f44
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:35 -0800

    Updated es_es language files

---
+++ language/es_es/accounts.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Accounts.!error.contact_id.exists'] = 'Di tcatnoc dilavni';
+$lang['Accounts.!error.first_name.empty'] = 'Eman tsrif a yficeps esaelp';
+$lang['Accounts.!error.last_name.empty'] = 'Eman tsal a yficeps esaelp';
+$lang['Accounts.!error.state.length'] = 'Sretcarahc 3 deecxe ton yam htgnel etats';
+$lang['Accounts.!error.country.length'] = 'Sretcarahc 3 deecxe ton yam htgnel yrtnuoc';
+$lang['Accounts.!error.state.country_exists'] = 'Etats detceles eht sehctam taht yrtnuoc eht tceles esaelp';
+$lang['Accounts.!error.currency.format'] = 'Dilavni si ycnerruc eht';
+$lang['Accounts.!error.account.length'] = 'Rebmun tnuocca na yficeps esaelp';
+$lang['Accounts.!error.routing.empty'] = 'Rebmun gnituor a yficeps esaelp';
+$lang['Accounts.!error.last4.empty_ach'] = 'Rebmun tnuocca eht fo stigid ruof tsal eht retne esaelp';
+$lang['Accounts.!error.type.ach_format'] = 'Epyt dilavni';
+$lang['Accounts.!error.ach_account_id.exists'] = 'Di tnuocca dilavni';
+$lang['Accounts.!error.number.valid'] = 'Dilavni si nevig rebmun drac eht';
+$lang['Accounts.!error.expiration.empty'] = 'Etad noitaripxe drac tiderc eht retne esaelp';
+$lang['Accounts.!error.expiration.valid'] = 'Despal ydaerla sah etad noitaripxe eht';
+$lang['Accounts.!error.last4.empty_cc'] = 'Rebmun drac tiderc eht fo stigid ruof tsal eht retne esaelp';
+$lang['Accounts.!error.type.cc_format'] = 'Denimreted eb ton dluoc ro dilavni si epyt drac eht';
+$lang['Accounts.!error.cc_account_id.exists'] = 'Di tnuocca dilavni';
+$lang['Accounts.getTypes.cc'] = 'Drac tiderc';
+$lang['Accounts.getTypes.ach'] = 'Esuoh gniraelc detamotua';
+$lang['Accounts.getTypes.other'] = 'Rehto';
+$lang['Accounts.getAchTypes.checking'] = 'Gnikcehc';
+$lang['Accounts.getAchTypes.savings'] = 'Sgnivas';
+$lang['Accounts.getCcTypes.amex'] = 'Sserpxe nacirema';
+$lang['Accounts.getCcTypes.bc'] = 'Dracknab';
+$lang['Accounts.getCcTypes.cup'] = 'Yap noinu anihc';
+$lang['Accounts.getCcTypes.dc-cb'] = 'Ehcnalb etrac bulc srenid';
+$lang['Accounts.getCcTypes.dc-er'] = 'Etuorne bulc srenid';
+$lang['Accounts.getCcTypes.dc-int'] = 'Lanoitanretni bulc srenid';
+$lang['Accounts.getCcTypes.dc-uc'] = 'Adanac dna su bulc srenid';
+$lang['Accounts.getCcTypes.disc'] = 'Revocsid';
+$lang['Accounts.getCcTypes.ipi'] = 'Tnemyapatsni';
+$lang['Accounts.getCcTypes.jcb'] = 'Uaerub tiderc napaj';
+$lang['Accounts.getCcTypes.lasr'] = 'Resal';
+$lang['Accounts.getCcTypes.maes'] = 'Ortseam';
+$lang['Accounts.getCcTypes.mc'] = 'Drac retsam';
+$lang['Accounts.getCcTypes.solo'] = 'Olos';
+$lang['Accounts.getCcTypes.switch'] = 'Hctiws';
+$lang['Accounts.getCcTypes.visa'] = 'Asiv';
+$lang['Accounts.getCcTypes.other'] = 'Rehto';
+
