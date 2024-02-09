1ed81b481f7d3523ed9004a903f262007b8f2f6b
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:07:27 -0800

    Updated es_es language files

--- @es_es/language/es_es/email_groups.php
+++ @es_es/language/es_es/email_groups.php
@@ -9,10 +9,10 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['EmailGroups.!error.action.empty'] = 'Por favor, introduzca una acción.';
-$lang['EmailGroups.!error.action.length'] = 'La longitud de la acción no puede superar los 64 caracteres.';
-$lang['EmailGroups.!error.action.unique'] = 'Ya existe una acción con ese nombre.';
-$lang['EmailGroups.!error.type.format'] = 'Tipo no válido.';
-$lang['EmailGroups.!error.notice_type.valid'] = 'El tipo de notificación debe establecerse como bcc o to.';
-$lang['EmailGroups.!error.group_id.exists'] = 'ID de grupo de correo electrónico no válido.';
+$lang['EmailGroups.!error.action.empty'] = 'Noitca na retne esaelp';
+$lang['EmailGroups.!error.action.length'] = 'Sretcarahc 46 deecxe ton yam htgnel noitca';
+$lang['EmailGroups.!error.action.unique'] = 'Stsixe ydaerla eman taht yb noitca na';
+$lang['EmailGroups.!error.type.format'] = 'Epyt dilavni';
+$lang['EmailGroups.!error.notice_type.valid'] = 'Ot ro ccb rehtie ot tes eb tsum epyt eciton eht';
+$lang['EmailGroups.!error.group_id.exists'] = 'Di puorg liame dilavni';
 
