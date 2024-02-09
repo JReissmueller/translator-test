cc011fd261334fa702d500ab73c0e2d42bf92ace
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:13 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/cornerstone/language/es_es/cornerstone.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Cornerstone
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cornerstone.!error.security_key.empty'] = 'Yek ytiruces ruoy retne esaelp';
+$lang['Cornerstone.name'] = 'Enotsrenroc';
+$lang['Cornerstone.description'] = 'Asu eht ni yrtsudni gnissecorp tnahcrem eht ni snoitazinagro selas tnednepedni detarepo dna denwo naitsirhc gnidael eht fo eno si enotsrenroc';
+$lang['Cornerstone.security_key'] = 'Yek ytiruces';
+
