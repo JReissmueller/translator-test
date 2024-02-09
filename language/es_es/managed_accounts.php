65c1ccb7009c73e9e1eeff6f3c05aeddec41052a
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:47 -0800

    Updated es_es language files

---
+++ language/es_es/managed_accounts.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Managed Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ManagedAccounts.!error.client.exists'] = 'Dedivorp sserdda liame eht ot dengissa tnuocca on si ereht';
+$lang['ManagedAccounts.!error.client_id.exists'] = 'Di tneilc dilavni';
+$lang['ManagedAccounts.!error.email.invitation'] = 'Tnuocca siht ot tnes neeb ydaerla sah noitativni na';
+$lang['ManagedAccounts.!error.invitation_id.exists'] = 'Di noitativni dilavni';
+$lang['ManagedAccounts.!error.contact_id.exists'] = 'Di tcatnoc dilavni';
+$lang['ManagedAccounts.!error.permissions.manage'] = 'Tnuocca siht eganam ot dezirohtua ton era uoy';
+$lang['ManagedAccounts.!error.permissions.empty'] = 'Reganam eht ot noissimrep eno tsael ta ngissa ot deen uoy';
+
