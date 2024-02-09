f288c9265d108d05174270604cacd7551d71827f
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:39 -0800

    Updated es_es language files

---
+++ language/es_es/admin_company_automation.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Company Automation
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyAutomation.!notice.cron_task_interval'] = 'Slavretni retrohs eseht fo egatnavda ekat ot etunim yreve nur ot norc ruoy etadpu .etunim yreve gninnur eb ot raeppa ton seod norc ruoy tub ,etunim yreve nur ot tes sksat noitamotua era ereht';
+$lang['AdminCompanyAutomation.!success.automation_updated'] = '!detadpu yllufsseccus erew sgnittes noitamotua eht';
+$lang['AdminCompanyAutomation.!success.task_cleared'] = 'Deraelc neeb sah ksat norc eht';
+$lang['AdminCompanyAutomation.index.page_title'] = 'Noitamotua > ynapmoc > sgnittes';
+$lang['AdminCompanyAutomation.index.boxtitle_automation'] = 'Noitamotua';
+$lang['AdminCompanyAutomation.index.field_automationsubmit'] = 'Sgnittes etadpu';
+$lang['AdminCompanyAutomation.index.field_enabled'] = 'Delbane';
+$lang['AdminCompanyAutomation.index.text_interval'] = 'Lavretni';
+$lang['AdminCompanyAutomation.index.text_starttime'] = 'Emit trats';
+$lang['AdminCompanyAutomation.index.text_task_lastran'] = 'Nar tsal ksat';
+$lang['AdminCompanyAutomation.index.no_results'] = 'Sksat norc on era ereht';
+$lang['AdminCompanyAutomation.index.no_cron_lastran'] = 'Reven';
+$lang['AdminCompanyAutomation.index.text_cron_last_ran'] = '%1$s no nar tsal norc eht';
+$lang['AdminCompanyAutomation.index.text_cron_never_ran'] = 'Nur reven sah norc eht';
+$lang['AdminCompanyAutomation.index.option_clear_task'] = 'Kcol ksat raelc';
+$lang['AdminCompanyAutomation.index.confirm_clear_task'] = 'Esuac eht enimreted ot srorre rof gol norc eht gnikcehc ro ,yllaunam norc eht gninnur yrt .eussi suoires erom a eb yam ereht ,sllats ylsuounitnoc ksat siht fi ?kcol ksat norc siht raelc ot tnaw uoy erus uoy era';
+$lang['AdminCompanyAutomation.getintervals.text_minute'] = 'Etunim';
+$lang['AdminCompanyAutomation.getintervals.text_minutes'] = 'Setunim';
+$lang['AdminCompanyAutomation.getintervals.text_hour'] = 'Ruoh';
+$lang['AdminCompanyAutomation.getintervals.text_hours'] = 'Sruoh';
+
