0b8001ed52aea166b199760a1c416705bc3a087c
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:00 -0800

    Updated es_es language files

---
+++ components/modules/multicraft/language/es_es/multicraft.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Multicraft
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Multicraft.!error.module_row.missing'] = 'Elbaliavanu si wor eludom eht .derrucco rorre lanretni na';
+$lang['Multicraft.name'] = 'Tfarcitlum';
+$lang['Multicraft.module_row.name'] = 'Revres';
+$lang['Multicraft.module_rows.name'] = 'Srevres';
+$lang['Multicraft.add_module_row'] = 'Revres dda';
+$lang['Multicraft.manage.module_rows_title'] = 'Srevres';
+$lang['Multicraft.manage.module_rows_heading.username'] = 'Emanresu';
+$lang['Multicraft.manage.module_rows_heading.server_name'] = 'Lebal revres';
+$lang['Multicraft.manage.module_rows_heading.panel_api_url'] = 'Lru ipa lenap';
+$lang['Multicraft.manage.module_rows_heading.options'] = 'Snoitpo';
+$lang['Multicraft.manage.module_rows.edit'] = 'Tide';
+$lang['Multicraft.manage.module_rows.delete'] = 'Eteled';
+$lang['Multicraft.manage.module_rows.confirm_delete'] = '?revres siht eteled ot tnaw uoy erus uoy era';
+$lang['Multicraft.manage.module_rows_no_results'] = 'Srevres on era ereht';
+$lang['Multicraft.manage.module_rows_in_use_0'] = 'On';
+$lang['Multicraft.manage.module_rows_in_use_1'] = 'Sey';
+$lang['Multicraft.service_info.options'] = 'Snoitpo';
+$lang['Multicraft.service_info.option_login'] = 'Ni gol';
+$lang['Multicraft.tab_client_actions'] = 'Snoitca';
+$lang['Multicraft.tab_client_players'] = 'Sreyalp';
+$lang['Multicraft.tab_client_console'] = 'Elosnoc';
+$lang['Multicraft.tab_client_actions.heading_server_address'] = 'Sserdda revres';
+$lang['Multicraft.tab_client_actions.address_ip'] = 'Sserdda pi';
+$lang['Multicraft.tab_client_actions.address_port'] = 'Trop';
+$lang['Multicraft.tab_client_actions.heading_server_status'] = 'Sutats revres';
+$lang['Multicraft.tab_client_actions.heading_server_actions'] = 'Snoitca revres';
+$lang['Multicraft.tab_client_actions.action_restart'] = 'Tratser';
+$lang['Multicraft.tab_client_actions.action_stop'] = 'Pots';
+$lang['Multicraft.tab_client_actions.action_start'] = 'Trats';
+$lang['Multicraft.tab_client_actions.action_set_daytime'] = 'Emit yad ot tes';
+$lang['Multicraft.tab_client_actions.action_set_nighttime'] = 'Emit thgin ot tes';
+$lang['Multicraft.tab_client_actions.action_server_name'] = 'Eman revres tes';
+$lang['Multicraft.tab_client_actions.unknown'] = 'Nwonknu';
+$lang['Multicraft.tab_client_actions.heading_server_name'] = 'Eman revres tes';
+$lang['Multicraft.tab_client_console.field_server_name'] = 'Eman revres';
+$lang['Multicraft.tab_client_actions.submit_save'] = 'Evas';
+$lang['Multicraft.tab_client_actions.status_online'] = 'Sreyalp %2$s/%1$s ,enilno';
+$lang['Multicraft.tab_client_actions.status_offline'] = 'Enilffo';
+$lang['Multicraft.tab_client_actions.status_suspended'] = 'Dednepsus';
+$lang['Multicraft.tab_client_actions.status_unknown'] = 'Nwonknu';
+$lang['Multicraft.tab_client_players.heading_players'] = 'Sreyalp detcennoc';
+$lang['Multicraft.tab_client_players.heading_player_name'] = 'Eman reyalp';
+$lang['Multicraft.tab_client_players.heading_action'] = 'Noitca';
+$lang['Multicraft.tab_client_players.action_kick'] = 'Reyalp kcik';
+$lang['Multicraft.tab_client_players.no_results'] = 'Detcennoc sreyalp on era ereht';
+$lang['Multicraft.tab_client_players.heading_console'] = 'Elosnoc revres';
+$lang['Multicraft.tab_client_console.field_command'] = 'Dnammoc';
+$lang['Multicraft.tab_client_console.submit_send'] = 'Dnes';
+$lang['Multicraft.tab_client_console.chat_entry'] = '%3$s :%2$s ]%1$s[';
+$lang['Multicraft.tab_client_console.submit_clear_log'] = 'Gol raelc';
+$lang['Multicraft.tab_client_players.heading_console_tab'] = 'Sgol elosnoc';
+$lang['Multicraft.tab_client_players.heading_chat_tab'] = 'Sgol tahc';
+$lang['Multicraft.add_row.box_title'] = 'Revres tfarcitlum dda';
+$lang['Multicraft.add_row.basic_title'] = 'Sgnittes cisab';
+$lang['Multicraft.add_row.add_btn'] = 'Revres dda';
+$lang['Multicraft.add_row.daemons_title'] = 'Sesserdda pi detacided';
+$lang['Multicraft.add_row.ip_btn'] = 'Sserdda pi dda';
+$lang['Multicraft.add_row.remove_ip'] = 'Evomer';
+$lang['Multicraft.add_row.daemon_col'] = 'Di nomead';
+$lang['Multicraft.add_row.ip_col'] = 'Sserdda pi';
+$lang['Multicraft.add_row.port_col'] = 'Trop';
+$lang['Multicraft.add_row.in_use_col'] = 'Esu ni';
+$lang['Multicraft.edit_row.box_title'] = 'Revres tfarcitlum tide';
+$lang['Multicraft.edit_row.basic_title'] = 'Sgnittes cisab';
+$lang['Multicraft.edit_row.add_btn'] = 'Revres tide';
+$lang['Multicraft.edit_row.daemons_title'] = 'Sesserdda pi detacided';
+$lang['Multicraft.edit_row.ip_btn'] = 'Sserdda pi dda';
+$lang['Multicraft.edit_row.remove_ip'] = 'Evomer';
+$lang['Multicraft.edit_row.daemon_col'] = 'Di nomead';
+$lang['Multicraft.edit_row.ip_col'] = 'Sserdda pi';
+$lang['Multicraft.edit_row.port_col'] = 'Trop';
+$lang['Multicraft.edit_row.in_use_col'] = 'Esu ni';
+$lang['Multicraft.!tooltip.panel_url'] = '/moc.niamod.tfarcitlum//:ptth .g.e .lenap tfarcitlum ruoy ot lru eht retne';
+$lang['Multicraft.!tooltip.panel_api_url'] = 'Php.ipa/moc.niamod.tfarcitlum//:ptth .g.e .ipa tfarcitlum ruoy ot lru eht retne';
+$lang['Multicraft.!tooltip.key'] = 'Tfarcitlum ni resu ruoy rof detareneg yek ipa eht retne';
+$lang['Multicraft.!tooltip.log_all'] = 'Stseuqer laitnesse gol ylno ot kcehcnu ro ,stseuqer ipa lla gol ot kcehc';
+$lang['Multicraft.row_meta.server_name'] = 'Lebal revres';
+$lang['Multicraft.row_meta.panel_url'] = 'Lru lenap';
+$lang['Multicraft.row_meta.panel_api_url'] = 'Lru ipa lenap';
+$lang['Multicraft.row_meta.username'] = 'Emanresu';
+$lang['Multicraft.row_meta.key'] = 'Yek ipa';
+$lang['Multicraft.row_meta.log_all'] = 'Stseuqer ipa lla gol';
+
