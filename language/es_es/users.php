8fdbb443aed408b2777b4a793d2d282001e8f7ef
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:14 -0800

    Updated es_es language files

---
+++ language/es_es/users.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Users
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Users.!error.username.empty'] = 'Emanresu a retne esaelp';
+$lang['Users.!error.username.unique'] = 'Nekat neeb ydaerla sah emanresu taht';
+$lang['Users.!error.current_password.matches'] = 'Drowssap dilavni';
+$lang['Users.!error.new_password.format'] = 'Htgnel ni sretcarahc 6 tsael ta drowssap a retne esaelp';
+$lang['Users.!error.new_password.matches'] = 'Hctam ton od sdrowssap eht';
+$lang['Users.!error.recovery_email.format'] = 'Sserdda liame yrevocer dilavni';
+$lang['Users.!error.two_factor_mode.format'] = 'Edom rotcaf owt dilavni';
+$lang['Users.!error.two_factor_key.format'] = 'Yek rotcaf owt dilavni';
+$lang['Users.!error.username.auth'] = 'Noitanibmoc drowssap/resu taht rof dnuof sehctam on';
+$lang['Users.!error.otp.auth'] = 'Sretcarahc 61 si htgnel mumixam .dilavni si deretne drowssap emit-eno eht';
+$lang['Users.!error.user_id.exists'] = 'Di resu dilavni';
+$lang['Users.!error.username.attempts'] = 'Detceted stpmetta nigol deliaf ynam oot';
+$lang['Users.!error.username.company'] = 'Noitacol siht ta nigol ot dezirohtua ton era uoy';
+$lang['Users.!error.clients.exist'] = 'Resu eht ot dengissa tneilc eno tsael ta si ereht esuaceb deteled eb tonnac resu eht';
+
