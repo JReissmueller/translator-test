786a6836afe1eb9f1433205f242fb606c20193dc
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:49 -0800

    Updated es_es language files

---
+++ language/es_es/message_groups.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Message Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MessageGroups.!error.action.empty'] = 'Noitca na retne esaelp';
+$lang['MessageGroups.!error.type.valid'] = '"derahs" ro "ffats" ,"tneilc" ot tes eb tsum epyt puorg egassem eht';
+$lang['MessageGroups.!error.plugin_dir.empty'] = 'Yrotcerid nigulp eht retne esaelp';
+$lang['MessageGroups.!error.tags.empty'] = 'Sgat eht retne esaelp';
+$lang['MessageGroups.getTypes.staff'] = 'Ffats';
+$lang['MessageGroups.getTypes.client'] = 'Tneilc';
+$lang['MessageGroups.getTypes.shared'] = 'Derahs';
+
