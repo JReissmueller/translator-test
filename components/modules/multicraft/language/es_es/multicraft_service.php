568c4be5d62cb4f98d2dc6eaf7ce9694b20a97c5
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:03 -0800

    Updated es_es language files

---
+++ components/modules/multicraft/language/es_es/multicraft_service.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Multicraft Service
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MulticraftService.!error.internal'] = 'Tseuqer eht ot dnopser ton did revres eht ro ,derrucco rorre lanretni na';
+$lang['MulticraftService.!error.client'] = 'Resu tfarcitlum a etaerc ot hcihw morf dnuof saw tneilc on';
+$lang['MulticraftService.!error.server_id.format'] = 'Knalb tfel ro ,regetni na eb tsum di revres eht';
+$lang['MulticraftService.!error.user_id.format'] = 'Knalb tfel ro ,regetni na eb tsum di resu eht';
+$lang['MulticraftService.!error.daemon_id.format'] = 'Knalb tfel ro ,regetni na eb tsum di nomead eht';
+$lang['MulticraftService.!error.ip.format'] = 'Knalb ti evael ro ,sserdda pi dilav a retne esaelp';
+$lang['MulticraftService.!error.port.format'] = 'Knalb ti evael ro ,rebmun trop dilav a retne esaelp';
+$lang['MulticraftService.service_fields.server_id'] = 'Di revres';
+$lang['MulticraftService.service_fields.tooltip.server_id'] = 'Esiwrehto knalb ti evael .dengissa eb ot si ecivres siht revres tfarcitlum eht fo di euqinu eht tes';
+$lang['MulticraftService.service_fields.user_id'] = 'Di resu';
+$lang['MulticraftService.service_fields.tooltip.user_id'] = 'Esiwrehto knalb ti evael .dengissa eb ot si ecivres siht resu tfarcitlum eht fo di euqinu eht tes';
+$lang['MulticraftService.service_fields.daemon_id'] = 'Di nomead';
+$lang['MulticraftService.service_fields.tooltip.daemon_id'] = 'Esiwrehto knalb ti evael .dengissa eb ot si ecivres siht nomead tfarcitlum eht fo di eht tes';
+$lang['MulticraftService.service_fields.ip'] = 'Sserdda pi';
+$lang['MulticraftService.service_fields.tooltip.ip'] = 'Esiwrehto knalb ti evael .dengissa eb ot si ecivres siht revres tfarcitlum eht fo sserdda pi eht tes';
+$lang['MulticraftService.service_fields.port'] = 'Trop';
+$lang['MulticraftService.service_fields.tooltip.port'] = 'Esiwrehto knalb ti evael .dengissa eb ot si ecivres siht revres tfarcitlum eht fo trop eht tes';
+$lang['MulticraftService.service_fields.yes'] = 'Sey';
+$lang['MulticraftService.service_fields.no'] = 'On';
+$lang['MulticraftService.!error.configoptions[user_players].format'] = '0 ro 1 ot tes eb tsum sreyalp fo rebmun eht tes nac renwo revres eht rehtehw';
+$lang['MulticraftService.!error.configoptions[players].format'] = 'Sreyalp fo rebmun eht tes esaelp';
+$lang['MulticraftService.!error.configoptions[memory].format'] = 'Yromem fo tnuoma dilav a tes esaelp';
+$lang['MulticraftService.!error.configoptions[daemon_id].format'] = 'Nomead dilav a tes esaelp';
+$lang['MulticraftService.!error.configoptions[dedicated_ip].format'] = '0 ro 1 ot tes eb tsum pi detacided a esu ot rehtehw';
+
