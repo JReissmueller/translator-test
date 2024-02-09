e98fc6bddbda16c45d3d13fe06cedffa078f1a98
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:17 -0800

    Updated es_es language files

---
+++ language/es_es/client_contacts.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Client Contacts
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientContacts.!success.contact_deleted'] = '!deteled yllufsseccus saw %2$s %1$s tcatnoc eht';
+$lang['ClientContacts.!success.contact_updated'] = '!detadpu yllufsseccus saw tcatnoc eht';
+$lang['ClientContacts.!success.contact_added'] = '!detaerc yllufsseccus saw tcatnoc eht';
+$lang['ClientContacts.!info.no_contacts'] = 'Woleb tcatnoc tsrif ruoy dda ,stcatnoc yna evah yltnerruc t\'nod uoy';
+$lang['ClientContacts.!info.unverified_email'] = '%1$s ot tnes neeb sah liame noitacifirev a .deifirev neeb ton sah tcatnoc siht ot detaicossa sserdda liame eht';
+$lang['ClientContacts.!info.unverified_email_button'] = 'Liame noitacifirev dneser';
+$lang['ClientContacts.index.page_title'] = 'Stcatnoc %1$s# tneilc';
+$lang['ClientContacts.index.create_contact'] = 'Tcatnoc dda';
+$lang['ClientContacts.index.boxtitle_contacts'] = 'Stcatnoc';
+$lang['ClientContacts.index.heading_name'] = 'Eman';
+$lang['ClientContacts.index.heading_email'] = 'Liame';
+$lang['ClientContacts.index.heading_type'] = 'Epyt';
+$lang['ClientContacts.index.heading_options'] = 'Snoitpo';
+$lang['ClientContacts.index.option_edit'] = 'Tide';
+$lang['ClientContacts.index.option_delete'] = 'Eteled';
+$lang['ClientContacts.index.confirm_delete'] = '?tcatnoc siht eteled ot tnaw uoy erus uoy era';
+$lang['ClientContacts.index.no_results'] = 'Evoba nottub tcatnoc dda eht kcilc ,tcatnoc tsrif ruoy dda ot .stcatnoc on evah uoy';
+$lang['ClientContacts.add.heading_contact'] = 'Noitamrofni tcatnoc';
+$lang['ClientContacts.add.page_title'] = 'Tcatnoc dda %1$s# tneilc';
+$lang['ClientContacts.add.boxtitle_create'] = 'Tcatnoc dda';
+$lang['ClientContacts.add.heading_settings'] = 'Sgnittes lanoitidda';
+$lang['ClientContacts.add.field_contact_type'] = 'Epyt tcatnoc';
+$lang['ClientContacts.add.field_addsubmit'] = 'Tcatnoc etaerc';
+$lang['ClientContacts.add.heading_authentication'] = 'Noitacitnehtua';
+$lang['ClientContacts.add.field_enable_login'] = 'Nigol elbane';
+$lang['ClientContacts.add.field_username'] = 'Emanresu';
+$lang['ClientContacts.add.field_new_password'] = 'Drowssap wen';
+$lang['ClientContacts.add.field_confirm_password'] = 'Drowssap mrifnoc';
+$lang['ClientContacts.add.field_recovery_email'] = ')lanoitpo( liame yrevocer';
+$lang['ClientContacts.add.heading_permissions'] = 'Snoissimrep';
+$lang['ClientContacts.edit.heading_contact'] = 'Noitamrofni tcatnoc';
+$lang['ClientContacts.edit.page_title'] = 'Tcatnoc tide %1$s# tneilc';
+$lang['ClientContacts.edit.boxtitle_edit'] = 'Tcatnoc tide';
+$lang['ClientContacts.edit.heading_settings'] = 'Sgnittes lanoitidda';
+$lang['ClientContacts.edit.field_contact_type'] = 'Epyt tcatnoc';
+$lang['ClientContacts.edit.field_editsubmit'] = 'Tcatnoc etadpu';
+$lang['ClientContacts.edit.heading_authentication'] = 'Noitacitnehtua';
+$lang['ClientContacts.edit.field_enable_login'] = 'Nigol elbane';
+$lang['ClientContacts.edit.field_username'] = 'Emanresu';
+$lang['ClientContacts.edit.field_new_password'] = 'Drowssap wen';
+$lang['ClientContacts.edit.field_confirm_password'] = 'Drowssap mrifnoc';
+$lang['ClientContacts.edit.field_recovery_email'] = ')lanoitpo( liame yrevocer';
+$lang['ClientContacts.edit.heading_permissions'] = 'Snoissimrep';
+$lang['ClientContacts.tooltip.client_invoices'] = 'Strela rednimer tnemyap yna sa llew sa ,draobhsad eht no seciovni yalpsid';
+$lang['ClientContacts.tooltip.client_quotations'] = 'Deined ro devorppa eb ot meht wolla dna ,draobhsad eht no snoitatouq yalpsid';
+$lang['ClientContacts.tooltip.client_services'] = 'Deganam eb ot meht wolla dna ,draobhsad eht no secivres yalpsid';
+$lang['ClientContacts.tooltip.client_transactions'] = 'Draobhsad eht no snoitcasnart yalpsid';
+$lang['ClientContacts.tooltip.client_contacts'] = 'Deganam eb ot stcatnoc wolla';
+$lang['ClientContacts.tooltip.client_accounts'] = 'Deganam eb ot stnuocca tnemyap wolla';
+$lang['ClientContacts.tooltip.client_emails'] = 'Yrotsih liame fo tsil a yalpsid';
+$lang['ClientContacts.tooltip._invoice_delivery'] = 'Degnahc dna deweiv eb ot dohtem yreviled eciovni eht wolla';
+$lang['ClientContacts.tooltip._credits'] = 'Deweiv eb ot stiderc tnuocca eht wolla';
+$lang['ClientContacts.setcontactview.text_none'] = 'Enon';
+$lang['ClientContacts.contact_info.heading_contact'] = 'Noitamrofni tcatnoc';
+$lang['ClientContacts.contact_info.text_select_contact'] = 'Mrof siht etalupop-erp ot tcatnoc gnitsixe na tceles yam uoy';
+$lang['ClientContacts.contact_info.field_contact_id'] = 'Morf noitamrofni tcatnoc ypoc';
+$lang['ClientContacts.contact_info.field_first_name'] = 'Eman tsrif';
+$lang['ClientContacts.contact_info.field_last_name'] = 'Eman tsal';
+$lang['ClientContacts.contact_info.field_company'] = 'Ynapmoc';
+$lang['ClientContacts.contact_info.field_title'] = 'Eltit';
+$lang['ClientContacts.contact_info.field_address1'] = '1 sserdda';
+$lang['ClientContacts.contact_info.field_address2'] = '2 sserdda';
+$lang['ClientContacts.contact_info.field_city'] = 'Ytic';
+$lang['ClientContacts.contact_info.field_country'] = 'Yrtnuoc';
+$lang['ClientContacts.contact_info.field_state'] = 'Etats';
+$lang['ClientContacts.contact_info.field_zip'] = 'Edoc latsop/piz';
+$lang['ClientContacts.contact_info.field_email'] = 'Liame';
+$lang['ClientContacts.phone_numbers.heading_phone'] = 'Srebmun enohp';
+$lang['ClientContacts.phone_numbers.categorylink_number'] = 'Lanoitidda dda';
+$lang['ClientContacts.phone_numbers.field_phonetype'] = 'Epyt';
+$lang['ClientContacts.phone_numbers.field_phonelocation'] = 'Noitacol';
+$lang['ClientContacts.phone_numbers.field_phonenumber'] = 'Rebmun';
+$lang['ClientContacts.phone_numbers.field_phoneoptions'] = 'Snoitpo';
+$lang['ClientContacts.phone_numbers.text_remove'] = 'Evomer';
+$lang['ClientContacts.navigation.nav_contacts'] = 'Stcatnoc';
+$lang['ClientContacts.navigation.nav_contacts_add'] = 'Tcatnoc dda';
+$lang['ClientContacts.navigation.nav_return'] = 'Draobhsad ot nruter';
+
