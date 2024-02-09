09f50280fa65d9bdfe65a1eca966a6ed82056480
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:52 -0800

    Updated es_es language files

---
+++ components/modules/direct_admin/language/es_es/direct_admin.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Direct Admin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DirectAdmin.!error.server_name.empty'] = 'Lebal revres a retne tsum uoy';
+$lang['DirectAdmin.!error.host_name.format'] = 'Dilavni eb ot sraeppa eman tsoh eht';
+$lang['DirectAdmin.!error.port.format'] = 'Rebmun a eb tsum trop eht';
+$lang['DirectAdmin.!error.user_name.empty'] = 'Eman resu a retne tsum uoy';
+$lang['DirectAdmin.!error.password.format'] = 'Drowssap a retne tsum uoy';
+$lang['DirectAdmin.!error.use_ssl.format'] = 'Eslaf ro eurt rehtie eb tsum lss esu';
+$lang['DirectAdmin.!error.account_limit.valid'] = 'Eulav regetni emos ot tes ro )stnuocca detimilnu rof( knalb tfel eb tsum timil tnuocca';
+$lang['DirectAdmin.!error.name_servers.valid'] = 'Dilavni era deretne srevres eman eht fo erom ro eno';
+$lang['DirectAdmin.!error.name_servers.count'] = 'Srevres eman 2 tsael ta enifed tsum uoy';
+$lang['DirectAdmin.!error.api.internal'] = 'Tseuqer eht ot dnopser ton did revres eht ro ,derrucco rorre lanretni na';
+$lang['DirectAdmin.!error.meta[type].format'] = 'Relleser ro resu rehtie eb tsum epyt tnuocca';
+$lang['DirectAdmin.!error.meta[package].empty'] = 'Deriuqer si egakcap nimdatcerid a';
+$lang['DirectAdmin.!error.meta[ip].empty'] = 'Deriuqer si sserdda pi na';
+$lang['DirectAdmin.!error.direct_admin_domain.format'] = 'Moc.niamod :mrof eht fo eman niamod dilav a retne esaelp';
+$lang['DirectAdmin.!error.direct_admin_username.format'] = 'Sretcarahc ciremunahpla niatnoc ylno yam emanresu eht';
+$lang['DirectAdmin.!error.direct_admin_username.length'] = 'Htgnel ni sretcarahc 8 dna 4 neewteb eb tsum emanresu eht';
+$lang['DirectAdmin.!error.direct_admin_password.format'] = '}|{ `_^][@?>=<;:/.-,+*)(\'&%$#"! :tes eht morf retcarahc laiceps 1 tsael ta dna ,9-0 rebmun 1 tsael ta ,z-a retcarahc esac-rewol 1 tsael ta ,z-a retcarahc esac-reppu 1 tsael ta ,sretcarahc 21 tsael ta :gniwollof eht sniatnoc drowssap eht erus ekam';
+$lang['DirectAdmin.!error.direct_admin_password.matches'] = 'Hctam ton od drowssap mrifnoc dna drowssap';
+$lang['DirectAdmin.!error.direct_admin_email.format'] = 'Sserdda liame dilav a retne esaelp';
+$lang['DirectAdmin.!error.change_package.type'] = 'Segakcap relleser dna resu neewteb gnignahc troppus ton seod eludom eht';
+$lang['DirectAdmin.name'] = 'Nimdatcerid';
+$lang['DirectAdmin.description'] = 'Reisae setisbew fo noitartsinimda ekam ot dengised lenap lortnoc gnitsoh bew desab-bew lacihparg a si nimdatcerid';
+$lang['DirectAdmin.module_row'] = 'Revres';
+$lang['DirectAdmin.module_row_plural'] = 'Srevres';
+$lang['DirectAdmin.module_group'] = 'Puorg revres';
+$lang['DirectAdmin.tab_stats'] = 'Scitsitats';
+$lang['DirectAdmin.tab_client_actions'] = 'Snoitca';
+$lang['DirectAdmin.order_options.first'] = 'Revres lluf-non tsrif';
+$lang['DirectAdmin.order_options.roundrobin'] = 'Srevres gnoma etubirtsid ylneve';
+$lang['DirectAdmin.servers.no_results'] = 'Srevres on era ereht';
+$lang['DirectAdmin.server_groups.no_results'] = 'Spuorg revres on era ereht';
+$lang['DirectAdmin.service_field.domain'] = 'Niamod';
+$lang['DirectAdmin.service_field.username'] = 'Emanresu';
+$lang['DirectAdmin.service_field.password'] = 'Drowssap';
+$lang['DirectAdmin.service_field.email'] = 'Liame';
+$lang['DirectAdmin.service_field.text_generate_password'] = 'Drowssap etareneg';
+$lang['DirectAdmin.tab_client_actions.change_password'] = 'Drowssap egnahc';
+$lang['DirectAdmin.tab_client_actions.field_direct_admin_password'] = 'Drowssap';
+$lang['DirectAdmin.tab_client_actions.field_direct_admin_confirm_password'] = 'Drowssap mrifnoc';
+$lang['DirectAdmin.tab_client_actions.field_password_submit'] = 'Drowssap etadpu';
+$lang['DirectAdmin.tab_client_actions.text_generate_password'] = 'Drowssap etareneg';
+$lang['DirectAdmin.service_info.username'] = 'Emanresu';
+$lang['DirectAdmin.service_info.password'] = 'Drowssap';
+$lang['DirectAdmin.service_info.server'] = 'Revres';
+$lang['DirectAdmin.service_info.options'] = 'Snoitpo';
+$lang['DirectAdmin.service_info.option_login'] = 'Ni gol';
+$lang['DirectAdmin.package_fields.type'] = 'Epyt tnuocca';
+$lang['DirectAdmin.package_fields.type_user'] = 'Resu';
+$lang['DirectAdmin.package_fields.type_reseller'] = 'Relleser';
+$lang['DirectAdmin.package_fields.package'] = 'Egakcap nimdatcerid';
+$lang['DirectAdmin.package_fields.ip'] = 'Sserdda pi';
+$lang['DirectAdmin.package_fields.ip_shared'] = 'Derahs';
+$lang['DirectAdmin.package_fields.ip_assign'] = 'Ngissa';
+$lang['DirectAdmin.add_module_row'] = 'Revres dda';
+$lang['DirectAdmin.add_module_group'] = 'Puorg revres dda';
+$lang['DirectAdmin.manage.module_rows_title'] = 'Srevres';
+$lang['DirectAdmin.manage.module_groups_title'] = 'Spuorg revres';
+$lang['DirectAdmin.manage.module_rows_heading.name'] = 'Lebal revres';
+$lang['DirectAdmin.manage.module_rows_heading.host_name'] = 'Eman tsoh';
+$lang['DirectAdmin.manage.module_rows_heading.accounts'] = 'Stnuocca';
+$lang['DirectAdmin.manage.module_rows_heading.options'] = 'Snoitpo';
+$lang['DirectAdmin.manage.module_groups_heading.name'] = 'Eman puorg';
+$lang['DirectAdmin.manage.module_groups_heading.servers'] = 'Tnuoc revres';
+$lang['DirectAdmin.manage.module_groups_heading.options'] = 'Snoitpo';
+$lang['DirectAdmin.manage.module_rows.count'] = '%2$s / %1$s';
+$lang['DirectAdmin.manage.module_rows.edit'] = 'Tide';
+$lang['DirectAdmin.manage.module_groups.edit'] = 'Tide';
+$lang['DirectAdmin.manage.module_rows.delete'] = 'Eteled';
+$lang['DirectAdmin.manage.module_groups.delete'] = 'Eteled';
+$lang['DirectAdmin.manage.module_rows.confirm_delete'] = '?revres siht eteled ot tnaw uoy erus uoy era';
+$lang['DirectAdmin.manage.module_groups.confirm_delete'] = '?puorg revres siht eteled ot tnaw uoy erus uoy era';
+$lang['DirectAdmin.row_meta.server_name'] = 'Lebal revres';
+$lang['DirectAdmin.row_meta.host_name'] = 'Eman tsoh';
+$lang['DirectAdmin.row_meta.port'] = 'Trop';
+$lang['DirectAdmin.row_meta.default_port'] = '2222';
+$lang['DirectAdmin.row_meta.user_name'] = 'Eman resu';
+$lang['DirectAdmin.row_meta.password'] = 'Drowssap';
+$lang['DirectAdmin.row_meta.use_ssl'] = ')dednemmocer( ipa eht ot gnitcennoc nehw lss esu';
+$lang['DirectAdmin.row_meta.account_limit'] = 'Timil tnuocca';
+$lang['DirectAdmin.row_meta.name_server'] = '%1$s revres eman';
+$lang['DirectAdmin.add_row.box_title'] = 'Revres nimdatcerid dda';
+$lang['DirectAdmin.add_row.basic_title'] = 'Sgnittes cisab';
+$lang['DirectAdmin.add_row.name_servers_title'] = 'Srevres eman';
+$lang['DirectAdmin.add_row.name_server_btn'] = 'Revres eman lanoitidda dda';
+$lang['DirectAdmin.add_row.name_server_col'] = 'Revres eman';
+$lang['DirectAdmin.add_row.name_server_host_col'] = 'Emantsoh';
+$lang['DirectAdmin.add_row.remove_name_server'] = 'Evomer';
+$lang['DirectAdmin.add_row.notes_title'] = 'Seton';
+$lang['DirectAdmin.add_row.add_btn'] = 'Revres dda';
+$lang['DirectAdmin.edit_row.box_title'] = 'Revres nimdatcerid tide';
+$lang['DirectAdmin.edit_row.basic_title'] = 'Sgnittes cisab';
+$lang['DirectAdmin.edit_row.name_servers_title'] = 'Srevres eman';
+$lang['DirectAdmin.edit_row.name_server_btn'] = 'Revres eman lanoitidda dda';
+$lang['DirectAdmin.edit_row.name_server_col'] = 'Revres eman';
+$lang['DirectAdmin.edit_row.name_server_host_col'] = 'Emantsoh';
+$lang['DirectAdmin.edit_row.remove_name_server'] = 'Evomer';
+$lang['DirectAdmin.edit_row.notes_title'] = 'Seton';
+$lang['DirectAdmin.edit_row.add_btn'] = 'Revres tide';
+$lang['DirectAdmin.tab_client_stats'] = 'Scitsitats';
+$lang['DirectAdmin.tab_client_stats.info_title'] = 'Noitamrofni';
+$lang['DirectAdmin.tab_client_stats.info_heading.field'] = 'Dleif';
+$lang['DirectAdmin.tab_client_stats.info_heading.value'] = 'Eulav';
+$lang['DirectAdmin.tab_client_stats.no_results'] = 'Elbaliavanu yltnerruc si noitamrofni lacitsitats';
+
