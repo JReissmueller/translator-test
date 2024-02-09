5d8f8fba2db270ab941d19449c37e55f0b543fdf
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:07 -0800

    Updated es_es language files

---
+++ language/es_es/staff.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Staff
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Staff.!error.user_id.exists'] = 'Di resu dilavni';
+$lang['Staff.!error.user_id.unique'] = 'Nekat ydaerla si %1$s di resu ffats';
+$lang['Staff.!error.first_name.empty'] = 'Eman tsrif a retne esaelp';
+$lang['Staff.!error.last_name.empty'] = 'Eman tsal a retne esaelp';
+$lang['Staff.!error.email.format'] = 'Sserdda liame dilav a retne esaelp';
+$lang['Staff.!error.status.format'] = 'Sutats dilavni';
+$lang['Staff.!error.groups[].format'] = 'S\'di puorg dilavni erom ro eno era ereht';
+$lang['Staff.!error.groups.unique_company'] = 'Rebmem ffats a ot dengissa eb yam ynapmoc rep puorg ffats eno ylno';
+$lang['Staff.!error.staff_id.format'] = 'Rebmun a eb tsum di ffats eht';
+$lang['Staff.!error.company_id.format'] = 'Rebmun a eb tsum di ynapmoc eht';
+$lang['Staff.!error.uri.empty'] = 'Iru a retne esaelp';
+$lang['Staff.!error.title.empty'] = 'Eltit a retne esaelp';
+$lang['Staff.!error.order.format'] = 'Rebmun a eb tsum redro tros eht';
+$lang['Staff.!error.order.length'] = 'Sretcarahc 5 deecxe ton yam htgnel redro tros eht';
+$lang['Staff.!error.staff_group_id.exists'] = 'Di puorg ffats dilavni';
+$lang['Staff.!error.staff_id.exists'] = 'Di ffats dilavni';
+$lang['Staff.!error.action.exists'] = 'Tsixe ton seod %1$s noitca puorg liame eht';
+$lang['Staff.!error.action[].exists'] = 'Tsixe ton seod snoitca puorg liame eht fo eno tsael ta';
+
