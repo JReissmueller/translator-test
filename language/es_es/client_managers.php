b46e5a0efcce9a61e12d9deeb8bb2de7515d3973
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:24 -0800

    Updated es_es language files

---
+++ language/es_es/client_managers.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Client Managers
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientManagers.!success.manager_invited'] = 'Drocer no resu gnihctam eht ot tnes yllufsseccus saw noitativni na';
+$lang['ClientManagers.!success.manager_revoked'] = '!yllufsseccus dekover neeb sah reganam eht';
+$lang['ClientManagers.!success.manager_updated'] = 'Yllufsseccus detadpu saw reganam eht';
+$lang['ClientManagers.!success.account_gave_up'] = '!yllufsseccus dekover neeb sah tnuocca %1$s eht ot ssecca ruoy';
+$lang['ClientManagers.!success.invitation_accepted'] = '.yllufsseccus noitativni eht detpecca evah uoy';
+$lang['ClientManagers.!success.invitation_declined'] = 'Yllufsseccus noitativni eht denilced evah uoy';
+$lang['ClientManagers.navigation.nav_managers'] = 'Sreganam detivni';
+$lang['ClientManagers.navigation.nav_managed_accounts'] = 'Stnuocca deganam';
+$lang['ClientManagers.navigation.nav_return'] = 'Draobhsad ot nruter';
+$lang['ClientManagers.index.boxtitle_managers'] = 'Sreganam detivni';
+$lang['ClientManagers.index.add_manager'] = 'Reganam dda';
+$lang['ClientManagers.index.heading_client_id'] = '# tneilc';
+$lang['ClientManagers.index.heading_email'] = 'Liame';
+$lang['ClientManagers.index.heading_name'] = 'Eman lluf';
+$lang['ClientManagers.index.heading_company'] = 'Ynapmoc';
+$lang['ClientManagers.index.heading_status'] = 'Sutats';
+$lang['ClientManagers.index.heading_options'] = 'Snoitpo';
+$lang['ClientManagers.index.text_status_accepted'] = 'Detpecca';
+$lang['ClientManagers.index.text_status_pending'] = 'Gnidnep';
+$lang['ClientManagers.index.option_edit'] = 'Tide';
+$lang['ClientManagers.index.option_revoke'] = 'Ekover';
+$lang['ClientManagers.index.confirm_revoke'] = '?tnuocca siht ot ssecca ekover ot tnaw uoy erus uoy era';
+$lang['ClientManagers.index.no_results'] = 'Tnuocca siht eganam ot sresu yna detivni ton evah uoy';
+$lang['ClientManagers.add.boxtitle_add_manager'] = 'Reganam dda';
+$lang['ClientManagers.add.field_addsubmit'] = 'Reganam dda';
+$lang['ClientManagers.add.heading_invite_manager'] = 'Reganam etivni';
+$lang['ClientManagers.add.field_email'] = 'Sserdda liame';
+$lang['ClientManagers.tooltip.field_email'] = '%1$s ni deretsiger tnuocca na ot detaicossa sserdda liam-e eht';
+$lang['ClientManagers.add.heading_permissions'] = 'Snoissimrep';
+$lang['ClientManagers.tooltip.client_invoices'] = 'Strela rednimer tnemyap yna sa llew sa ,draobhsad eht no seciovni yalpsid';
+$lang['ClientManagers.tooltip.client_quotations'] = 'Deined ro devorppa eb ot meht wolla dna ,draobhsad eht no snoitatouq yalpsid';
+$lang['ClientManagers.tooltip.client_services'] = 'Deganam eb ot meht wolla dna ,draobhsad eht no secivres yalpsid';
+$lang['ClientManagers.tooltip.client_transactions'] = 'Draobhsad eht no snoitcasnart yalpsid';
+$lang['ClientManagers.tooltip.client_contacts'] = 'Deganam eb ot stcatnoc wolla';
+$lang['ClientManagers.tooltip.client_accounts'] = 'Deganam eb ot stnuocca tnemyap wolla';
+$lang['ClientManagers.tooltip.client_emails'] = 'Yrotsih liame fo tsil a yalpsid';
+$lang['ClientManagers.tooltip._managed'] = 'Deganam eb ot ,ot detivni neeb evah uoy taht stnuocca eht wolla';
+$lang['ClientManagers.tooltip._invoice_delivery'] = 'Degnahc dna deweiv eb ot dohtem yreviled eciovni eht wolla';
+$lang['ClientManagers.tooltip._credits'] = 'Deweiv eb ot stiderc tnuocca eht wolla';
+$lang['ClientManagers.edit.boxtitle_edit_manager'] = 'Reganam tide';
+$lang['ClientManagers.edit.field_addsubmit'] = 'Reganam tide';
+$lang['ClientManagers.edit.heading_permissions'] = 'Snoissimrep';
+$lang['ClientManagers.accounts.boxtitle_managed_accounts'] = 'Stnuocca deganam';
+$lang['ClientManagers.accounts.heading_client_id'] = '# tneilc';
+$lang['ClientManagers.accounts.heading_email'] = 'Liame';
+$lang['ClientManagers.accounts.heading_name'] = 'Eman lluf';
+$lang['ClientManagers.accounts.heading_company'] = 'Ynapmoc';
+$lang['ClientManagers.accounts.heading_options'] = 'Snoitpo';
+$lang['ClientManagers.accounts.option_manage'] = 'Eganam';
+$lang['ClientManagers.accounts.option_give_up_access'] = 'Ssecca pu evig';
+$lang['ClientManagers.accounts.confirm_give_up'] = '?tnuocca siht ot ssecca ruoy pu evig ot tnaw uoy erus uoy era';
+$lang['ClientManagers.accounts.no_results'] = 'Stnuocca stseug yna gniganam yltnerruc ton era uoy';
+$lang['ClientManagers.invite.boxtitle_management_invitation'] = 'Noitativni tnemeganam';
+$lang['ClientManagers.invite.text_invitation'] = 'Ti enilced ylefas nac uoy ekatsim yb noitativni siht deviecer evah uoy fi ,tnuocca eht eganam dna ssecca ot elba eb lliw uoy noitativni siht tpecca uoy fi .tnuocca na eganam ot uoy detivni sah %2$s %1$s';
+$lang['ClientManagers.invite.field_accept'] = 'Tpecca';
+$lang['ClientManagers.invite.field_decline'] = 'Enilced';
+
