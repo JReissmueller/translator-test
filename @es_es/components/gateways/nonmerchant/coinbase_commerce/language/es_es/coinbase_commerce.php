af773d7b61c40a552f456924e8a5853e4385e96b
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:04:19 -0800

    Updated es_es language files

--- @es_es/components/gateways/nonmerchant/coinbase_commerce/language/es_es/coinbase_commerce.php
+++ @es_es/components/gateways/nonmerchant/coinbase_commerce/language/es_es/coinbase_commerce.php
@@ -9,13 +9,13 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['CoinbaseCommerce.name'] = 'Coinbase Comercio';
-$lang['CoinbaseCommerce.description'] = 'Coinbase Commerce te permite integrar pagos en criptomoneda en tu negocio, y dejar que los clientes te paguen directamente desde su ordenador o dispositivo móvil utilizando la blockchain.';
-$lang['CoinbaseCommerce.!error.api_key.valid'] = 'Clave API inválida.';
-$lang['CoinbaseCommerce.!error.webhook_secret.valid'] = 'Webhook Secreto inválido.';
-$lang['CoinbaseCommerce.meta.api_key'] = 'Clave API';
-$lang['CoinbaseCommerce.meta.webhook_secret'] = 'Secreto de Webhook';
-$lang['CoinbaseCommerce.webhook'] = 'Webhook de Coinbase Commerce';
-$lang['CoinbaseCommerce.webhook_note'] = 'Antes de empezar a utilizar esta pasarela, debes configurar la siguiente url como Webhook en tu cuenta Coinbase Commerce y establecer Webhook Secret.';
-$lang['CoinbaseCommerce.buildprocess.submit'] = 'Pagar con Coinbase';
+$lang['CoinbaseCommerce.name'] = 'Ecremmoc esabnioc';
+$lang['CoinbaseCommerce.description'] = 'Niahckcolb eht gnisu ecived elibom ro retupmoc rieht morf yltcerid uoy yap sremotsuc tel dna ,ssenisub ruoy otni stnemyap ycnerrucotpyrc etargetni uoy stel ecremmoc esabnioc';
+$lang['CoinbaseCommerce.!error.api_key.valid'] = 'Dilavni yek ipa';
+$lang['CoinbaseCommerce.!error.webhook_secret.valid'] = 'Dilavni terces koohbew';
+$lang['CoinbaseCommerce.meta.api_key'] = 'Yek ipa';
+$lang['CoinbaseCommerce.meta.webhook_secret'] = 'Terces koohbew';
+$lang['CoinbaseCommerce.webhook'] = 'Koohbew ecremmoc esabnioc';
+$lang['CoinbaseCommerce.webhook_note'] = 'Terces koohbew tes dna tnuocca ecremmoc esabnioc ruoy ni koohbew a sa lru gniwollof eht erugifnoc tsum uoy yawetag siht gnisu trats uoy erofeb';
+$lang['CoinbaseCommerce.buildprocess.submit'] = 'Esabnioc htiw yap';
 
