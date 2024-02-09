e6f117f7b6fc6cb7a9730e0ccc6f5583829a724d
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:12 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/converge/language/es_es/converge.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Converge
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Converge.name'] = 'Egrevnoc';
+$lang['Converge.description'] = 'Tnahcremlautriv ylremrof';
+$lang['Converge.login_id'] = 'Di nigol';
+$lang['Converge.transaction_key'] = 'Yek noitcasnart';
+$lang['Converge.api'] = 'Ipa tnemyap';
+$lang['Converge.merchant_id'] = 'Di tnahcrem';
+$lang['Converge.user_id'] = 'Di resu';
+$lang['Converge.pin'] = 'Nip';
+$lang['Converge.live_mode'] = 'Edom evil';
+$lang['Converge.multiCurrency'] = 'Ycnerruc itlum rof putes si tnuocca egrevnoc';
+$lang['Converge.yes'] = 'Sey';
+$lang['Converge.no'] = 'On';
+$lang['Converge.multiCurrency.error'] = 'Ton ro ycnerruc itlum rof putes si tnuocca egrevnoc ruoy rehtehw yificeps tsum uoy';
+$lang['Converge.!error.login_id.format'] = 'Ytpme eb ton yam dna sretcarahc 02 naht erom on eb dluohs di nigol';
+$lang['Converge.!error.transaction_key.format'] = 'Htgnel ni sretcarahc 61 eb tsum yek noitcasnart eht';
+$lang['Converge.!error.live_mode.valid'] = 'Nevig fi "eurt" ot tes eb tsum edom evil';
+
