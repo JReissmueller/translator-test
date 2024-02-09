b67cb06d7e84ee3f4d8ba32dfe0c74f938cd9f41
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:37 -0800

    Updated es_es language files

---
+++ language/es_es/email_groups.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Email Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['EmailGroups.!error.action.empty'] = 'Noitca na retne esaelp';
+$lang['EmailGroups.!error.action.length'] = 'Sretcarahc 46 deecxe ton yam htgnel noitca';
+$lang['EmailGroups.!error.action.unique'] = 'Stsixe ydaerla eman taht yb noitca na';
+$lang['EmailGroups.!error.type.format'] = 'Epyt dilavni';
+$lang['EmailGroups.!error.notice_type.valid'] = 'Ot ro ccb rehtie ot tes eb tsum epyt eciton eht';
+$lang['EmailGroups.!error.group_id.exists'] = 'Di puorg liame dilavni';
+
