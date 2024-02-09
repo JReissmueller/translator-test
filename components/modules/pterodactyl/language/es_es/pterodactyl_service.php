457a8ef04d9896b58d32342b59917b5dd182d34a
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:13 -0800

    Updated es_es language files

---
+++ components/modules/pterodactyl/language/es_es/pterodactyl_service.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Pterodactyl Service
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PterodactylService.!error.server_name.empty'] = 'Revres eht rof eman a tes esaelp';
+$lang['PterodactylService.service_fields.server_name'] = 'Eman revres';
+$lang['PterodactylService.service_fields.tooltip.server_name'] = 'Revres eht fo eman eht';
+$lang['PterodactylService.service_fields.server_description'] = ')lanoitpo( noitpircsed revres';
+$lang['PterodactylService.service_fields.tooltip.server_description'] = 'Revres eht fo noitpircsed eht';
+$lang['PterodactylService.service_fields.server_id'] = ')lanoitpo( di revres';
+$lang['PterodactylService.service_fields.tooltip.server_id'] = 'Revres tnetsixe-erp a ot ecivres siht gniknil era uoy nehw desu eb ylno dluohs siht .lenap lytcadoretp eht ni revres eht fo di eht';
+$lang['PterodactylService.service_fields.optional'] = ')lanoitpo( %1$s';
+
