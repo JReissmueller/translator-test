d5609a304df913edd6404d8191d54296a1b0fdc3
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:50 -0800

    Updated es_es language files

---
+++ language/es_es/admin_company_modules.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Admin Company Modules
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyModules.!success.upgraded'] = 'Dedargpu yllufsseccus saw eludom eht';
+$lang['AdminCompanyModules.!success.installed'] = 'Dellatsni yllufsseccus saw eludom eht';
+$lang['AdminCompanyModules.!success.uninstalled'] = 'Dellatsninu yllufsseccus saw eludom eht';
+$lang['AdminCompanyModules.!success.module_updated'] = 'Detadpu yllufsseccus saw eludom eht';
+$lang['AdminCompanyModules.!success.group_added'] = 'Dedda yllufsseccus saw puorg eht';
+$lang['AdminCompanyModules.!success.group_updated'] = 'Detadpu yllufsseccus saw puorg eht';
+$lang['AdminCompanyModules.!success.group_deleted'] = 'Deteled yllufsseccus saw puorg eht';
+$lang['AdminCompanyModules.!success.row_added'] = 'Dedda yllufsseccus saw %1$s eht';
+$lang['AdminCompanyModules.!success.row_updated'] = 'Detadpu yllufsseccus saw %1$s eht';
+$lang['AdminCompanyModules.!success.row_deleted'] = 'Deteled yllufsseccus saw %1$s eht';
+$lang['AdminCompanyModules.!tab.installed'] = 'Dellatsni';
+$lang['AdminCompanyModules.!tab.available'] = 'Elbaliava';
+$lang['AdminCompanyModules.available.page_title'] = 'Elbaliava > seludom > ynapmoc > sgnittes';
+$lang['AdminCompanyModules.available.boxtitle_module'] = 'Seludom';
+$lang['AdminCompanyModules.available.text_author'] = 'Rohtua';
+$lang['AdminCompanyModules.available.text_author_url'] = 'Lru rohtua';
+$lang['AdminCompanyModules.available.text_version'] = ')%1$s rev(';
+$lang['AdminCompanyModules.available.text_none'] = 'Seludom elbaliava on era ereht';
+$lang['AdminCompanyModules.available.btn_install'] = 'Llatsni';
+$lang['AdminCompanyModules.installed.page_title'] = 'Dellatsni > seludom > ynapmoc > sgnittes';
+$lang['AdminCompanyModules.installed.boxtitle_installed'] = 'Seludom';
+$lang['AdminCompanyModules.installed.text_author'] = 'Rohtua';
+$lang['AdminCompanyModules.installed.text_author_url'] = 'Lru rohtua';
+$lang['AdminCompanyModules.installed.text_version'] = ')%1$s rev(';
+$lang['AdminCompanyModules.installed.text_none'] = 'Seludom dellatsni on era ereht';
+$lang['AdminCompanyModules.installed.btn_uninstall'] = 'Llatsninu';
+$lang['AdminCompanyModules.installed.btn_manage'] = 'Eganam';
+$lang['AdminCompanyModules.installed.btn_upgrade'] = 'Edargpu';
+$lang['AdminCompanyModules.installed.confirm_uninstall'] = '?eludom siht llatsninu ot tnaw uoy erus uoy era';
+$lang['AdminCompanyModules.manage.page_title'] = '%1$s eganam > seludom > ynapmoc > sgnittes';
+$lang['AdminCompanyModules.manage.boxtitle_manage'] = '%1$s eganam';
+$lang['AdminCompanyModules.addgroup.page_title'] = 'Puorg %2$s %1$s dda > seludom > ynapmoc > sgnittes';
+$lang['AdminCompanyModules.addgroup.boxtitle_add'] = 'Puorg %2$s %1$s dda';
+$lang['AdminCompanyModules.addgroup.btn_add'] = 'Puorg dda';
+$lang['AdminCompanyModules.addgroup.assigned'] = '%1$s dengissa';
+$lang['AdminCompanyModules.addgroup.available'] = '%1$s elbaliava';
+$lang['AdminCompanyModules.addgroup.force_limits'] = 'Stimil %1$s ecrof';
+$lang['AdminCompanyModules.addgroup.tooltip_force_limits'] = 'Timil sti sedeecxe reven %1$s a fo tnuoc eht taht os stimil drah secrofne';
+$lang['AdminCompanyModules.addgroup.text_name'] = 'Eman puorg';
+$lang['AdminCompanyModules.addgroup.text_order'] = 'Redro dda';
+$lang['AdminCompanyModules.editgroup.page_title'] = 'Puorg %2$s %1$s tide > seludom > ynapmoc > sgnittes';
+$lang['AdminCompanyModules.editgroup.boxtitle_edit'] = 'Puorg %2$s %1$s tide';
+$lang['AdminCompanyModules.editgroup.btn_edit'] = 'Puorg etadpu';
+$lang['AdminCompanyModules.editgroup.assigned'] = '%1$s dengissa';
+$lang['AdminCompanyModules.editgroup.available'] = '%1$s elbaliava';
+$lang['AdminCompanyModules.editgroup.force_limits'] = 'Stimil %1$s ecrof';
+$lang['AdminCompanyModules.editgroup.tooltip_force_limits'] = 'Timil sti sedeecxe reven %1$s a fo tnuoc eht taht os stimil drah secrofne';
+$lang['AdminCompanyModules.editgroup.text_name'] = 'Eman puorg';
+$lang['AdminCompanyModules.editgroup.text_order'] = 'Redro dda';
+$lang['AdminCompanyModules.addrow.page_title'] = '%2$s %1$s dda > seludom > ynapmoc > sgnittes';
+$lang['AdminCompanyModules.editrow.page_title'] = '%2$s %1$s tide > seludom > ynapmoc > sgnittes';
+
