8c1afe67e4ce250a497a81d995c4eb48fbc122af
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:12 -0800

    Updated es_es language files

---
+++ language/es_es/app_controller.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * App Controller
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AppController.lang.dir'] = 'Rtl';
+$lang['AppController.!error.unauthorized_access'] = 'Ecruoser taht ssecca ot dezirohtua ton era uoy';
+$lang['AppController.!error.invalid_csrf'] = 'Dilavni si nekot mrof eht';
+$lang['AppController.!success.license_updated'] = 'Ni gol esaelp .detadilaver yllufsseccus esnecil ruoy';
+$lang['AppController.!success.license_key_updated'] = 'Ni gol esaelp .detadpu yllufsseccus saw yek esnecil ruoy';
+$lang['AppController.select.please'] = '-- tceles esaelp --';
+$lang['AppController.modal.text_close'] = 'Esolc';
+$lang['AppController.modal.text_confirm'] = 'Mrifnoc esaelp';
+$lang['AppController.tooltip.text'] = '?';
+$lang['AppController.banners.client_manager'] = 'Ti eganam ot uoy detivni sah taht tnuocca na gniganam yltnerruc era uoy';
+$lang['AppController.banners.text_switch_back'] = 'Tnuocca ym ot kcab hctiws';
+$lang['AppController.message.close'] = '';
+$lang['AppController.structure.text_myinfo'] = 'Ofni ym';
+$lang['AppController.structure.text_settings'] = 'Sgnittes';
+$lang['AppController.structure.text_logout'] = 'Tuo gol';
+$lang['AppController.structure.text_maintenance'] = 'Egnahc ot ereh kcilc .nigol ton yam stneilc dna delbane yltnerruc si edom ecnanetniam';
+$lang['AppController.structure.text_search_placeholder'] = '%1$s';
+$lang['AppController.client_structure.staff_as_client_note'] = 'Latrop ffats ot nruter';
+$lang['AppController.client_structure.default_title'] = 'Tnuocca ym';
+$lang['AppController.client_structure.text_return_to_portal'] = 'Latrop ot nruter';
+$lang['AppController.client_structure.text_logout'] = 'Tuo gol';
+$lang['AppController.client_structure.text_login'] = 'Ni gol';
+$lang['AppController.client_structure.text_update_account'] = 'Tnuocca eganam';
+$lang['AppController.client_structure.text_emails'] = 'Yrotsih liame';
+$lang['AppController.client_structure.text_accounts'] = 'Stnuocca tnemyap';
+$lang['AppController.client_structure.text_contacts'] = 'Stcatnoc';
+$lang['AppController.client_structure.text_managers'] = 'Sreganam';
+$lang['AppController.dates.day_sun'] = 'Yadnus';
+$lang['AppController.dates.day_mon'] = 'Yadnom';
+$lang['AppController.dates.day_tue'] = 'Yadseut';
+$lang['AppController.dates.day_wed'] = 'Yadsendew';
+$lang['AppController.dates.day_thu'] = 'Yadsruht';
+$lang['AppController.dates.day_fri'] = 'Yadirf';
+$lang['AppController.dates.day_sat'] = 'Yadrutas';
+$lang['AppController.dates.dayabbr_sun'] = 'Nus';
+$lang['AppController.dates.dayabbr_mon'] = 'Nom';
+$lang['AppController.dates.dayabbr_tue'] = 'Eut';
+$lang['AppController.dates.dayabbr_wed'] = 'Dew';
+$lang['AppController.dates.dayabbr_thur'] = 'Uht';
+$lang['AppController.dates.dayabbr_fri'] = 'Irf';
+$lang['AppController.dates.dayabbr_sat'] = 'Tas';
+$lang['AppController.dates.month_jan'] = 'Yraunaj';
+$lang['AppController.dates.month_feb'] = 'Yraurbef';
+$lang['AppController.dates.month_mar'] = 'Hcram';
+$lang['AppController.dates.month_apr'] = 'Lirpa';
+$lang['AppController.dates.month_may'] = 'Yam';
+$lang['AppController.dates.month_jun'] = 'Enuj';
+$lang['AppController.dates.month_jul'] = 'Yluj';
+$lang['AppController.dates.month_aug'] = 'Tsugua';
+$lang['AppController.dates.month_sep'] = 'Rebmetpes';
+$lang['AppController.dates.month_oct'] = 'Rebotco';
+$lang['AppController.dates.month_nov'] = 'Rebmevon';
+$lang['AppController.dates.month_dec'] = 'Rebmeced';
+$lang['AppController.dates.monthabbr_jan'] = 'Naj';
+$lang['AppController.dates.monthabbr_feb'] = 'Bef';
+$lang['AppController.dates.monthabbr_mar'] = 'Ram';
+$lang['AppController.dates.monthabbr_apr'] = 'Rpa';
+$lang['AppController.dates.monthabbr_may'] = 'Yam';
+$lang['AppController.dates.monthabbr_jun'] = 'Nuj';
+$lang['AppController.dates.monthabbr_jul'] = 'Luj';
+$lang['AppController.dates.monthabbr_aug'] = 'Gua';
+$lang['AppController.dates.monthabbr_sep'] = 'Pes';
+$lang['AppController.dates.monthabbr_oct'] = 'Tco';
+$lang['AppController.dates.monthabbr_nov'] = 'Von';
+$lang['AppController.dates.monthabbr_dec'] = 'Ced';
+$lang['AppController.graphs.control_label.stacked'] = 'Dekcats';
+$lang['AppController.graphs.control_label.stream'] = 'Maerts';
+$lang['AppController.graphs.control_label.expanded'] = 'Dednapxe';
+$lang['AppController.graphs.control_label.grouped'] = 'Depuorg';
+$lang['AppController.graphs.label.total'] = 'Latot';
+$lang['AppController.sreader.dropdown'] = 'Nwodpord elggot';
+$lang['AppController.sreader.navigation'] = 'Noitagivan elggot';
+
