aeba94733cf0376520a7f82e68139eb45212c6fd
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:28 -0800

    Updated es_es language files

---
+++ language/es_es/client_verify.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Client Verify
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientVerify.!success.email_verified'] = 'Deifirev yllufsseccus neeb sah sserdda liame ruoy';
+$lang['ClientVerify.!success.email_sent'] = '%1$s ot tnes neeb sah liame noitacifirev a';
+$lang['ClientVerify.!error.invalid_token'] = 'Deripxe neeb sah ro dilav ton si nekot dedivorp eht';
+
