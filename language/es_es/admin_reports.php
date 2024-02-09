5363c7d2c570efca7a5b2dfb3b045339380ffb93
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:58 -0800

    Updated es_es language files

---
+++ language/es_es/admin_reports.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Admin Reports
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminReports.index.page_title'] = 'Stroper gnillib';
+$lang['AdminReports.index.box_title'] = 'Stroper';
+$lang['AdminReports.index.field_format'] = 'Tamrof';
+$lang['AdminReports.index.field_report_type'] = 'Epyt troper';
+$lang['AdminReports.index.field_submit'] = 'Tropxe';
+$lang['AdminReports.index.reports'] = 'Etareneg';
+$lang['AdminReports.index.customize'] = 'Ezimotsuc';
+
