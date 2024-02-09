c04400908a64ace2beb861238e7a5733383a9661
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:18 -0800

    Updated es_es language files

---
+++ plugins/auto_cancel/language/es_es/auto_cancel_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Auto Cancel Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AutoCancelPlugin.name'] = 'Lecnac otua';
+$lang['AutoCancelPlugin.description'] = 'Noitallecnac rof secivres seludehcs yllacitamotua';
+$lang['AutoCancelPlugin.getCronTasks.schedule_cancellation_name'] = 'Noitallecnac eludehcs otua';
+$lang['AutoCancelPlugin.getCronTasks.schedule_cancellation_desc'] = 'Noitallecnac rof secivres dednepsus seludehcs yllacitamotua';
+
