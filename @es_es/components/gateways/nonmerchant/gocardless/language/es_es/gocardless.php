95ab26d5142272ee77b331cdab0bdc5633e46c64
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:04:23 -0800

    Updated es_es language files

--- @es_es/components/gateways/nonmerchant/gocardless/language/es_es/gocardless.php
+++ @es_es/components/gateways/nonmerchant/gocardless/language/es_es/gocardless.php
@@ -9,22 +9,22 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Gocardless.name'] = 'GoCardless';
-$lang['Gocardless.description'] = 'Cobra pagos periódicos de más de 30 países y haz que se abonen en tu cuenta bancaria existente';
-$lang['Gocardless.access_token'] = 'Ficha de acceso';
-$lang['Gocardless.webhook_secret'] = 'Secreto de Webhook';
-$lang['Gocardless.pay_type'] = 'Formas de pago';
-$lang['Gocardless.pay_type_onetime'] = 'Pagos únicos';
-$lang['Gocardless.pay_type_subscribe'] = 'Sólo pagos de suscripción';
-$lang['Gocardless.pay_type_both'] = 'Pagos únicos y de suscripción cuando sea posible';
-$lang['Gocardless.dev_mode'] = 'Modo Desarrollador';
-$lang['Gocardless.dev_mode_note'] = 'Al activar esta opción, las transacciones se publicarán en el entorno GoCardless Sandbox. Habilite esta opción únicamente si está realizando pruebas con una cuenta de GoCardless Sandbox.';
-$lang['Gocardless.buildprocess.submit'] = 'Pague con GoCardless';
-$lang['Gocardless.buildprocess.subscription'] = 'Suscribirse con GoCardless';
-$lang['Gocardless.webhook'] = 'Webhook GoCardless';
-$lang['Gocardless.webhook_note'] = 'Antes de empezar a utilizar esta pasarela debe configurar la siguiente url como Webhook en su cuenta GoCardless. El Webhook Secret debe tener al menos 40 caracteres.';
-$lang['Gocardless.!error.access_token.valid'] = 'Debe introducir un código de acceso válido.';
-$lang['Gocardless.!error.webhook_secret.valid'] = 'Debe introducir un Webhook Secret válido de al menos 40 caracteres.';
-$lang['Gocardless.!error.dev_mode.valid'] = 'El modo desarrollador debe ser "true" si se indica.';
-$lang['Gocardless.!tooltip.webhook_secret'] = 'El Webhook Secret debe tener al menos 40 caracteres.';
+$lang['Gocardless.name'] = 'Sseldracog';
+$lang['Gocardless.description'] = 'Tnuocca knab gnitsixe ruoy otni tuo diap meht evah dna ,seirtnuoc 03 naht erom morf stnemyap gnirrucer tcelloc';
+$lang['Gocardless.access_token'] = 'Nekot ssecca';
+$lang['Gocardless.webhook_secret'] = 'Terces koohbew';
+$lang['Gocardless.pay_type'] = 'Snoitpo tnemyap';
+$lang['Gocardless.pay_type_onetime'] = 'Ylno stnemyap emit eno';
+$lang['Gocardless.pay_type_subscribe'] = 'Ylno stnemyap noitpircsbus';
+$lang['Gocardless.pay_type_both'] = 'Elbissop nehw stnemyap noitpircsbus dna emit eno';
+$lang['Gocardless.dev_mode'] = 'Edom repoleved';
+$lang['Gocardless.dev_mode_note'] = 'Tnuocca xobdnas sseldracog a htiw gnitset era uoy fi noitpo siht elbane ylno .tnemnorivne xobdnas sseldracog eht ot snoitcasnart tsop lliw noitpo siht gnilbane';
+$lang['Gocardless.buildprocess.submit'] = 'Sseldracog htiw yap';
+$lang['Gocardless.buildprocess.subscription'] = 'Sseldracog htiw ebircsbus';
+$lang['Gocardless.webhook'] = 'Koohbew sseldracog';
+$lang['Gocardless.webhook_note'] = 'Gnol sretcarahc 04 tsael ta eb tsum terces koohbew eht .tnuocca sseldracog ruoy ni koohbew a sa lru gniwollof eht erugifnoc tsum uoy yawetag siht gnisu trats uoy erofeb';
+$lang['Gocardless.!error.access_token.valid'] = 'Nekot ssecca dilav a retne tsum uoy';
+$lang['Gocardless.!error.webhook_secret.valid'] = 'Gnol sretcarahc 04 tsael ta terces koohbew dilav a retne tsum uoy';
+$lang['Gocardless.!error.dev_mode.valid'] = 'Nevig fi "eurt" ot tes eb tsum edom repoleved';
+$lang['Gocardless.!tooltip.webhook_secret'] = 'Gnol sretcarahc 04 tsael ta eb tsum terces koohbew eht';
 
