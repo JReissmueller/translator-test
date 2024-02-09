b20ebfca560967790736317e6ed3e31c4f635d16
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:45 -0800

    Updated es_es language files

---
+++ language/es_es/languages.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Languages
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Languages.!error.code.valid'] = 'Devomer eb ton nac \'su_ne\' egaugnal tluafed eht';
+$lang['Languages.!error.code.format'] = 'Dettamrof ylreporp ton si edoc egaugnal eht';
+$lang['Languages.!error.code.unique'] = 'Ynapmoc siht rof stsixe ydaerla edoc egaugnal taht';
+$lang['Languages.!error.company_id.format'] = 'Dilavni eb ot sraeppa di ynapmoc eht';
+$lang['Languages.!error.name.format'] = 'Ytpme eb ton yam eman egaugnal eht';
+
