fbeb5e9cd840a27ae4294375b348e70520283664
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:24:36 -0800

    Updated es_es language files

--- language/es_es/admin_company_automation.php
+++ language/es_es/admin_company_automation.php
@@ -9,24 +9,24 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['AdminCompanyAutomation.!notice.cron_task_interval'] = 'Slavretni retrohs eseht fo egatnavda ekat ot etunim yreve nur ot norc ruoy etadpu .etunim yreve gninnur eb ot raeppa ton seod norc ruoy tub ,etunim yreve nur ot tes sksat noitamotua era ereht';
-$lang['AdminCompanyAutomation.!success.automation_updated'] = '!detadpu yllufsseccus erew sgnittes noitamotua eht';
-$lang['AdminCompanyAutomation.!success.task_cleared'] = 'Deraelc neeb sah ksat norc eht';
-$lang['AdminCompanyAutomation.index.page_title'] = 'Noitamotua > ynapmoc > sgnittes';
-$lang['AdminCompanyAutomation.index.boxtitle_automation'] = 'Noitamotua';
-$lang['AdminCompanyAutomation.index.field_automationsubmit'] = 'Sgnittes etadpu';
-$lang['AdminCompanyAutomation.index.field_enabled'] = 'Delbane';
-$lang['AdminCompanyAutomation.index.text_interval'] = 'Lavretni';
-$lang['AdminCompanyAutomation.index.text_starttime'] = 'Emit trats';
-$lang['AdminCompanyAutomation.index.text_task_lastran'] = 'Nar tsal ksat';
-$lang['AdminCompanyAutomation.index.no_results'] = 'Sksat norc on era ereht';
-$lang['AdminCompanyAutomation.index.no_cron_lastran'] = 'Reven';
-$lang['AdminCompanyAutomation.index.text_cron_last_ran'] = '%1$s no nar tsal norc eht';
-$lang['AdminCompanyAutomation.index.text_cron_never_ran'] = 'Nur reven sah norc eht';
-$lang['AdminCompanyAutomation.index.option_clear_task'] = 'Kcol ksat raelc';
-$lang['AdminCompanyAutomation.index.confirm_clear_task'] = 'Esuac eht enimreted ot srorre rof gol norc eht gnikcehc ro ,yllaunam norc eht gninnur yrt .eussi suoires erom a eb yam ereht ,sllats ylsuounitnoc ksat siht fi ?kcol ksat norc siht raelc ot tnaw uoy erus uoy era';
-$lang['AdminCompanyAutomation.getintervals.text_minute'] = 'Etunim';
-$lang['AdminCompanyAutomation.getintervals.text_minutes'] = 'Setunim';
-$lang['AdminCompanyAutomation.getintervals.text_hour'] = 'Ruoh';
-$lang['AdminCompanyAutomation.getintervals.text_hours'] = 'Sruoh';
+$lang['AdminCompanyAutomation.!notice.cron_task_interval'] = 'Hay tareas de automatización configuradas para ejecutarse cada minuto, pero su cron no parece estar ejecutándose cada minuto. Actualiza tu cron para que se ejecute cada minuto y aprovechar estos intervalos más cortos.';
+$lang['AdminCompanyAutomation.!success.automation_updated'] = 'Los ajustes de automatización se han actualizado correctamente.';
+$lang['AdminCompanyAutomation.!success.task_cleared'] = 'La tarea cron ha sido borrada.';
+$lang['AdminCompanyAutomation.index.page_title'] = 'Configuración > Empresa > Automatización';
+$lang['AdminCompanyAutomation.index.boxtitle_automation'] = 'Automatización';
+$lang['AdminCompanyAutomation.index.field_automationsubmit'] = 'Actualizar ajustes';
+$lang['AdminCompanyAutomation.index.field_enabled'] = 'Activado';
+$lang['AdminCompanyAutomation.index.text_interval'] = 'Intervalo';
+$lang['AdminCompanyAutomation.index.text_starttime'] = 'Hora de inicio';
+$lang['AdminCompanyAutomation.index.text_task_lastran'] = 'Última tarea ejecutada:';
+$lang['AdminCompanyAutomation.index.no_results'] = 'No hay tareas cron.';
+$lang['AdminCompanyAutomation.index.no_cron_lastran'] = 'Nunca';
+$lang['AdminCompanyAutomation.index.text_cron_last_ran'] = 'El cron se ejecutó por última vez el %1$s.';
+$lang['AdminCompanyAutomation.index.text_cron_never_ran'] = 'El cron nunca se ha ejecutado.';
+$lang['AdminCompanyAutomation.index.option_clear_task'] = 'Borrar bloqueo de tareas';
+$lang['AdminCompanyAutomation.index.confirm_clear_task'] = '¿Está seguro de que desea borrar el bloqueo de esta tarea cron? Si esta tarea se bloquea continuamente, puede haber un problema más grave. Intente ejecutar el cron manualmente o compruebe si hay errores en el registro del cron para determinar la causa.';
+$lang['AdminCompanyAutomation.getintervals.text_minute'] = 'minuto';
+$lang['AdminCompanyAutomation.getintervals.text_minutes'] = 'minutos';
+$lang['AdminCompanyAutomation.getintervals.text_hour'] = 'hora';
+$lang['AdminCompanyAutomation.getintervals.text_hours'] = 'horas';
 
