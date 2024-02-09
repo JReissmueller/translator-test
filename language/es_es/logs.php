f422da43eb68034f506828570bdb1fcdedb2333b
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:46 -0800

    Updated es_es language files

---
+++ language/es_es/logs.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Logs
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Logs.!error.deletelog_datetime'] = 'Tamrof etad dilavni';
+$lang['Logs.!error.company_id.exists'] = 'Di ynapmoc dilavni';
+$lang['Logs.!error.to_address.empty'] = 'Sserdda ot a gnissim';
+$lang['Logs.!error.from_address.empty'] = 'Sserdda morf a gnissim';
+$lang['Logs.!error.from_name.empty'] = 'Eman morf a gnissim';
+$lang['Logs.!error.subject.empty'] = 'Tcejbus a gnissim';
+$lang['Logs.!error.sent.format'] = 'Rebmun a eb tsum tnes';
+$lang['Logs.!error.sent.length'] = 'Retcarahc 1 deecxe ton yam htgnel tnes';
+$lang['Logs.!error.staff_id.exists'] = 'Di ffats dilavni';
+$lang['Logs.!error.module_id.exists'] = 'Di eludom dilavni';
+$lang['Logs.!error.gateway_id.exists'] = 'Di yawetag dilavni';
+$lang['Logs.!error.direction.format'] = 'Epyt noitcerid dilavni';
+$lang['Logs.!error.url.empty'] = 'Lru gnissim';
+$lang['Logs.!error.status.format'] = 'Epyt sutats dilavni';
+$lang['Logs.!error.group.empty'] = 'Puorg gnissim';
+$lang['Logs.!error.group.maxlength'] = 'Sretcarahc 8 deecxe ton yam htgnel puorg';
+$lang['Logs.!error.user_id.empty'] = 'Di resu gnissim';
+$lang['Logs.!error.ip_address.empty'] = 'Sserdda pi gnissim';
+$lang['Logs.!error.ip_address.length'] = 'Htgnel ni sretcarahc 93 deecxe ton yam sserdda pi eht';
+$lang['Logs.!error.user_log_exists.empty'] = 'Tsixe ton seod sserdda pi nevig eht ta resu eht';
+$lang['Logs.!error.result.format'] = '\'eruliaf\' ro \'sseccus\' rehtie eb tsum tluser nigol';
+$lang['Logs.!error.to_user_id.exists'] = 'Di resu dilavni';
+$lang['Logs.!error.messenger_id.exists'] = 'Di regnessem dilavni';
+$lang['Logs.!error.success.format'] = 'Eulav sseccus dilavni';
+$lang['Logs.!error.contact_id.empty'] = 'Di tcatnoc gnissim';
+$lang['Logs.!error.client_id.exists'] = 'Di tneilc dilavni';
+$lang['Logs.!error.fields.empty'] = 'Sdleif elbaggol gnissim';
+$lang['Logs.!error.by_user_id.exists'] = 'Di resu yb dilavni';
+$lang['Logs.!error.date_changed.valid'] = 'Degnahc etad dilavni';
+$lang['Logs.!error.staff_id.empty'] = 'Di ffats gnissim';
+$lang['Logs.!error.type.format'] = 'Epyt dilavni';
+$lang['Logs.!error.account_type.format'] = 'Epyt tnuocca dilavni';
+$lang['Logs.!error.account_id.empty'] = 'Di tnuocca gnissim';
+$lang['Logs.!error.first_name.empty'] = 'Ytpme eb ton tsum eman tsrif';
+$lang['Logs.!error.last_name.empty'] = 'Ytpme eb ton tsum eman tsal';
+$lang['Logs.!error.run_id.exists'] = 'Di ksat norc dilavni';
+$lang['Logs.!error.event.maxlength'] = 'Sretcarahc 23 deecxe ton yam htgnel tneve';
+$lang['Logs.!error.group.unique'] = 'Nekat neeb ydaerla evah puorg dna di ksat norc eht';
+$lang['Logs.!error.group.betweenlength'] = 'Htgnel ni sretcarahc 23 dna 1 neewteb eb tsum puorg';
+$lang['Logs.!error.start_date.format'] = 'Tamrof etad trats dilavni';
+$lang['Logs.!error.end_date.format'] = 'Tamrof etad dne dilavni';
+
