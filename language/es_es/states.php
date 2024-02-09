2f84d3a38c0edc3e06c6008916cd5697249ff411
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:09 -0800

    Updated es_es language files

---
+++ language/es_es/states.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * States
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['States.!error.code.format'] = '2-6613 osi ni denifed sa ciremun-ahpla eb tsum edoc etats eht';
+$lang['States.!error.country_alpha2.format'] = 'Sretcarahc ahpla 2 eb tsum 2ahpla yrtnuoc';
+$lang['States.!error.country_alpha2.in_use'] = 'Nekat ydaerla era %2$s edoc noisividbus dna %1$s edoc yrtnuoc 2ahpla eht';
+$lang['States.!error.name.format'] = 'Eman s\'etats eht retne esaelp';
+
