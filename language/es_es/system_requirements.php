075b06a2503c9e2bfadcb35a9b60c222f1777210
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:26:40 -0800

    Updated es_es language files

--- language/es_es/system_requirements.php
+++ language/es_es/system_requirements.php
@@ -9,23 +9,23 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['SystemRequirements.!error.php.minimum'] = '%2$s :noisrev ruoy .deriuqer si retaerg ro %1$s noisrev php';
-$lang['SystemRequirements.!error.extension.minimum'] = 'Deriuqer si \'%1$s\' noisnetxe eht';
-$lang['SystemRequirements.!error.extension_version.minimum'] = '%2$s :noisrev ruoy .deriuqer si \'%1$s\' noisnetxe eht';
-$lang['SystemRequirements.!error.config_writable.minimum'] = 'Revresbew eht yb elbatirw eb tsum )%2$s( yrotcerid dna )%1$s( elif gifnoc eht';
-$lang['SystemRequirements.!info.ext-ioncube.minimum'] = 'Esiwrehto noitallatsni retfa elbissecca eb ton lliw atselb .yrotcerid %1$s eht morf esoht htiw selif atselb ruoy gnitirwrevo yb selif xiftoh yrassecen eht ylppa ot erus eb';
-$lang['SystemRequirements.!error.ext-ioncube-sourceguardian.minimum'] = 'Deriuqer si noisnetxe \'redaol ebucnoi\' ro \'naidraugecruos\' eht';
-$lang['SystemRequirements.!info.ext-ioncube-sourceguardian.minimum'] = 'Esiwrehto noitallatsni retfa elbissecca eb ton lliw atselb .yrotcerid 8php-xiftoh eht morf esoht htiw selif atselb ruoy gnitirwrevo yb selif xiftoh yrassecen eht ylppa ot erus eb naidraugecruos gnisu fi .deriuqer si noisnetxe \'redaol ebucnoi\' ro \'naidraugecruos\' eht';
-$lang['SystemRequirements.!warning.gd.recommended'] = 'Noitareneg ahctpac lanretni dna noitareneg fdp gnirud troppus egami retteb rof dednemmocer si noisnetxe dg eht';
-$lang['SystemRequirements.!warning.gmp.recommended'] = 'Ecnamrofrep retteb rof dednemmocer ylhgih si noisnetxe pmg eht';
-$lang['SystemRequirements.!warning.imap.recommended'] = 'Pami dna ptms aiv liam eviecer dna dnes ot deriuqer si noisnetxe pami eht';
-$lang['SystemRequirements.!warning.libxml.recommended'] = 'Smetsys emos htiw ecafretni ot deriuqer eb yam ti sa dednemmocer ylhgih si noisnetxe lmxbil eht';
-$lang['SystemRequirements.!warning.mailparse.recommended'] = 'Sliame gnimocni gnisrap rof deriuqer si noisnetxe esrapliam eht';
-$lang['SystemRequirements.!warning.iconv.recommended'] = 'Gnidoced dna gnidocne retcarahc laiceps rof deriuqer si noisnetxe vnoci eht';
-$lang['SystemRequirements.!warning.mbstring.recommended'] = 'Serutaef lanoitpo emos yb deriuqer si noisnetxe gnirtsbm eht';
-$lang['SystemRequirements.!warning.mcrypt.recommended'] = 'Ecnamrofrep retteb rof dednemmocer si noisnetxe tpyrcm eht';
-$lang['SystemRequirements.!warning.simplexml.recommended'] = 'Smetsys emos htiw ecafretni ot deriuqer eb yam ti sa dednemmocer ylhgih si noisnetxe lmxelpmis eht';
-$lang['SystemRequirements.!warning.zlib.recommended'] = 'Ecnamrofrep retteb rof dnemmocer ylhgih si noisnetxe bilz eht';
-$lang['SystemRequirements.!warning.soap.recommended'] = 'Gnildnah xat tav citamotua rof deriuqer si noisnetxe paos eht';
-$lang['SystemRequirements.!warning.cache_writable.recommended'] = 'Revresbew eht yb elbatirw si %1$s taht erusne ecnamrofrep retteb rof';
+$lang['SystemRequirements.!error.php.minimum'] = 'Se requiere la versión de PHP %1$s o superior. Su versión: %2$s.';
+$lang['SystemRequirements.!error.extension.minimum'] = 'Se requiere la extensión \'%1$s\'.';
+$lang['SystemRequirements.!error.extension_version.minimum'] = 'La extensión \'%1$s\' es necesaria. Su versión: %2$s.';
+$lang['SystemRequirements.!error.config_writable.minimum'] = 'El archivo de configuración (%1$s) y el directorio (%2$s) deben ser escribibles por el servidor web.';
+$lang['SystemRequirements.!info.ext-ioncube.minimum'] = 'Asegúrese de aplicar los ficheros hotfix necesarios sobrescribiendo sus ficheros Blesta con los del directorio %1$s. De lo contrario, Blesta no será accesible tras la instalación.';
+$lang['SystemRequirements.!error.ext-ioncube-sourceguardian.minimum'] = 'Se requiere la extensión \'SourceGuardian\' o \'ioncube loader\'.';
+$lang['SystemRequirements.!info.ext-ioncube-sourceguardian.minimum'] = 'Se requiere la extensión \'SourceGuardian\' o \'ioncube loader\'. Si utiliza SourceGuardian asegúrese de aplicar los archivos hotfix necesarios sobrescribiendo sus archivos Blesta con los del directorio hotfix-php8. De lo contrario, Blesta no será accesible tras la instalación.';
+$lang['SystemRequirements.!warning.gd.recommended'] = 'Se recomienda la extensión gd para mejorar el soporte de imágenes durante la generación de PDF y la generación interna de captchas.';
+$lang['SystemRequirements.!warning.gmp.recommended'] = 'La extensión gmp es muy recomendable para un mejor rendimiento.';
+$lang['SystemRequirements.!warning.imap.recommended'] = 'La extensión imap es necesaria para enviar y recibir correo a través de SMTP e IMAP.';
+$lang['SystemRequirements.!warning.libxml.recommended'] = 'La extensión libxml es muy recomendable, ya que puede ser necesaria para interactuar con algunos sistemas.';
+$lang['SystemRequirements.!warning.mailparse.recommended'] = 'La extensión mailparse es necesaria para analizar los correos electrónicos entrantes.';
+$lang['SystemRequirements.!warning.iconv.recommended'] = 'La extensión iconv es necesaria para la codificación y descodificación de caracteres especiales.';
+$lang['SystemRequirements.!warning.mbstring.recommended'] = 'La extensión mbstring es necesaria para algunas funciones opcionales.';
+$lang['SystemRequirements.!warning.mcrypt.recommended'] = 'Se recomienda la extensión mcrypt para un mejor rendimiento.';
+$lang['SystemRequirements.!warning.simplexml.recommended'] = 'La extensión simplexml es muy recomendable, ya que puede ser necesaria para interactuar con algunos sistemas.';
+$lang['SystemRequirements.!warning.zlib.recommended'] = 'La extensión zlib es muy recomendable para un mejor rendimiento.';
+$lang['SystemRequirements.!warning.soap.recommended'] = 'La ampliación del jabón es necesaria para la gestión automática del IVA.';
+$lang['SystemRequirements.!warning.cache_writable.recommended'] = 'Para un mejor rendimiento asegúrese de que %1$s es escribible por el servidor web.';
 
