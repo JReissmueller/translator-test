c2627dff4f6d42302a800420f9cb4023d7961350
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:15:33 -0800

    Updated es_es language files

--- components/gateways/nonmerchant/widepay/language/es_es/widepay.php
+++ components/gateways/nonmerchant/widepay/language/es_es/widepay.php
@@ -9,14 +9,14 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Widepay.name'] = 'Yap ediw';
-$lang['Widepay.wallet_id'] = 'Di tellaw';
-$lang['Widepay.wallet_token'] = 'Nekot tellaw';
-$lang['Widepay.allow_card_payment'] = 'Stnemyap drac wolla';
-$lang['Widepay.!error.wallet_id.format'] = 'Di tellaw a retne esaelp';
-$lang['Widepay.!error.wallet_token.format'] = 'Nekot ipa tellaw a retne esaelp';
-$lang['Widepay.buildprocess.submit_card'] = 'Drac tiderc yap ediw htiw yap';
-$lang['Widepay.buildprocess.submit_ticket'] = 'Tekcit yap ediw htiw yap';
-$lang['Widepay.charge_types.ticket'] = 'Tekcit';
-$lang['Widepay.charge_types.card'] = 'Drac tiderc';
+$lang['Widepay.name'] = 'Pago amplio';
+$lang['Widepay.wallet_id'] = 'Cartera ID';
+$lang['Widepay.wallet_token'] = 'Ficha de cartera';
+$lang['Widepay.allow_card_payment'] = 'Permitir pagos con tarjeta';
+$lang['Widepay.!error.wallet_id.format'] = 'Por favor, introduzca un ID de cartera';
+$lang['Widepay.!error.wallet_token.format'] = 'Introduzca un token de API de monedero';
+$lang['Widepay.buildprocess.submit_card'] = 'Pagar con tarjeta de crédito Wide Pay';
+$lang['Widepay.buildprocess.submit_ticket'] = 'Pagar con Wide Pay Ticket';
+$lang['Widepay.charge_types.ticket'] = 'Billete';
+$lang['Widepay.charge_types.card'] = 'Tarjeta de crédito';
 
