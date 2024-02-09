f8f3d38abe49bb554b162eb4951809be998e5368
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:32 -0800

    Updated es_es language files

---
+++ components/upload/language/es_es/upload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Upload
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Upload.!error.max_file_size'] = 'Ezis elif dewolla mumixam eht sdeecxe elif eht';
+$lang['Upload.!error.mime_type'] = 'Dewolla ton si %1$s ,epyt elif eht';
+$lang['Upload.!error.file_exists'] = 'Stsixe ydaerla elif eht';
+$lang['Upload.!error.write_failed'] = 'Nettirw eb ton dluoc elif eht';
+$lang['Upload.!error.path_created'] = 'Detaerc eb ton dluoc htap daolpu eht';
+
