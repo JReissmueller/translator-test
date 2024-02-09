1d4409a78f05bc0f05fca28df8dbbbc3f025b0eb
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:41 -0800

    Updated es_es language files

---
+++ plugins/download_manager/language/es_es/download_manager_files.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Download Manager Files
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerFiles.!error.category_id.exists'] = 'Yrogetac dilav a tceles esaelp';
+$lang['DownloadManagerFiles.!error.company_id.exists'] = 'Di ynapmoc dilavni';
+$lang['DownloadManagerFiles.!error.name.empty'] = 'Elif siht rof eman a retne esaelp';
+$lang['DownloadManagerFiles.!error.file_name.format'] = 'Elif eht ot htap dilav a retne ro elif a daolpu esaelp';
+$lang['DownloadManagerFiles.!error.public.format'] = 'Tamrof dilavni na ni si noitpo ytilibaliava cilbup eht';
+$lang['DownloadManagerFiles.!error.public.assignment'] = 'Segakcap ro ,spuorg tneilc ,cilbup eht rehtie ot elbaliava eb tsum elif eht';
+$lang['DownloadManagerFiles.!error.permit_client_groups.format'] = 'Tamrof dilavni na ni si noitpo ytilibaliava puorg tneilc eht';
+$lang['DownloadManagerFiles.!error.file_groups.format'] = 'Tsixe ton seod ti esuaceb dedda eb tonnac detceles puorg tneilc eno tsael ta';
+$lang['DownloadManagerFiles.!error.permit_packages.format'] = 'Tamrof dilavni na ni si noitpo ytilibaliava egakcap eht';
+$lang['DownloadManagerFiles.!error.file_packages.format'] = 'Tsixe ton seod ti esuaceb dedda eb tonnac detceles egakcap eno tsael ta';
+$lang['DownloadManagerFiles.!error.file_id.exists'] = 'Tsixe ton seod detadpu gnieb elif eht';
+
