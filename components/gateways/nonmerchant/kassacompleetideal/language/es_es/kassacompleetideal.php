39fbfc75f6311ab47e44eb85c632585942200afc
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:29 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/kassacompleetideal/language/es_es/kassacompleetideal.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Kassacompleetideal
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Kassacompleetideal.!error.api_key.empty'] = 'Yek ipa na retne esaelp';
+$lang['Kassacompleetideal.!error.api_key.valid'] = 'Dilavni si yek ipa';
+$lang['Kassacompleetideal.name'] = ')teelpmoc assak( laedi';
+$lang['Kassacompleetideal.description'] = 'Epyt tnemyap laedi eht gnisu noitargetni teelpmoc assak';
+$lang['Kassacompleetideal.api_key'] = 'Yek ipa';
+$lang['Kassacompleetideal.tooltip.api_key'] = 'Tnuocca tset a morf yek ipa na retne ylpmis ,edom tset esu ot redro ni';
+$lang['Kassacompleetideal.buildprocess.issuer_id'] = 'Reussi laedi';
+$lang['Kassacompleetideal.buildprocess.submit'] = 'Teelpmoc assak aiv laedi htiw yap';
+
