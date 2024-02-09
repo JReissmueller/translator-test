3f9b3cbbcb984c3da74a8593a14d1153c7a33aa9
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:14 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/payflow/language/es_es/payflow.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Payflow
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payflow.!error.user.empty'] = 'Snoitcasnart ssecorp ot dezirohtua resu a retne esaelp';
+$lang['Payflow.!error.vendor.empty'] = 'Nigol tnuocca tnahcrem ruoy retne esaelp';
+$lang['Payflow.!error.partner.empty'] = 'Rentrap tnahcrem ruoy retne esaelp';
+$lang['Payflow.!error.password.format'] = 'Htgnel ni sretcarahc 23 dna 6 neewteb drowssap a retne esaelp';
+$lang['Payflow.!error.test_mode.valid'] = 'Nevig fi "eurt" ot tes eb tsum edom tset';
+$lang['Payflow.name'] = 'Orp wolfyap lapyap';
+$lang['Payflow.description'] = 'Sdrac tibed dna tiderc rojam lla seldnah orp wolfyap';
+$lang['Payflow.vendor'] = 'Nigol tnahcrem/rodnev';
+$lang['Payflow.user'] = 'Resu';
+$lang['Payflow.password'] = 'Drowssap';
+$lang['Payflow.partner'] = 'Rentrap';
+$lang['Payflow.test_mode'] = 'Edom tset';
+$lang['Payflow.tooltip_user'] = 'Nigol tnahcrem/rodnev eht sa eulav emas eht retne ,esiwrehto .snoitcasnart ssecorp ot dezirohtua resu eht retne ,tnuocca ruoy rof putes resu eno naht erom evah uoy fi';
+$lang['Payflow.tooltip_partner'] = 'Daetsni "lapyap" esu ,yltcerid lapyap morf desahcrup saw tnuocca ruoy fi .kds wolfyap eht rof uoy deretsiger mohw relleser lapyap dezirohtua na yb uoy ot nevig rentrap tnahcrem eht si siht';
+
