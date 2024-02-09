c3096c609eb47e1ac1022570011a4e11d32a5f52
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:01 -0800

    Updated es_es language files

---
+++ language/es_es/permissions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Permissions
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Permissions.!error.group_id.exists'] = 'Tsixe ton seod nevig puorg noissimrep eht';
+$lang['Permissions.!error.name.empty'] = 'Eman a evah tsum noissimrep eht';
+$lang['Permissions.!error.alias.empty'] = 'Saila na evah tsum noissimrep eht';
+$lang['Permissions.!error.action.empty'] = 'Noitca na evah tsum noissimrep eht';
+$lang['Permissions.!error.plugin_id.exists'] = 'Tsixe ton seod noissimrep siht rof deificeps nigulp eht';
+$lang['Permissions.!error.name.group_empty'] = 'Eman a evah tsum puorg noissimrep eht';
+$lang['Permissions.!error.level.group_empty'] = '\'tneilc\' ro \'ffats\' rehtie eb tsum level eht';
+$lang['Permissions.!error.alias.group_empty'] = 'Saila na evah tsum puorg noissimrep eht';
+$lang['Permissions.!error.plugin_id.group_exists'] = 'Tsixe ton seod puorg noissimrep siht rof deificeps nigulp eht';
+
