342f295cc6ff276322a6b2fb5554ddd1f5506466
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:20:42 -0800

    Updated es_es language files

---
+++ language/es_es/admin_company_currencies.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Admin Company Currencies
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyCurrencies.!success.setup_updated'] = '!detadpu yllufsseccus erew sgnittes putes ycnerruc eht';
+$lang['AdminCompanyCurrencies.!success.add_created'] = '!detaerc yllufsseccus saw ycnerruc %1$s eht';
+$lang['AdminCompanyCurrencies.!success.edit_updated'] = '!detadpu yllufsseccus saw ycnerruc %1$s eht';
+$lang['AdminCompanyCurrencies.!success.delete_deleted'] = '!deteled yllufsseccus saw ycnerruc %1$s eht';
+$lang['AdminCompanyCurrencies.!success.rates_updated'] = 'Detadpu yllufsseccus erew setar egnahcxe eht';
+$lang['AdminCompanyCurrencies.!tooltip.precision'] = 'Stroppus ycnerruc eht taht ecalp lamiced eht retfa stigid tnacifingis fo rebmun eht si noisicerp eht';
+$lang['AdminCompanyCurrencies.!tooltip.default_currency'] = 'Ycnerruc derreferp tsom ruoy ot siht tes .metsys eht ni desu ycnerruc tluafed eht';
+$lang['AdminCompanyCurrencies.!tooltip.show_currency_code'] = 'Secirp deyalpsid ot txen edoc ycnerruc eht gniyalpsid elbane ot kcehc';
+$lang['AdminCompanyCurrencies.!tooltip.client_set_currency'] = 'Metsys eht ni elbaliava esoht morf ycnerruc tluafed rieht esoohc ot stneilc wolla ot kcehc';
+$lang['AdminCompanyCurrencies.!tooltip.multi_currency_pricing'] = 'Ecalp gnikat morf noisrevnoc ycnerruc stneverp siht ,sdrow rehto ni .egakcap taht rof tpecca ot hsiw uoy ycnerruc hcae rof denifed eb gnicirp taht seriuqer siht gnikcehc .egakcap taht rof denifed gnicirp eht gnisu ylno deredro eb ot segakcap wolla ot kcehc';
+$lang['AdminCompanyCurrencies.!tooltip.exchange_rates_auto_update'] = 'Srossecorp etar egnahcxe elbaliava eht fo eno gnisu detadpu yllacitamotua eb ot setar egnahcxe wolla ot kcehc';
+$lang['AdminCompanyCurrencies.!tooltip.exchange_rates_processor'] = 'Rossecorp yb yrav elbaliava seicnerruc .setar egnahcxe ycnerruc etadpu ot esu ot rossecorp eht stes';
+$lang['AdminCompanyCurrencies.!tooltip.exchange_rates_processor_key'] = 'Rossecorp detceles eht morf setar egnahcxe ycnerruc eveirter ot deriuqer si yek ipa na';
+$lang['AdminCompanyCurrencies.!tooltip.exchange_rates_padding'] = 'Egatnecrep nevig eht yb desaercni eb lliw rossecorp a yb denimreted seulav etar egnahcxe';
+$lang['AdminCompanyCurrencies.!cancel.field_cancel'] = 'Lecnac';
+$lang['AdminCompanyCurrencies.setup.page_title'] = 'Putes ycnerruc > seicnerruc > ynapmoc > sgnittes';
+$lang['AdminCompanyCurrencies.setup.tooltip_currency_pricing'] = 'Egakcap eht rof deificeps ton fi ycnerruc tluafed eht morf etar egnahcxe eht esu lliw tub ,ycnerruc nesohc eht ni gnicirp egakcap referp syawla secivres gniwener
+
+.ycnerruc tluafed eht morf etar egnahcxe eht no desab detaluclac eb lliw ecirp eht ,ycnerruc eht rof tsixe ton seod gnicirp egakcap dna dekcehcnu si ti fi .secivres wen gnitaerc rof elbaliava eb ot ycnerruc deificeps eht ni tes eb ot gnicirp egakcap seriuqer noitpo siht';
+$lang['AdminCompanyCurrencies.setup.boxtitle_setup'] = 'Putes ycnerruc';
+$lang['AdminCompanyCurrencies.setup.heading_general'] = 'Lareneg';
+$lang['AdminCompanyCurrencies.setup.field.default_currency'] = 'Ycnerruc tluafed';
+$lang['AdminCompanyCurrencies.setup.field.show_currency_code'] = 'Edoc ycnerruc wohs';
+$lang['AdminCompanyCurrencies.setup.field.client_set_currency'] = 'Ycnerruc tes ot tneilc wolla';
+$lang['AdminCompanyCurrencies.setup.heading_multicurrency'] = 'Ycnerruc-itlum';
+$lang['AdminCompanyCurrencies.setup.field.multi_currency_pricing'] = 'Ylno secivres wen rof gnicirp egakcap esu';
+$lang['AdminCompanyCurrencies.setup.field.exchange_rates_auto_update'] = 'Setar egnahcxe etadpu yllacitamotua';
+$lang['AdminCompanyCurrencies.setup.field.last_updated'] = 'Detadpu tsal setar';
+$lang['AdminCompanyCurrencies.setup.field.exchange_rates_processor'] = 'Rossecorp setar egnahcxe';
+$lang['AdminCompanyCurrencies.setup.field.exchange_rates_processor_key'] = 'Yek ipa rossecorp setar egnahcxe';
+$lang['AdminCompanyCurrencies.setup.field.exchange_rates_padding'] = 'Setar egnahcxe dap';
+$lang['AdminCompanyCurrencies.setup.field.setupsubmit'] = 'Sgnittes etadpu';
+$lang['AdminCompanyCurrencies.setup.no_exchange_updated'] = 'Reven';
+$lang['AdminCompanyCurrencies.setup.open_parenthesis'] = '(';
+$lang['AdminCompanyCurrencies.setup.closed_parenthesis'] = ')';
+$lang['AdminCompanyCurrencies.active.page_title'] = 'Seicnerruc evitca > seicnerruc > ynapmoc > sgnittes';
+$lang['AdminCompanyCurrencies.active.boxtitle_active'] = 'Seicnerruc evitca';
+$lang['AdminCompanyCurrencies.active.categorylink_addcurrency'] = 'Ycnerruc dda';
+$lang['AdminCompanyCurrencies.active.no_results'] = 'Seicnerruc evitca on era ereht';
+$lang['AdminCompanyCurrencies.active.text_currency_code'] = ')7124 osi( edoc ycnerruc';
+$lang['AdminCompanyCurrencies.active.text_format'] = 'Tamrof';
+$lang['AdminCompanyCurrencies.active.text_precision'] = 'Noisicerp';
+$lang['AdminCompanyCurrencies.active.text_exchange_rate'] = 'Etar egnahcxe';
+$lang['AdminCompanyCurrencies.active.text_exchange_updated'] = 'Detadpu tsal';
+$lang['AdminCompanyCurrencies.active.text_options'] = 'Snoitpo';
+$lang['AdminCompanyCurrencies.active.option_edit'] = 'Tide';
+$lang['AdminCompanyCurrencies.active.option_delete'] = 'Eteled';
+$lang['AdminCompanyCurrencies.active.confirm_delete'] = '?ycnerruc siht eteled ot tnaw uoy erus uoy era';
+$lang['AdminCompanyCurrencies.active.no_exchange_updated'] = 'Reven';
+$lang['AdminCompanyCurrencies.add.page_title'] = 'Ycnerruc dda > seicnerruc > ynapmoc > sgnittes';
+$lang['AdminCompanyCurrencies.add.boxtitle_add'] = 'Ycnerruc dda';
+$lang['AdminCompanyCurrencies.add.field_code'] = ')7124 osi( edoc ycnerruc';
+$lang['AdminCompanyCurrencies.add.field_format'] = 'Tamrof';
+$lang['AdminCompanyCurrencies.add.field_precision'] = 'Noisicerp';
+$lang['AdminCompanyCurrencies.add.field_prefix'] = 'Lobmys xiferp';
+$lang['AdminCompanyCurrencies.add.field_suffix'] = 'Lobmys xiffus';
+$lang['AdminCompanyCurrencies.add.field_exchange_rate'] = 'Etar egnahcxe';
+$lang['AdminCompanyCurrencies.add.field_addsubmit'] = 'Ycnerruc etaerc';
+$lang['AdminCompanyCurrencies.add.confirm_add'] = ']putes ycnerruc[ > ]seicnerruc[ > ]ynapmoc[ > ]sgnittes[ rednu setadpu citamotua elbasid yam uoy .metsys eht ni detadpu yllacitamotua era setar egnahcxe nehw nettirwrevo eb lliw etar egnahcxe s\'ycnerruc siht ?ycnerruc siht etaerc ot tnaw uoy erus uoy era';
+$lang['AdminCompanyCurrencies.edit.page_title'] = 'Ycnerruc tide > seicnerruc > ynapmoc > sgnittes';
+$lang['AdminCompanyCurrencies.edit.boxtitle_edit'] = 'Ycnerruc tide';
+$lang['AdminCompanyCurrencies.edit.field_code'] = ')7124 osi( edoc ycnerruc';
+$lang['AdminCompanyCurrencies.edit.field_format'] = 'Tamrof';
+$lang['AdminCompanyCurrencies.edit.field_precision'] = 'Noisicerp';
+$lang['AdminCompanyCurrencies.edit.field_prefix'] = 'Lobmys xiferp';
+$lang['AdminCompanyCurrencies.edit.field_suffix'] = 'Lobmys xiffus';
+$lang['AdminCompanyCurrencies.edit.field_exchange_rate'] = 'Etar egnahcxe';
+$lang['AdminCompanyCurrencies.edit.field_editsubmit'] = 'Ycnerruc tide';
+$lang['AdminCompanyCurrencies.edit.confirm_edit'] = ']putes ycnerruc[ > ]seicnerruc[ > ]ynapmoc[ > ]sgnittes[ rednu setadpu citamotua elbasid yam uoy .metsys eht ni detadpu yllacitamotua era setar egnahcxe nehw nettirwrevo eb lliw etar egnahcxe s\'ycnerruc siht ?ycnerruc siht etadpu ot tnaw uoy erus uoy era';
+
