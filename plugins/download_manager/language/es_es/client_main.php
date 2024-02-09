aad667b983275c3a311a3f9f3ad6d33c82f76d31
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:39 -0800

    Updated es_es language files

---
+++ plugins/download_manager/language/es_es/client_main.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.!text.root_directory'] = 'Emoh';
+$lang['ClientMain.index.page_title'] = 'Sdaolnwod';
+$lang['ClientMain.index.boxtitle_downloads'] = 'Sdaolnwod';
+$lang['ClientMain.index.login'] = 'Noitces siht ni sdaolnwod weiv ot ni gol tsum uoy';
+$lang['ClientMain.index.no_downloads'] = 'Noitces siht ni elbaliava sdaolnwod on era ereht';
+
