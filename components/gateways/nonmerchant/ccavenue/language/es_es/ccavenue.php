25ef2612e83b5116ed0928e701aaa2de7a365dd0
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:21 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/ccavenue/language/es_es/ccavenue.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Ccavenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Ccavenue.name'] = 'Tnemyap eunevacc';
+$lang['Ccavenue.description'] = 'Senigne tibed tcerid tsegral s\'aidni fo eno si eunevacc ,sisab emit-laer a no sknab naidni 55 naht erom gnitcennoc snoitpo tnemyap 042 revo htiw';
+$lang['Ccavenue.merchant_id'] = 'Di tnahcrem';
+$lang['Ccavenue.merchant_id_note'] = 'Syek ipa rednu tnuocca eunevacc ruoy ni elbaliava si di tnahcrem ruoy';
+$lang['Ccavenue.encryption_key'] = 'Yek noitpyrcne';
+$lang['Ccavenue.encryption_key_note'] = 'Syek ipa rednu tnuocca eunevacc ruoy ni detareneg si siht';
+$lang['Ccavenue.access_code'] = 'Edoc ssecca';
+$lang['Ccavenue.access_code_note'] = 'Syek ipa rednu tnuocca eunevacc ruoy ni dnuof eb nac siht';
+$lang['Ccavenue.test_mode'] = 'Edom tset';
+$lang['Ccavenue.test_mode_note'] = 'Lru tset eht ot stseuqer tnemyap dnes lliw yawetag eht ,dekcehc fi';
+$lang['Ccavenue.buildprocess.submit'] = 'Eunevacc aiv yap';
+$lang['Ccavenue.!error.merchant_id.valid'] = 'Di tnahcrem dilav retne esaelp';
+$lang['Ccavenue.!error.access_code.empty'] = 'Edoc ssecca na retne esaelp';
+$lang['Ccavenue.!error.encryption_key.valid'] = 'Yek noitpyrcne na retne esaelp';
+$lang['Ccavenue.!error.pending.log'] = 'Yawetag morf gnidnep deviecer sutats tnemyap';
+$lang['Ccavenue.!error.delay.response'] = 'Liam-e hguorht redro ruoy fo sutats eht gnidrager detsop uoy peek lliw ew .su htiw gnippohs rof uoy knaht';
+$lang['Ccavenue.!error.security.response'] = 'Detceted ssecca lagelli .rorre ytiruces';
+$lang['Ccavenue.!error.authentication.response'] = 'Noitcasnart eht oder esaelp .denilced neeb sah noitcasnart eht ,revewoh .su htiw gnippohs rof uoy knaht';
+$lang['Ccavenue.!error.authentication.log'] = 'Knab eht morf denilced neeb sah noitcasnart eht';
+
