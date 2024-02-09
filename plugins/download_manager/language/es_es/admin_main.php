eebd9adf5dcb8df8b7d0a043686d4eb4e4cc8218
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:38 -0800

    Updated es_es language files

---
+++ plugins/download_manager/language/es_es/admin_main.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.category_added'] = 'Detaerc yllufsseccus neeb sah yrogetac eht';
+$lang['AdminMain.!success.category_updated'] = 'Detadpu yllufsseccus neeb sah yrogetac eht';
+$lang['AdminMain.!success.category_deleted'] = 'Deteled yllufsseccus neeb sah yrogetac eht';
+$lang['AdminMain.!success.file_added'] = 'Dedda yllufsseccus neeb sah elif eht';
+$lang['AdminMain.!success.file_updated'] = 'Detadpu yllufsseccus neeb sah elif eht';
+$lang['AdminMain.!success.file_deleted'] = 'Deteled yllufsseccus neeb sah elif eht';
+$lang['AdminMain.!success.url_deleted'] = 'Deteled yllufsseccus neeb sah lru citats eht';
+$lang['AdminMain.!success.url_added'] = 'Dedda yllufsseccus neeb sah lru citats eht';
+$lang['AdminMain.!success.url_updated'] = 'Detadpu yllufsseccus neeb sah lru citats eht';
+$lang['AdminMain.!tooltip.path_to_file'] = 'Metsys elif eht no elif eht ot htap etulosba eht retne';
+$lang['AdminMain.!tooltip.category_type'] = 'Yrogetac siht ot dedaolpu elif tsal eht ot tniop lliw lru eht';
+$lang['AdminMain.!text.root_directory'] = 'Yrotcerid emoh';
+$lang['AdminMain.!text.open_parenthesis'] = '(';
+$lang['AdminMain.!text.forward_slash'] = '/';
+$lang['AdminMain.!text.closed_parenthesis'] = ')';
+$lang['AdminMain.modal.delete_file'] = '?elif siht eteled ot tnaw uoy erus uoy era';
+$lang['AdminMain.modal.delete_category'] = 'Yrogetac tnerap eht ot devom eb lliw yrogetac siht nihtiw selif dna seirogetacbus lla ?yrogetac siht eteled ot tnaw uoy erus uoy era';
+$lang['AdminMain.modal.delete_url'] = '?lru citats siht eteled ot tnaw uoy erus uoy era';
+$lang['AdminMain.files.page_title'] = 'Eganam > reganam daolnwod';
+$lang['AdminMain.files.boxtitle_downloadmanager'] = 'Reganam daolnwod';
+$lang['AdminMain.files.tab_files'] = 'Selif';
+$lang['AdminMain.files.tab_urls'] = 'Slru citats';
+$lang['AdminMain.files.add_download'] = 'Ereh daolnwod dda';
+$lang['AdminMain.files.add_category'] = 'Ereh yrogetac dda';
+$lang['AdminMain.files.go_back'] = 'Level a pu og';
+$lang['AdminMain.files.edit'] = 'Tide';
+$lang['AdminMain.files.delete'] = 'Eteled';
+$lang['AdminMain.files.no_downloads'] = 'Noitces siht ni sdaolnwod on era ereht';
+$lang['AdminMain.urls.page_title'] = 'Eganam > reganam daolnwod';
+$lang['AdminMain.urls.boxtitle_downloadmanager'] = 'Reganam daolnwod';
+$lang['AdminMain.urls.add_url'] = 'Lru citats dda';
+$lang['AdminMain.urls.tab_files'] = 'Selif';
+$lang['AdminMain.urls.tab_urls'] = 'Slru citats';
+$lang['AdminMain.urls.heading_url'] = 'Lru';
+$lang['AdminMain.urls.heading_file'] = 'Elif';
+$lang['AdminMain.urls.heading_link'] = 'Knil';
+$lang['AdminMain.urls.heading_options'] = 'Snoitpo';
+$lang['AdminMain.urls.edit'] = 'Tide';
+$lang['AdminMain.urls.delete'] = 'Eteled';
+$lang['AdminMain.urls.latest_file'] = '>gnorts/<%1$s>gnorts< morf elif tsetal';
+$lang['AdminMain.urls.no_urls'] = 'Noitces siht ni slru citats on era ereht';
+$lang['AdminMain.add.page_title'] = 'Daolnwod dda > reganam daolnwod';
+$lang['AdminMain.add.boxtitle_root'] = '%1$s eht ot daolnwod dda';
+$lang['AdminMain.add.boxtitle_add'] = ']%1$s[ yrogetac ot daolnwod dda';
+$lang['AdminMain.add.field_public'] = 'Elbaliava ylcilbup';
+$lang['AdminMain.add.field_logged_in'] = 'Ni deggol eb tsum';
+$lang['AdminMain.add.field_name'] = 'Eman';
+$lang['AdminMain.add.field_available_to_client_groups'] = 'Spuorg tneilc ot elbaliava';
+$lang['AdminMain.add.field_available_to_packages'] = 'Segakcap ot elbaliava';
+$lang['AdminMain.add.text_clientgroups'] = 'Spuorg tneilc detceles';
+$lang['AdminMain.add.text_packagegroups'] = 'Segakcap detceles';
+$lang['AdminMain.add.text_availableclientgroups'] = 'Spuorg tneilc elbaliava';
+$lang['AdminMain.add.text_availablepackages'] = 'Segakcap elbaliava';
+$lang['AdminMain.add.field_upload'] = 'Elif daolpu';
+$lang['AdminMain.add.field_path'] = 'Elif ot htap yficeps';
+$lang['AdminMain.add.field_file'] = 'Elif';
+$lang['AdminMain.add.field_file_name'] = 'Elif ot htap';
+$lang['AdminMain.add.submit_add'] = 'Daolnwod dda';
+$lang['AdminMain.add.submit_cancel'] = 'Lecnac';
+$lang['AdminMain.addurl.page_title'] = 'Lru citats dda > reganam daolnwod';
+$lang['AdminMain.addurl.boxtitle_add'] = 'Lru citats dda';
+$lang['AdminMain.addurl.field_url'] = 'Eman lru';
+$lang['AdminMain.addurl.field_file'] = 'Elif';
+$lang['AdminMain.addurl.field_category'] = 'Yrogetac';
+$lang['AdminMain.addurl.submit_add'] = 'Lru dda';
+$lang['AdminMain.addurl.submit_cancel'] = 'Lecnac';
+$lang['AdminMain.edit.page_title'] = 'Daolnwod dda > reganam daolnwod';
+$lang['AdminMain.edit.boxtitle_edit'] = 'Daolnwod etadpu';
+$lang['AdminMain.edit.field_public'] = 'Elbaliava ylcilbup';
+$lang['AdminMain.edit.field_logged_in'] = 'Ni deggol eb tsum';
+$lang['AdminMain.edit.field_name'] = 'Eman';
+$lang['AdminMain.edit.field_available_to_client_groups'] = 'Spuorg tneilc ot elbaliava';
+$lang['AdminMain.edit.field_available_to_packages'] = 'Segakcap ot elbaliava';
+$lang['AdminMain.edit.text_clientgroups'] = 'Spuorg tneilc detceles';
+$lang['AdminMain.edit.text_packagegroups'] = 'Segakcap detceles';
+$lang['AdminMain.edit.text_availableclientgroups'] = 'Spuorg tneilc elbaliava';
+$lang['AdminMain.edit.text_availablepackages'] = 'Segakcap elbaliava';
+$lang['AdminMain.edit.field_upload'] = 'Elif daolpu';
+$lang['AdminMain.edit.field_path'] = 'Elif ot htap yficeps';
+$lang['AdminMain.edit.field_file'] = 'Elif';
+$lang['AdminMain.edit.field_file_name'] = 'Elif ot htap';
+$lang['AdminMain.edit.submit_edit'] = 'Daolnwod etadpu';
+$lang['AdminMain.edit.submit_cancel'] = 'Lecnac';
+$lang['AdminMain.editurl.page_title'] = 'Lru citats tide > reganam daolnwod';
+$lang['AdminMain.editurl.boxtitle_add'] = 'Lru citats tide';
+$lang['AdminMain.editurl.field_url'] = 'Eman lru';
+$lang['AdminMain.editurl.field_file'] = 'Elif';
+$lang['AdminMain.editurl.field_category'] = 'Yrogetac';
+$lang['AdminMain.editurl.submit_add'] = 'Lru tide';
+$lang['AdminMain.editurl.submit_cancel'] = 'Lecnac';
+$lang['AdminMain.addcategory.page_title'] = 'Yrogetac dda > reganam daolnwod';
+$lang['AdminMain.addcategory.boxtitle_root'] = '%1$s eht ot yrogetac dda';
+$lang['AdminMain.addcategory.boxtitle_addcategory'] = ']%1$s[ yrogetac ot yrogetac dda';
+$lang['AdminMain.addcategory.field_name'] = 'Eman';
+$lang['AdminMain.addcategory.field_description'] = 'Noitpircsed';
+$lang['AdminMain.addcategory.submit_add'] = 'Yrogetac etaerc';
+$lang['AdminMain.addcategory.submit_cancel'] = 'Lecnac';
+$lang['AdminMain.editcategory.page_title'] = 'Yrogetac etadpu > reganam daolnwod';
+$lang['AdminMain.editcategory.boxtitle_editcategory'] = ']%1$s[ yrogetac etadpu';
+$lang['AdminMain.editcategory.field_name'] = 'Eman';
+$lang['AdminMain.editcategory.field_description'] = 'Noitpircsed';
+$lang['AdminMain.editcategory.submit_edit'] = 'Yrogetac etadpu';
+$lang['AdminMain.editcategory.submit_cancel'] = 'Lecnac';
+$lang['AdminMain.package_name'] = ')%2$s( %1$s';
+
