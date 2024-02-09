03a4d20a33653272f9cf35f706cb400a6f9da5f2
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 12:17:46 -0800

    Updated el_gr language files

---
+++ @el_gr/plugins/domains/language/el_gr/admin_main.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.change_auto_renewal'] = 'Η αυτόματη ανανέωση domain έχει ενημερωθεί!';
+$lang['AdminMain.!success.domain_renewal'] = 'Ο τομέας ανανεώθηκε!';
+$lang['AdminMain.!success.update_nameservers'] = 'Οι διακομιστές ονομάτων τομέα έχουν ενημερωθεί!';
+$lang['AdminMain.!success.domains_pushed'] = 'Οι επιλεγμένοι τομείς μεταφέρθηκαν επιτυχώς στον νέο πελάτη.';
+$lang['AdminMain.index.page_title'] = 'Πελάτης #%1$s Domains';
+$lang['AdminMain.index.boxtitle_domains'] = 'Τομείς';
+$lang['AdminMain.index.heading_addons'] = 'Πρόσθετα';
+$lang['AdminMain.index.heading_status'] = 'Κατάσταση';
+$lang['AdminMain.index.heading_domain'] = 'Τομέας';
+$lang['AdminMain.index.heading_term'] = 'Όρος';
+$lang['AdminMain.index.heading_datecreated'] = 'Ημερομηνία δημιουργίας';
+$lang['AdminMain.index.heading_daterenews'] = 'Ημερομηνία Ανανεώνει';
+$lang['AdminMain.index.heading_dateexpires'] = 'Ημερομηνία Λήξης';
+$lang['AdminMain.index.heading_datesuspended'] = 'Ημερομηνία αναστολής';
+$lang['AdminMain.index.heading_datecanceled'] = 'Ημερομηνία Ακύρωσης';
+$lang['AdminMain.index.heading_options'] = 'Επιλογές';
+$lang['AdminMain.index.category_active'] = 'Ενεργό';
+$lang['AdminMain.index.category_pending'] = 'Εκκρεμεί';
+$lang['AdminMain.index.category_suspended'] = 'Αναστολή';
+$lang['AdminMain.index.category_canceled'] = 'Ακυρώθηκε';
+$lang['AdminMain.index.categorylink_newservice'] = 'Νέος τομέας';
+$lang['AdminMain.index.recurring_term'] = '%1$s %2$s @ %3$s';
+$lang['AdminMain.index.option_parent'] = 'Γονέας';
+$lang['AdminMain.index.option_manage'] = 'Διαχείριση';
+$lang['AdminMain.index.option_delete'] = 'Διαγραφή';
+$lang['AdminMain.index.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή την υπηρεσία;';
+$lang['AdminMain.index.no_results'] = 'Δεν υπάρχουν υπηρεσίες με αυτό το καθεστώς.';
+$lang['AdminMain.index.text_never'] = 'Ποτέ';
+$lang['AdminMain.index.text_on'] = 'Στο';
+$lang['AdminMain.index.text_off'] = 'Off';
+$lang['AdminMain.index.change_auto_renewal'] = 'Αλλαγή αυτόματης ανανέωσης';
+$lang['AdminMain.index.domain_renewal'] = 'Ανανέωση τομέα';
+$lang['AdminMain.index.update_nameservers'] = 'Ενημέρωση διακομιστών ονομάτων';
+$lang['AdminMain.index.domain_push_to_client'] = 'Πίεση στον πελάτη';
+$lang['AdminMain.index.field_actionsubmit'] = 'Υποβολή';
+$lang['AdminMain.domains.action.field_years'] = 'Έτη';
+$lang['AdminMain.domains.action.field_nameservers'] = 'Διακομιστές ονομάτων';
+$lang['AdminMain.domains.action.field_client'] = 'Πελάτης';
+$lang['AdminMain.getfilters.any'] = 'Οποιοδήποτε';
+$lang['AdminMain.getfilters.field_package_name'] = 'TLD';
+$lang['AdminMain.getfilters.field_service_meta'] = 'Όνομα τομέα';
+
