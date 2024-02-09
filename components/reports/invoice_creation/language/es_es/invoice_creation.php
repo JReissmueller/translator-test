13054f18eab8d46c1c1e7546554001ebc3a5fef1
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:23:40 -0800

    Updated es_es language files

--- components/reports/invoice_creation/language/es_es/invoice_creation.php
+++ components/reports/invoice_creation/language/es_es/invoice_creation.php
@@ -9,19 +9,19 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['InvoiceCreation.name'] = 'Noitaerc eciovni';
-$lang['InvoiceCreation.options.field_start_date'] = 'Etad trats';
-$lang['InvoiceCreation.options.field_end_date'] = 'Etad dne';
-$lang['InvoiceCreation.options.field_status'] = 'Sutats';
-$lang['InvoiceCreation.option.any'] = 'Yna';
-$lang['InvoiceCreation.heading.id_code'] = '# eciovni';
-$lang['InvoiceCreation.heading.client_id_code'] = '# tneilc';
-$lang['InvoiceCreation.heading.subtotal'] = 'Latotbus';
-$lang['InvoiceCreation.heading.total'] = 'Latot';
-$lang['InvoiceCreation.heading.paid'] = 'Diap';
-$lang['InvoiceCreation.heading.currency'] = 'Ycnerruc';
-$lang['InvoiceCreation.heading.status'] = 'Sutats';
-$lang['InvoiceCreation.heading.date_billed'] = 'Dellib etad';
-$lang['InvoiceCreation.heading.date_due'] = 'Eud etad';
-$lang['InvoiceCreation.heading.date_closed'] = 'Desolc etad';
+$lang['InvoiceCreation.name'] = 'Creación de facturas';
+$lang['InvoiceCreation.options.field_start_date'] = 'Fecha de inicio';
+$lang['InvoiceCreation.options.field_end_date'] = 'Fecha final';
+$lang['InvoiceCreation.options.field_status'] = 'Estado';
+$lang['InvoiceCreation.option.any'] = 'Cualquier';
+$lang['InvoiceCreation.heading.id_code'] = 'Factura #';
+$lang['InvoiceCreation.heading.client_id_code'] = 'Cliente';
+$lang['InvoiceCreation.heading.subtotal'] = 'Subtotal';
+$lang['InvoiceCreation.heading.total'] = 'Total';
+$lang['InvoiceCreation.heading.paid'] = 'Pagado';
+$lang['InvoiceCreation.heading.currency'] = 'Moneda';
+$lang['InvoiceCreation.heading.status'] = 'Estado';
+$lang['InvoiceCreation.heading.date_billed'] = 'Fecha de facturación';
+$lang['InvoiceCreation.heading.date_due'] = 'Fecha de vencimiento';
+$lang['InvoiceCreation.heading.date_closed'] = 'Fecha de cierre';
 
