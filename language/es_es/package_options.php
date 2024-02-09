74b123a50498064d8e376b4cb85fbf68a6308fa5
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:59 -0800

    Updated es_es language files

---
+++ language/es_es/package_options.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Package Options
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageOptions.!error.company_id.exists'] = 'Di ynapmoc dilavni';
+$lang['PackageOptions.!error.label.empty'] = 'Lebal a retne esaelp';
+$lang['PackageOptions.!error.label.length'] = 'Htgnel ni sretcarahc 821 deecxe ton yam lebal eht';
+$lang['PackageOptions.!error.name.empty'] = 'Noitpo siht rof eman a retne esaelp';
+$lang['PackageOptions.!error.name.length'] = 'Htgnel ni sretcarahc 821 deecxe ton yam eman eht';
+$lang['PackageOptions.!error.type.valid'] = 'Epyt noitpo na tceles esaelp';
+$lang['PackageOptions.!error.values.count'] = 'Sepyt drowssap ro ,aeratxet ,txet ,ytitnauq ,xobkcehc rof yrtne eulav noitpo eno eb ylno yam ereht';
+$lang['PackageOptions.!error.values.unique'] = 'Euqinu eb tsum seulav noitpo';
+$lang['PackageOptions.!error.values.select_value'] = 'Sretcarahc laiceps dilavni sniatnoc eulav noitpo eno tsael ta';
+$lang['PackageOptions.!error.values.active_status'] = 'Evitca eb tsum eulav noitpo eno tsael ta';
+$lang['PackageOptions.!error.values.single_default_value'] = 'Eulav tluafed eht sa detceles eb yam eulav eno ylno';
+$lang['PackageOptions.!error.values[][step].valid'] = 'Retaerg ro 1 fo eulav a eb tsum dna ,epyt ytitnauq eht rof tes eb ylno yam eulav pets a';
+$lang['PackageOptions.!error.values[][min].valid'] = 'Retaerg ro 0 fo eulav a eb tsum dna ,epyt ytitnauq eht rof tes eb ylno yam eulav timil muminim a';
+$lang['PackageOptions.!error.values[][max].valid'] = 'Retaerg ro 1 fo eulav a eb tsum dna ,epyt ytitnauq eht rof tes eb ylno yam eulav timil mumixam eht';
+$lang['PackageOptions.!error.values[][name].empty'] = 'Eulav noitpo eht rof eman a retne esaelp';
+$lang['PackageOptions.!error.values[][name].length'] = 'Htgnel ni sretcarahc 821 deecxe ton yam eman eulav noitpo eht';
+$lang['PackageOptions.!error.values[][value].length'] = 'Htgnel ni sretcarahc 552 deecxe ton yam eulav noitpo eht';
+$lang['PackageOptions.!error.values[][value].edit_in_use'] = 'Ecivres a yb esu ni yltnerruc si ti esuaceb degnahc eb ton yam eulav noitpo eht';
+$lang['PackageOptions.!error.values[][value].delete_in_use'] = 'Ecivres a yb esu ni yltnerruc si ti esuaceb deteled eb ton yam eulav noitpo eht';
+$lang['PackageOptions.!error.values[][status].valid'] = 'Sutats eulav noitpo dilav a tceles esaelp';
+$lang['PackageOptions.!error.values[][default].inactive'] = 'Eulav tluafed eht sa tes eb ton yam eulav noitpo evitcani na';
+$lang['PackageOptions.!error.values[][default].qty_valid'] = 'Pets dna ,xam ,nim derugifnoc eht rof dilavni si detceles ytitnauq eulav tluafed eht';
+$lang['PackageOptions.!error.values[][default].format'] = 'Rebmun a eb tsum tluafed eht';
+$lang['PackageOptions.!error.values[][id].exists'] = 'Di eulav noitpo egakcap dilavni';
+$lang['PackageOptions.!error.groups.exists'] = 'Ynapmoc emas eht ot gnoleb ton seod ro tsixe ton seod nevig sdi puorg noitpo egakcap eht fo eno tsael ta';
+$lang['PackageOptions.!error.option_id.exists'] = 'Di noitpo egakcap dilavni';
+$lang['PackageOptions.!error.hidden.valid'] = '1 ro 0 ot tes eb tsum noitpo siht edih ot rehtehw';
+$lang['PackageOptions.!error.values[][pricing][][id].exists'] = 'Di gnicirp noitpo egakcap dilavni';
+$lang['PackageOptions.!error.values[][pricing][][id].delete_in_use'] = 'Ecivres a yb esu ni yltnerruc si ti esuaceb deteled eb ton yam gnicirp eulav noitpo eht';
+$lang['PackageOptions.!error.values[][pricing][][term].format'] = 'Rebmun a eb tsum mret';
+$lang['PackageOptions.!error.values[][pricing][][term].length'] = 'Sretcarahc 5 deecxe ton yam htgnel mret';
+$lang['PackageOptions.!error.values[][pricing][][term].valid'] = '0 naht retaerg eb tsum mret eht';
+
