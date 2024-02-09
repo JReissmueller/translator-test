9ea6d671edd2710acd59b4abf6551e0ca74f1aac
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:22:11 -0800

    Updated es_es language files

--- components/gateways/nonmerchant/ccavenue/language/es_es/ccavenue.php
+++ components/gateways/nonmerchant/ccavenue/language/es_es/ccavenue.php
@@ -9,23 +9,23 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Ccavenue.name'] = 'Tnemyap eunevacc';
-$lang['Ccavenue.description'] = 'Senigne tibed tcerid tsegral s\'aidni fo eno si eunevacc ,sisab emit-laer a no sknab naidni 55 naht erom gnitcennoc snoitpo tnemyap 042 revo htiw';
-$lang['Ccavenue.merchant_id'] = 'Di tnahcrem';
-$lang['Ccavenue.merchant_id_note'] = 'Syek ipa rednu tnuocca eunevacc ruoy ni elbaliava si di tnahcrem ruoy';
-$lang['Ccavenue.encryption_key'] = 'Yek noitpyrcne';
-$lang['Ccavenue.encryption_key_note'] = 'Syek ipa rednu tnuocca eunevacc ruoy ni detareneg si siht';
-$lang['Ccavenue.access_code'] = 'Edoc ssecca';
-$lang['Ccavenue.access_code_note'] = 'Syek ipa rednu tnuocca eunevacc ruoy ni dnuof eb nac siht';
-$lang['Ccavenue.test_mode'] = 'Edom tset';
-$lang['Ccavenue.test_mode_note'] = 'Lru tset eht ot stseuqer tnemyap dnes lliw yawetag eht ,dekcehc fi';
-$lang['Ccavenue.buildprocess.submit'] = 'Eunevacc aiv yap';
-$lang['Ccavenue.!error.merchant_id.valid'] = 'Di tnahcrem dilav retne esaelp';
-$lang['Ccavenue.!error.access_code.empty'] = 'Edoc ssecca na retne esaelp';
-$lang['Ccavenue.!error.encryption_key.valid'] = 'Yek noitpyrcne na retne esaelp';
-$lang['Ccavenue.!error.pending.log'] = 'Yawetag morf gnidnep deviecer sutats tnemyap';
-$lang['Ccavenue.!error.delay.response'] = 'Liam-e hguorht redro ruoy fo sutats eht gnidrager detsop uoy peek lliw ew .su htiw gnippohs rof uoy knaht';
-$lang['Ccavenue.!error.security.response'] = 'Detceted ssecca lagelli .rorre ytiruces';
-$lang['Ccavenue.!error.authentication.response'] = 'Noitcasnart eht oder esaelp .denilced neeb sah noitcasnart eht ,revewoh .su htiw gnippohs rof uoy knaht';
-$lang['Ccavenue.!error.authentication.log'] = 'Knab eht morf denilced neeb sah noitcasnart eht';
+$lang['Ccavenue.name'] = 'Pago CCAvenue';
+$lang['Ccavenue.description'] = 'Con más de 240 opciones de pago que conectan más de 55 bancos indios en tiempo real, CCAvenue es uno de los mayores motores de domiciliación bancaria de la India.';
+$lang['Ccavenue.merchant_id'] = 'ID de comerciante';
+$lang['Ccavenue.merchant_id_note'] = 'Tu ID de comerciante está disponible en tu cuenta de CCAvenue en Claves API';
+$lang['Ccavenue.encryption_key'] = 'Clave de cifrado';
+$lang['Ccavenue.encryption_key_note'] = 'Se genera en tu cuenta de CCAvenue en Claves API.';
+$lang['Ccavenue.access_code'] = 'Código de acceso';
+$lang['Ccavenue.access_code_note'] = 'Puedes encontrarlo en tu cuenta de CCAvenue en Claves API.';
+$lang['Ccavenue.test_mode'] = 'Modo de prueba';
+$lang['Ccavenue.test_mode_note'] = 'Si está marcada, la pasarela enviará solicitudes de pago a la URL de prueba.';
+$lang['Ccavenue.buildprocess.submit'] = 'Pagar a través de CCAvenue';
+$lang['Ccavenue.!error.merchant_id.valid'] = 'Introduzca un ID de comerciante válido.';
+$lang['Ccavenue.!error.access_code.empty'] = 'Por favor, introduzca un código de acceso.';
+$lang['Ccavenue.!error.encryption_key.valid'] = 'Por favor, introduzca una clave de encriptación.';
+$lang['Ccavenue.!error.pending.log'] = 'Estado del pago recibido pendiente de la pasarela.';
+$lang['Ccavenue.!error.delay.response'] = 'Gracias por comprar con nosotros. Le mantendremos informado sobre el estado de su pedido por correo electrónico.';
+$lang['Ccavenue.!error.security.response'] = 'Error de seguridad. Acceso ilegal detectado.';
+$lang['Ccavenue.!error.authentication.response'] = 'Gracias por comprar con nosotros. Sin embargo, la transacción ha sido rechazada. Por favor, vuelva a realizar la transacción.';
+$lang['Ccavenue.!error.authentication.log'] = 'El banco ha rechazado la transacción.';
 
