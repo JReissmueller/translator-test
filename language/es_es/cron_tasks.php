a71513ecf58122439cc8185a7a44595247214b48
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:35 -0800

    Updated es_es language files

---
+++ language/es_es/cron_tasks.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Cron Tasks
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CronTasks.!error.key.unique'] = 'Nekat ydaerla si dedivorp yek ksat norc eht';
+$lang['CronTasks.!error.key.length'] = 'Sretcarahc 46 deecxe ton yam htgnel yek ksat norc eht';
+$lang['CronTasks.!error.task_type.format'] = 'Metsys ro ,nigulp ,eludom eb tsum .epyt ksat dilavni';
+$lang['CronTasks.!error.dir.length'] = 'Sretcarahc 46 deecxe ton yam htgnel yrotcerid eht';
+$lang['CronTasks.!error.run_id.exists'] = 'Di nur ksat norc dilavni';
+$lang['CronTasks.!error.id.exists'] = 'Di ksat norc dilavni';
+$lang['CronTasks.!error.name.empty'] = 'Eman a retne esaelp';
+$lang['CronTasks.!error.is_lang.format'] = 'Rebmun a eb tsum gnal_si';
+$lang['CronTasks.!error.is_lang.length'] = 'Retcarahc 1 deecxe ton yam htgnel gnal_si';
+$lang['CronTasks.!error.enabled.format'] = 'Rebmun a eb tsum delbane';
+$lang['CronTasks.!error.enabled.length'] = 'Retcarahc 1 deecxe ton yam htgnel delbane';
+$lang['CronTasks.!error.interval.format'] = 'Setunim gnitneserper ,rebmun a eb tsum lavretni';
+$lang['CronTasks.!error.time.format'] = 'Tamrof dilavni na ni si emit';
+$lang['CronTasks.!error.type.format'] = 'Lavretni ro emit rehtie eb tsum .epyt ksat norc dilavni';
+$lang['CronTasks.task_type.system'] = 'Metsys';
+$lang['CronTasks.task_type.plugin'] = 'Nigulp';
+$lang['CronTasks.task_type.module'] = 'Eludom';
+$lang['CronTasks.crontask.name.create_invoice'] = 'Eciovni etaerc';
+$lang['CronTasks.crontask.description.create_invoice'] = 'Deificeps emit eht ta yliad ecno snur hcihw ,ksat siht hguorht deciovni era secivres gniwener dna seciovni gnirrucer';
+$lang['CronTasks.crontask.name.apply_invoice_late_fees'] = 'Seef etal eciovni ylppa';
+$lang['CronTasks.crontask.description.apply_invoice_late_fees'] = 'Eud retfa syad fo rebmun derugifnoc a seciovni nepo ot seef etal seilppa';
+$lang['CronTasks.crontask.name.autodebit'] = 'Tibed otua';
+$lang['CronTasks.crontask.description.autodebit'] = 'Deificeps emit eht ta yliad seciovni nepo ffo yap ot nur eb lliw tibed otua rof detceles stnuocca tnemyap';
+$lang['CronTasks.crontask.name.payment_reminders'] = 'Srednimer tnemyap';
+$lang['CronTasks.crontask.description.payment_reminders'] = 'Deificeps emit eht ta yliad tnes era seciton etal dna srednimer tnemyap';
+$lang['CronTasks.crontask.name.apply_payments'] = 'Seciovni nepo ot stnemyap ylppa';
+$lang['CronTasks.crontask.description.apply_payments'] = 'Detceles lavretni eht ta yllacitamotua seciovni nepo ot deilppa era stiderc esool';
+$lang['CronTasks.crontask.name.process_service_changes'] = 'Segnahc ecivres ssecorp';
+$lang['CronTasks.crontask.description.process_service_changes'] = 'Detceles lavretni eht ta dessecorp era )sedargpu .g.e( segnahc ecivres deueuq diap';
+$lang['CronTasks.crontask.name.process_renewing_services'] = 'Slawener ecivres ssecorp';
+$lang['CronTasks.crontask.description.process_renewing_services'] = 'Detceles lavretni eht ta dewener era seludom ot dehcatta era taht secivres gniwener';
+$lang['CronTasks.crontask.name.provision_pending_services'] = 'Secivres gnidnep diap noisivorp';
+$lang['CronTasks.crontask.description.provision_pending_services'] = 'Detceles lavretni eht ta detavitca era secivres gnidnep diap';
+$lang['CronTasks.crontask.name.cancel_scheduled_services'] = 'Secivres deludehcs lecnac';
+$lang['CronTasks.crontask.description.cancel_scheduled_services'] = 'Detceles lavretni eht ta devomer era tes setad noitallecnac erutuf htiw secivres';
+$lang['CronTasks.crontask.name.card_expiration_reminders'] = 'Htnom fo ht51 srednimer noitaripxe drac';
+$lang['CronTasks.crontask.description.card_expiration_reminders'] = 'Deificeps emit eht ta htnom taht gniripxe sdrac tiderc rof htnom eht fo ht51 eht no tnes eb lliw rednimer a';
+$lang['CronTasks.crontask.name.deliver_invoices'] = 'Seciovni reviled';
+$lang['CronTasks.crontask.description.deliver_invoices'] = 'Detceles lavretni eht ta tnes eb lliw yreviled rof deludehcs era taht seciovni';
+$lang['CronTasks.crontask.name.backups_amazons3'] = 'Spukcab 3s nozama';
+$lang['CronTasks.crontask.description.backups_amazons3'] = '3s nozama > pukcab > sgnittes metsys rednu deludehcs era spukcab 3s nozama';
+$lang['CronTasks.crontask.name.backups_sftp'] = 'Spukcab ptfs';
+$lang['CronTasks.crontask.description.backups_sftp'] = 'Ptf eruces > pukcab > sgnittes metsys rednu deludehcs era spukcab ptfs';
+$lang['CronTasks.crontask.name.suspend_services'] = 'Secivres dnepsus';
+$lang['CronTasks.crontask.description.suspend_services'] = 'Deificeps emit eht ta yliad dednepsus eb lliw secivres eud tsap';
+$lang['CronTasks.crontask.name.exchange_rates'] = 'Setadpu etar egnahcxe';
+$lang['CronTasks.crontask.description.exchange_rates'] = 'Dekcolb gnieb fo ksir rof yliad eciwt naht erom siht nur ot dednemmocer ton si ti .deificeps lavretni eht ta detadpu eb lliw setar egnahcxe';
+$lang['CronTasks.crontask.name.deliver_reports'] = 'Stroper reviled';
+$lang['CronTasks.crontask.description.deliver_reports'] = 'Deificeps emit eht ta yliad dereviled eb lliw stroper rehto dna ,ytilibail xat ,noitareneg eciovni ,r/a';
+$lang['CronTasks.crontask.name.cleanup_logs'] = 'Sgol pu naelc';
+$lang['CronTasks.crontask.description.cleanup_logs'] = 'Deificeps emit eht ta sgnittes noitneter rieht no gnidneped yliad detator eb lliw sgol rehto dna ,eludom ,yawetag dlo';
+$lang['CronTasks.crontask.name.unsuspend_services'] = 'Secivres dnepsusnu';
+$lang['CronTasks.crontask.description.unsuspend_services'] = 'Detceles lavretni eht ta dednepsusnu eb lliw diap neeb evah taht secivres dednepsus';
+$lang['CronTasks.crontask.name.transition_quotations'] = 'Snoitatouq noitisnart';
+$lang['CronTasks.crontask.description.transition_quotations'] = 'Deripxe sa ,etad dilav eht tsap snoitatouq kram';
+$lang['CronTasks.crontask.name.license_validation'] = 'Noitadilav esnecil';
+
