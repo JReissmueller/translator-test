540e5e3bf5d7d0815ece749455f999a771b34022
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:37 -0800

    Updated es_es language files

---
+++ plugins/domains/language/es_es/domains_plugin.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Domains Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsPlugin.name'] = 'Reganam niamod';
+$lang['DomainsPlugin.description'] = 'Esahp ateb ni yltnerruc si nigulp siht .selas dna tnemeganam niamod fo stcepsa suoirav eldnah ot desu';
+$lang['DomainsPlugin.!error.module_id.exists'] = 'Di eludom dilavni';
+$lang['DomainsPlugin.getCronTasks.domain_synchronization'] = 'Noitazinorhcnys niamod';
+$lang['DomainsPlugin.getCronTasks.domain_synchronization_description'] = 'Eludom rartsiger rieht morf etad yripxe eht htiw secivres niamod ezinorhcnys';
+$lang['DomainsPlugin.getCronTasks.domain_tld_synchronization'] = 'Noitazinorhcnys dlt niamod';
+$lang['DomainsPlugin.getCronTasks.domain_tld_synchronization_description'] = 'Eludom rartsiger rieht morf gnicirp dlt eht htiw segakcap ezinorhcnys';
+$lang['DomainsPlugin.getCronTasks.domain_term_change'] = 'Mret niamod egnahc';
+$lang['DomainsPlugin.getCronTasks.domain_term_change_description'] = 'Mret ylraey a ot raey a naht regnol mret a htiw secivres egnahc';
+$lang['DomainsPlugin.getCronTasks.domain_renewal_reminders'] = 'Srednimer noitaripxe dnes';
+$lang['DomainsPlugin.getCronTasks.domain_renewal_reminders_description'] = 'Etad noitaripxe rieht ot esolc gniward era taht sniamod rof srednimer liame dnes';
+$lang['DomainsPlugin.nav_secondary_staff.domain_options'] = 'Snoitpo niamod';
+$lang['DomainsPlugin.nav_secondary_staff.domains'] = 'Sniamod';
+$lang['DomainsPlugin.nav_client.services'] = 'Secivres';
+$lang['DomainsPlugin.nav_client.domains'] = 'Sniamod';
+$lang['DomainsPlugin.card_client.getDomainCount'] = 'Sniamod';
+$lang['DomainsPlugin.permission.admin_domains'] = 'Sniamod';
+$lang['DomainsPlugin.permission.admin_domains.browse'] = 'Sniamod';
+$lang['DomainsPlugin.permission.admin_domains.tlds'] = 'Gnicirp dlt';
+$lang['DomainsPlugin.permission.admin_domains.registrars'] = 'Srartsiger';
+$lang['DomainsPlugin.permission.admin_domains.whois'] = 'Siohw';
+$lang['DomainsPlugin.permission.admin_domains.configuration'] = 'Noitarugifnoc';
+$lang['DomainsPlugin.tld_package_group.name'] = 'Segakcap gnicirp sdlt';
+$lang['DomainsPlugin.tld_package_group.description'] = 'Segakcap gnicirp dlt eht lla gniganam dna gnidih rof puorg egakcap a';
+$lang['DomainsPlugin.email_forwarding.name'] = 'Gnidrawrof liame';
+$lang['DomainsPlugin.email_forwarding.description'] = 'Gnidrawrof liame';
+$lang['DomainsPlugin.dns_management.name'] = 'Tnemeganam snd';
+$lang['DomainsPlugin.dns_management.description'] = 'Tnemeganam snd';
+$lang['DomainsPlugin.id_protection.name'] = 'Noitcetorp di';
+$lang['DomainsPlugin.id_protection.description'] = 'Noitcetorp di';
+$lang['DomainsPlugin.enabled'] = 'Delbane';
+$lang['DomainsPlugin.widget_staff_home.main'] = 'Sniamod';
+$lang['DomainsPlugin.widget_client_home.main'] = 'Sniamod';
+$lang['DomainsPlugin.upgrade.domains_dns_management_option_group'] = 'Niamod ruoy rof erom dna ,txt ,xm ,emanc ,a sa hcus sdrocer tide dna etaerc .edivorp ew srevres eman hguorht eman niamod ruoy rof enoz snd eht eganam';
+$lang['DomainsPlugin.upgrade.domains_email_forwarding_option_group'] = 'Sserdda liame lanretxe na ot drawrof taht niamod ruoy ta sesserdda liame etaerc';
+$lang['DomainsPlugin.upgrade.domains_id_protection_option_group'] = 'Etavirp noitamrofni ruoy gnipeek ,siohw cilbup eht morf sliated eseht edih lliw noitpo siht .selur nnaci fo trap sa noitamrofni tcatnoc dilav evah tsum seman niamod lla';
+
