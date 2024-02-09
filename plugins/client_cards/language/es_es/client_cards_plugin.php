de0cc92f3f6aad05a4cd0a420c4257ac63ed3e45
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:23 -0800

    Updated es_es language files

---
+++ plugins/client_cards/language/es_es/client_cards_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Client Cards Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientCardsPlugin.name'] = 'Sdrac tneilc';
+$lang['ClientCardsPlugin.description'] = 'Ecafretni tneilc eht no sdrac ni noitamrofni yrammus tneilc syalpsid';
+$lang['ClientCardsPlugin.card_client.services'] = 'Secivres';
+$lang['ClientCardsPlugin.card_client.invoices'] = 'Seciovni';
+
