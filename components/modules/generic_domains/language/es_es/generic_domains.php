0d59d19c2905707268fb88037dc99886cd09b1f8
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:54 -0800

    Updated es_es language files

---
+++ components/modules/generic_domains/language/es_es/generic_domains.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Generic Domains
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GenericDomains.name'] = 'Sniamod cireneg';
+$lang['GenericDomains.description'] = 'Noitartsiger niamod rof eludom cireneg a';
+$lang['GenericDomains.module_row'] = 'Wor eludom';
+$lang['GenericDomains.module_row_plural'] = 'Swor eludom';
+$lang['GenericDomains.domain.domain'] = 'Niamod';
+$lang['GenericDomains.transfer.domain'] = 'Niamod';
+$lang['GenericDomains.transfer.transfer_key'] = 'Edoc ppe';
+$lang['GenericDomains.package_fields.tld_options'] = 'Sdlt';
+$lang['GenericDomains.manage.text'] = 'Noitces ]seciton[ >- ]ofni ym[ rieht rednu meht ot sebircsbus rebmem ffats eht dna ,tes seciton ccb eseht sah puorg ffats rieht fi sliame noitallecnac dna noisnepsus ,noitaerc ecivres fo seipoc ccb eviecer yam ffats';
+
