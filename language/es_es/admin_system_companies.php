0b7961c9700d687811fdd9260c096cb9a78d1605
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:06 -0800

    Updated es_es language files

---
+++ language/es_es/admin_system_companies.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Admin System Companies
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemCompanies.!success.company_added'] = '!detaerc yllufsseccus neeb sah %1$s ynapmoc eht';
+$lang['AdminSystemCompanies.!success.company_edited'] = '!detadpu yllufsseccus neeb sah %1$s ynapmoc eht';
+$lang['AdminSystemCompanies.!success.company_deleted'] = '!devomer yllufsseccus neeb sah %1$s ynapmoc eht';
+$lang['AdminSystemCompanies.index.page_title'] = 'Seinapmoc > metsys > sgnittes';
+$lang['AdminSystemCompanies.index.boxtitle_companies'] = 'Seinapmoc';
+$lang['AdminSystemCompanies.index.categorylink_addcompany'] = 'Ynapmoc dda';
+$lang['AdminSystemCompanies.index.no_results'] = 'Seinapmoc on era ereht';
+$lang['AdminSystemCompanies.index.text_name'] = 'Eman';
+$lang['AdminSystemCompanies.index.text_hostname'] = 'Emantsoh';
+$lang['AdminSystemCompanies.index.text_address'] = 'Sserdda';
+$lang['AdminSystemCompanies.index.text_phone'] = 'Enohp';
+$lang['AdminSystemCompanies.index.text_options'] = 'Snoitpo';
+$lang['AdminSystemCompanies.index.option_edit'] = 'Tide';
+$lang['AdminSystemCompanies.index.option_delete'] = 'Eteled';
+$lang['AdminSystemCompanies.index.confirm_delete'] = 'Raeppasid lliw ynapmoc siht ot gniniatrep gnihtyreve ?ynapmoc siht eteled yllaer';
+$lang['AdminSystemCompanies.add.page_title'] = 'Ynapmoc dda > seinapmoc > metsys > sgnittes';
+$lang['AdminSystemCompanies.add.boxtitle_addcompany'] = 'Ynapmoc dda';
+$lang['AdminSystemCompanies.add.field_company'] = 'Morf sgnittes ynapmoc tropmi';
+$lang['AdminSystemCompanies.add.field_name'] = 'Eman';
+$lang['AdminSystemCompanies.add.field_hostname'] = 'Emantsoh';
+$lang['AdminSystemCompanies.add.field_address'] = 'Sserdda';
+$lang['AdminSystemCompanies.add.field_phone'] = 'Enohp';
+$lang['AdminSystemCompanies.add.field_fax'] = 'Xaf';
+$lang['AddSystemCompanies.add.field_companysubmit'] = 'Ynapmoc etaerc';
+$lang['AdminSystemCompanies.edit.page_title'] = 'Ynapmoc tide > seinapmoc > metsys > sgnittes';
+$lang['AdminSystemCompanies.edit.boxtitle_editcompany'] = 'Ynapmoc tide';
+$lang['AdminSystemCompanies.edit.field_name'] = 'Eman';
+$lang['AdminSystemCompanies.edit.field_hostname'] = 'Emantsoh';
+$lang['AdminSystemCompanies.edit.field_address'] = 'Sserdda';
+$lang['AdminSystemCompanies.edit.field_phone'] = 'Enohp';
+$lang['AdminSystemCompanies.edit.field_fax'] = 'Xaf';
+$lang['AddSystemCompanies.edit.field_companysubmit'] = 'Ynapmoc tide';
+$lang['AdminSystemCompanies.!tooltip.hostname'] = '\'0808:moc.niamod.niamodbus\' ro \'moc.niamod\' ,elpmaxe rof .rebmun trop lanoitpo dna ,dlt ,eman niamod eht ylno niatnoc dluohs emantsoh eht';
+
