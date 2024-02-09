5fa19d6b1285466cc8c16d9f26c595ba843aad87
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:50 -0800

    Updated es_es language files

---
+++ components/modules/cwatch/language/es_es/cwatch.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Cwatch
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CWatch.name'] = 'Hctawc';
+$lang['CWatch.description'] = 'Staerht rekcah fo noitacitsihpos gnisaercni eht tsniaga snoitacilppa bew dna srevres bew ,etisbew ruoy rof loot noitcetorp a';
+$lang['CWatch.module_row.name'] = 'Tnuocca ipa';
+$lang['CWatch.module_row_plural.name'] = 'Stnuocca ipa';
+$lang['CWatch.!success.sftp_test'] = '!lufsseccus saw noitcennoc ptfs';
+$lang['CWatch.!success.ftp_test'] = '!lufsseccus saw noitcennoc ptf';
+$lang['CWatch.!tooltip.initiateDns'] = 'Sdrocer snd eht fo nacs a trats ot rehtehw';
+$lang['CWatch.!tooltip.autoSsl'] = 'Etacifitrec lss eerf odomoc a llatsni ot rehtehw';
+$lang['CWatch.!error.username.valid'] = 'Emanresu a retne esaelp';
+$lang['CWatch.!error.password.valid'] = 'Drowssap a retne esaelp';
+$lang['CWatch.!error.password.valid_connection'] = 'Slaitnederc nevig eht gnisu noitcennoc a ekam ot elbanu';
+$lang['CWatch.!error.cwatch_email.format'] = 'Tamrof liame dilavni';
+$lang['CWatch.!error.cwatch_email.unique'] = 'Stsixe ydaerla liame nevig eht htiw remotsuc a';
+$lang['CWatch.!error.cwatch_firstname.empty'] = 'Eman tsrif a retne';
+$lang['CWatch.!error.cwatch_lastname.empty'] = 'Eman tsal a retne';
+$lang['CWatch.!error.cwatch_country.length'] = 'Ytnuoc dilavni';
+$lang['CWatch.!error.cwatch_domain.format'] = 'Moc.niamod .g.e ,eman niamod dilav a retne esaelp';
+$lang['CWatch.!error.cwatch_domain.unique'] = 'Niamod tnereffid a tceles esaelp .deretsiger ydaerla niamod';
+$lang['CWatch.!error.limit_exceeded'] = 'Timil ruoy gnicuder erofeb sniamod gnivomer yb sesnecil eerf esaelp  .deretne timil eht sdeecxe sniamod ot dengissa yltnerruc sesnecil fo rebmun eht';
+$lang['CWatch.!error.api.internal'] = 'Tseuqer eht ot dnopser ton did revres eht ro ,derrucco rorre lanretni na';
+$lang['CWatch.!error.sftp_test'] = 'Niaga yrt dna sgnittes ruoy kcehc esaelp !deliaf noitcennoc ptfs eht';
+$lang['CWatch.!error.ftp_test'] = 'Niaga yrt dna sgnittes ruoy kcehc esaelp !deliaf noitcennoc ptf eht';
+$lang['CWatch.manage.boxtitle'] = 'Hctawc eganam';
+$lang['CWatch.manage.title'] = 'Stnuocca ipa';
+$lang['CWatch.manage.add_module_row'] = 'Tnuocca ipa dda';
+$lang['CWatch.manage.option_edit'] = 'Tide';
+$lang['CWatch.manage.delete'] = 'Eteled';
+$lang['CWatch.manage.confirm_delete'] = '?resu siht eteled ot tnaw uoy erus uoy era';
+$lang['CWatch.manage.no_results'] = 'Dnuof stnuocca on';
+$lang['CWatch.manage.heading_username'] = 'Emanresu';
+$lang['CWatch.manage.heading_options'] = 'Snoitpo';
+$lang['CWatch.package_fields.package_type'] = 'Epyt egakcap';
+$lang['CWatch.package_fields.available_terms'] = 'Smret elbaliava';
+$lang['CWatch.package_fields.tooltip.package_type'] = 'Sepyt tnereffid fo sesnecil fo rebmun elbairav a redro ot sremotsuc wolla ot snoitpo elbarugifnoc esu segakcap esnecil itlum  .egakcap eht yb ereh detceles epyt eht fo hctawc ni esnecil eno ylno noisivorp segakcap esnecil elgnis';
+$lang['CWatch.package_fields.license_type'] = 'Epyt esnecil';
+$lang['CWatch.package_fields.license_basic_detection'] = 'Cisab';
+$lang['CWatch.package_fields.license_starter'] = 'Retrats';
+$lang['CWatch.package_fields.license_starter_paid_with_trial'] = 'Lairt htiw diap retrats';
+$lang['CWatch.package_fields.license_waf_starter'] = 'Retrats faw';
+$lang['CWatch.package_fields.license_pro'] = 'Lanoisseforp';
+$lang['CWatch.package_fields.license_pro_paid_with_trial'] = 'Lairt htiw diap lanoisseforp';
+$lang['CWatch.package_fields.license_pro_free'] = 'Lairt lanoisseforp';
+$lang['CWatch.package_fields.license_pro_free_60d'] = 'Lairt lanoisseforp dednetxe';
+$lang['CWatch.package_fields.license_premium'] = 'Muimerp';
+$lang['CWatch.package_fields.license_premium_paid_with_trial'] = 'Lairt htiw diap muimerp';
+$lang['CWatch.package_fields.license_premium_free'] = 'Lairt muimerp';
+$lang['CWatch.package_fields.license_premium_free_60d'] = 'Lairt muimerp dednetxe';
+$lang['CWatch.packagetypes.single_license'] = 'Esnecil elgnis';
+$lang['CWatch.packagetypes.multi_license'] = 'Esnecil itlum';
+$lang['Cwatch.service_field.domain'] = 'Niamod';
+$lang['Cwatch.service_field.email'] = 'Liame';
+$lang['Cwatch.service_field.firstname'] = 'Eman tsrif';
+$lang['Cwatch.service_field.lastname'] = 'Eman tsal';
+$lang['Cwatch.service_field.country'] = 'Yrtnuoc';
+$lang['CWatch.add_row.box_title'] = 'Tnuocca ipa dda';
+$lang['CWatch.add_row.basic_title'] = 'Sgnittes resu';
+$lang['CWatch.add_row.field_username'] = 'Emanresu';
+$lang['CWatch.add_row.field_password'] = 'Drowssap';
+$lang['CWatch.add_row.field_sandbox'] = 'Xobdnas elbane';
+$lang['CWatch.add_row.add_btn'] = 'Tnuocca dda';
+$lang['CWatch.edit_row.box_title'] = 'Tnuocca ipa tide';
+$lang['CWatch.edit_row.basic_title'] = 'Sgnittes resu';
+$lang['CWatch.edit_row.field_username'] = 'Emanresu';
+$lang['CWatch.edit_row.field_password'] = 'Drowssap';
+$lang['CWatch.edit_row.field_sandbox'] = 'Xobdnas elbane';
+$lang['CWatch.edit_row.add_btn'] = 'Slaitnederc tide';
+$lang['CWatch.service_info.option_login'] = 'Hctawc ot ni gol';
+$lang['CWatch.tab_licenses.licenses'] = 'Sesnecil eganam';
+$lang['CWatch.tab_licenses.inactive_licenses'] = 'Sesnecil evitcani';
+$lang['CWatch.tab_licenses.add'] = 'Etis wen dda';
+$lang['CWatch.tab_licenses.licenseKey'] = 'Yek esnecil';
+$lang['CWatch.tab_licenses.type'] = 'Epyt';
+$lang['CWatch.tab_licenses.domain'] = 'Niamod';
+$lang['CWatch.tab_licenses.status'] = 'Sutats niamod';
+$lang['CWatch.tab_licenses.malware_scanner'] = 'Rennacs';
+$lang['CWatch.tab_licenses.options'] = 'Snoitpo';
+$lang['CWatch.tab_licenses.no_upgrade_options'] = 'Edargpu rof elbaliava sesnecil on era ereht';
+$lang['CWatch.tab_licenses.no_results'] = 'Ecivres siht rof denoisivorp neeb evah sesnecil on';
+$lang['CWatch.tab_licenses.upgrade_delay'] = 'Nwohs eb llits yam epyt esnecil dlo eht os ,etagoporp ot emit emos ekat yam sedargpu';
+$lang['CWatch.tab_licenses.not_applicable'] = 'A/n';
+$lang['CWatch.tab_licenses.deactivate_license'] = 'Esnecil etavitcaed';
+$lang['CWatch.tab_licenses.remove_site'] = 'Etis evomer';
+$lang['CWatch.tab_licenses.add_site'] = 'Etis dda';
+$lang['CWatch.tab_licenses.upgrade_site'] = 'Etis edargpu';
+$lang['CWatch.tab_licenses.confirm_delete'] = '?niamod siht evomer ot tnaw uoy erus uoy era';
+$lang['CWatch.tab_licenses.confirm_deactivate'] = '?esnecil siht etavitcaed ot tnaw uoy erus uoy era';
+$lang['CWatch.tab_licenses.cancel'] = 'Lecnac';
+$lang['CWatch.tab_licenses.current_license'] = 'Esnecil tnerruc';
+$lang['CWatch.tab_licenses.license_name'] = ')%2$s( %1$s';
+$lang['CWatch.tab_licenses.initiateDns'] = 'Snd laitini';
+$lang['CWatch.tab_licenses.autoSsl'] = 'Lssotua';
+$lang['CWatch.tab_licenses.submit'] = 'Timbus';
+$lang['CWatch.tab_malware.malware'] = 'Lortnoc erawlam';
+$lang['CWatch.tab_malware.add_scanner'] = 'Rennacs dda';
+$lang['CWatch.tab_malware.test_credentials'] = 'Slaitnederc eseht tset';
+$lang['CWatch.tab_malware.domainname'] = 'Eman niamod';
+$lang['CWatch.tab_malware.ftppassword'] = 'Drowssap ptf';
+$lang['CWatch.tab_malware.ftpusername'] = 'Emanresu ptf';
+$lang['CWatch.tab_malware.host'] = 'Emantsoh ptf';
+$lang['CWatch.tab_malware.port'] = 'Trop ptf';
+$lang['CWatch.tab_malware.sftp'] = 'Ptfs esu';
+$lang['CWatch.tab_malware.path'] = 'Yrotcerid etis';
+$lang['CWatch.tab_malware.submit'] = 'Timbus';
+$lang['CWatch.getsitestatuses.waiting'] = 'Gnitiaw';
+$lang['CWatch.getsitestatuses.site_inprogress'] = 'Ssergorp ni noisivorp etis';
+$lang['CWatch.getsitestatuses.site_retry'] = 'Noisivorp etis gniyrter';
+$lang['CWatch.getsitestatuses.site_completed'] = 'Dedda etis';
+$lang['CWatch.getsitestatuses.site_failed'] = 'Deliaf noisivorp etis';
+$lang['CWatch.getsitestatuses.dns_inprogress'] = 'Ssergorp ni nacs snd';
+$lang['CWatch.getsitestatuses.dns_retry'] = 'Nacs snd gniyrter';
+$lang['CWatch.getsitestatuses.dns_completed'] = 'Detelpmoc nacs snd';
+$lang['CWatch.getsitestatuses.dns_failed'] = 'Deliaf nacs snd ,dedda etis';
+$lang['CWatch.getsitestatuses.ssl_inprogress'] = 'Ssergorp ni llatsni lss';
+$lang['CWatch.getsitestatuses.ssl_retry'] = 'Llatsni lss gniyrter';
+$lang['CWatch.getsitestatuses.ssl_completed'] = 'Dedda etacifitrec lss';
+$lang['CWatch.getsitestatuses.ssl_fail'] = 'Deliaf llatsni lss ,dedda etis';
+
