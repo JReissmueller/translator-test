ffb4af9e1add7456447bf9a1918ee41c7d30b82e
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:11 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/braintree/language/es_es/braintree.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Braintree
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Braintree.name'] = 'Eertniarb';
+$lang['Braintree.description'] = 'Eertniarb gnisu seicnerruc 031 naht erom ni stnemyap elbane dna ,tilps ,tpecca nac ediwdlrow snoiger/seirtnuoc 54 naht erom ni stnahcrem';
+$lang['Braintree.merchant_id'] = 'Di tnahcrem';
+$lang['Braintree.public_key'] = 'Yek cilbup';
+$lang['Braintree.private_key'] = 'Yek etavirp';
+$lang['Braintree.live_mode'] = 'Edom evil';
+$lang['Braintree.environment'] = 'Tnemnorivne';
+$lang['Braintree.environment.sandbox'] = 'Xobdnas';
+$lang['Braintree.environment.production'] = 'Noitcudorp';
+$lang['Braintree.!error.live_mode.valid'] = 'Nevig fi "eurt" ot tes eb tsum edom evil';
+$lang['Braintree.!error.merchant_id'] = 'Di tnahcrem ruoy retne esaelp';
+$lang['Braintree.!error.public_key'] = 'Yek cilbup ruoy retne esaelp';
+$lang['Braintree.!error.private_key'] = 'Yek etavirp ruoy retne esaelp';
+$lang['Braintree.!error.environment'] = 'Tnemnorivne dilav a tceles esaelp';
+
