a38d816b3a80d461a4d92b65bd56fe5a992cb3f6
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:21:05 -0800

    Updated es_es language files

---
+++ language/es_es/admin_system_backup.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Admin System Backup
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemBackup.!success.backup_updated'] = '!detadpu yllufsseccus erew sgnittes pukcab eht';
+$lang['AdminSystemBackup.!success.sftp_test'] = '!lufsseccus saw noitcennoc ptfs';
+$lang['AdminSystemBackup.!success.amazons3_test'] = '!lufsseccus saw noitcennoc 3snozama eht';
+$lang['AdminSystemBackup.!success.backup_uploaded'] = '!secivres etomer derugifnoc eht ot tnes yllufsseccus saw pukcab eht';
+$lang['AdminSystemBackup.!error.sftp_test'] = 'Niaga yrt dna sgnittes ruoy kcehc esaelp !deliaf noitcennoc ptfs eht';
+$lang['AdminSystemBackup.!error.amazons3_test'] = 'Evitisnes-esac era sliated noitcennoc taht eton .niaga yrt dna sgnittes ruoy kcehc esaelp !deliaf noitcennoc 3snozama eht';
+$lang['AdminSystemBackup.!error.backup_frequency'] = 'Ycneuqerf pukcab dilavni';
+$lang['AdminSystemBackup.!tooltip.ftp_host'] = ')"moc.niamod.pukcab" .g.e( revres pukcab eht fo eman niamod deifilauq-ylluf eht';
+$lang['AdminSystemBackup.!tooltip.ftp_port'] = 'Hss rof desu si sa rebmun trop emas eht si siht .22 yllausu ,rebmun trop eht';
+$lang['AdminSystemBackup.!tooltip.ftp_username'] = 'Tnuocca ptf eht rof emanresu eht';
+$lang['AdminSystemBackup.!tooltip.ftp_password'] = 'Tnuocca ptf eht rof drowssap eht';
+$lang['AdminSystemBackup.!tooltip.ftp_path'] = ')"/pukcab/" .g.e( revres etomer eht no derots eb dluohs spukcab erehw htap noitanitsed eht';
+$lang['AdminSystemBackup.!tooltip.ftp_rate'] = 'Pukcab siht mrofrep ot lavretni ycneuqerf eht';
+$lang['AdminSystemBackup.!tooltip.amazons3_region'] = 'Spukcab erots ot hcihw ta noiger 3s nozama eht';
+$lang['AdminSystemBackup.!tooltip.amazons3_access_key'] = 'Yek ssecca tnuocca 3s nozama eht';
+$lang['AdminSystemBackup.!tooltip.amazons3_secret_key'] = 'Yek terces tnuocca 3s nozama eht';
+$lang['AdminSystemBackup.!tooltip.amazons3_bucket'] = 'Spukcab erots ot hcihw rednu tekcub 3s nozama eht fo eman evitisnes-esac eht';
+$lang['AdminSystemBackup.!tooltip.amazons3_rate'] = 'Pukcab siht mrofrep ot lavretni ycneuqerf eht';
+$lang['AdminSystemBackup.ftp.page_title'] = 'Ptf eruces > pukcab > metsys > sgnittes';
+$lang['AdminSystemBackup.ftp.boxtitle_backup'] = 'Ptf eruces';
+$lang['AdminSystemBackup.ftp.field.test'] = 'Sgnittes eseht tset';
+$lang['AdminSystemBackup.ftp.field.ftp_host'] = 'Emantsoh';
+$lang['AdminSystemBackup.ftp.field.ftp_port'] = 'Trop';
+$lang['AdminSystemBackup.ftp.field.ftp_username'] = 'Emanresu';
+$lang['AdminSystemBackup.ftp.field.ftp_password'] = 'Drowssap';
+$lang['AdminSystemBackup.ftp.field.ftp_path'] = 'Htap';
+$lang['AdminSystemBackup.ftp.field.ftp_rate'] = 'Yreve pukcab';
+$lang['AdminSystemBackup.ftp.field.backupsubmit'] = 'Sgnittes etadpu';
+$lang['AdminSystemBackup.amazon.page_title'] = '3s nozama > pukcab > metsys > sgnittes';
+$lang['AdminSystemBackup.amazon.boxtitle_backup'] = '3s nozama';
+$lang['AdminSystemBackup.amazon.field.test'] = 'Sgnittes eseht tset';
+$lang['AdminSystemBackup.amazon.field.amazons3_region'] = 'Noiger';
+$lang['AdminSystemBackup.amazon.field.amazons3_accesskey'] = 'Yek ssecca';
+$lang['AdminSystemBackup.amazon.field.amazons3_secretkey'] = 'Yek terces';
+$lang['AdminSystemBackup.amazon.field.amazons3_bucket'] = 'Tekcub';
+$lang['AdminSystemBackup.amazon.field.amazons3_rate'] = 'Yreve pukcab';
+$lang['AdminSystemBackup.amazon.field.backupsubmit'] = 'Sgnittes etadpu';
+$lang['AdminSystemBackup.index.page_title'] = 'Dnamed no > pukcab > metsys > sgnittes';
+$lang['AdminSystemBackup.index.boxtitle_index'] = 'Dnamed no';
+$lang['AdminSystemBackup.index.field_uploadbackup'] = 'Pukcab etisffo ecrof';
+$lang['AdminSystemBackup.index.field_downloadbackup'] = 'Pukcab daolnwod';
+$lang['AdminSystemBackup.index.text_note'] = 'Revres 3s nozama ro/dna ptfs derugifnoc ruoy ot pukcab a daolpu yllacitamotua ro retupmoc ruoy ot esabatad atselb ruoy fo pukcab a daolnwod nac uoy ereh';
+
