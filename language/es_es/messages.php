83dd2bb4033e3dbfc8ea55ee397d570c6f5a9d1a
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 15:26:11 -0800

    Updated es_es language files

--- language/es_es/messages.php
+++ language/es_es/messages.php
@@ -9,12 +9,12 @@
  * @link http://www.blesta.com/ Blesta
  */
 
-$lang['Messages.!error.message_group_id.valid'] = 'Di puorg egassem dilavni';
-$lang['Messages.!error.company_id.valid'] = 'Di ynapmoc dilavni';
-$lang['Messages.!error.type.valid'] = '"sms" ot tes eb tsum epyt egassem eht';
-$lang['Messages.!error.status.valid'] = '"evitcani" ro "evitca" ot tes eb tsum sutats egassem eht';
-$lang['Messages.!error.content.format'] = 'Yarra na eb tsum tnetnoc egassem eht';
-$lang['Messages.getTypes.sms'] = 'Sms';
-$lang['Messages.getStatusTypes.active'] = 'Evitca';
-$lang['Messages.getStatusTypes.inactive'] = 'Evitcani';
+$lang['Messages.!error.message_group_id.valid'] = 'ID de grupo de mensajes no válido.';
+$lang['Messages.!error.company_id.valid'] = 'ID de empresa no válida.';
+$lang['Messages.!error.type.valid'] = 'El tipo de mensaje debe ser "sms".';
+$lang['Messages.!error.status.valid'] = 'El estado del mensaje debe ser "activo" o "inactivo".';
+$lang['Messages.!error.content.format'] = 'El contenido del mensaje debe ser un array.';
+$lang['Messages.getTypes.sms'] = 'SMS';
+$lang['Messages.getStatusTypes.active'] = 'Activo';
+$lang['Messages.getStatusTypes.inactive'] = 'Inactivo';
 
