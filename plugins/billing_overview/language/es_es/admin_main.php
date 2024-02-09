3552bbc654b971b20e351a5dcdbc2f82d79d0479
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:22:20 -0800

    Updated es_es language files

---
+++ plugins/billing_overview/language/es_es/admin_main.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.options_updated'] = '!detadpu yllufsseccus erew snoitpo ecnalg a ta gnillib eht';
+$lang['AdminMain.index.boxtitle_overview'] = 'Ecnalg a ta gnillib';
+$lang['AdminMain.index.statistic.revenue_today'] = 'Yadot eunever';
+$lang['AdminMain.index.statistic.revenue_month'] = 'Htnom siht eunever';
+$lang['AdminMain.index.statistic.revenue_year'] = 'Raey siht eunever';
+$lang['AdminMain.index.statistic.credits_today'] = 'Yadot stiderc';
+$lang['AdminMain.index.statistic.credits_month'] = 'Htnom siht stiderc';
+$lang['AdminMain.index.statistic.credits_year'] = 'Raey siht stiderc';
+$lang['AdminMain.index.statistic.invoiced_today'] = 'Yadot deciovni';
+$lang['AdminMain.index.statistic.invoiced_month'] = 'Htnom siht deciovni';
+$lang['AdminMain.index.statistic.invoiced_today_proforma'] = ')amroforp( yadot deciovni';
+$lang['AdminMain.index.statistic.invoiced_month_proforma'] = ')amroforp( htnom siht deciovni';
+$lang['AdminMain.index.statistic.balance_outstanding'] = 'Ecnalab gnidnatstuo';
+$lang['AdminMain.index.statistic.balance_overdue'] = 'Ecnalab eudrevo';
+$lang['AdminMain.index.statistic.scheduled_cancelation'] = 'Snoitalecnac deludehcs';
+$lang['AdminMain.index.statistic.services_active'] = 'Secivres evitca';
+$lang['AdminMain.index.statistic.services_added_today'] = 'Yadot dedda secivres';
+$lang['AdminMain.index.statistic.services_canceled_today'] = 'Yadot delecnac secivres';
+$lang['AdminMain.settings.text_options'] = 'Weivrevo gnillib eht no raeppa lliw taht snoitpo eht kcehc';
+$lang['AdminMain.settings.heading_statistics'] = 'Scitsitats';
+$lang['AdminMain.settings.heading_graphs'] = 'Shparg';
+$lang['AdminMain.settings.heading_graph_settings'] = 'Sgnittes hparg';
+$lang['AdminMain.settings.revenue_today'] = 'Yadot eunever';
+$lang['AdminMain.settings.revenue_month'] = 'Htnom siht eunever';
+$lang['AdminMain.settings.revenue_year'] = 'Raey siht eunever';
+$lang['AdminMain.settings.credits_today'] = 'Yadot stiderc';
+$lang['AdminMain.settings.credits_month'] = 'Htnom siht stiderc';
+$lang['AdminMain.settings.credits_year'] = 'Raey siht stiderc';
+$lang['AdminMain.settings.invoiced_today'] = 'Yadot deciovni';
+$lang['AdminMain.settings.invoiced_month'] = 'Htnom siht deciovni';
+$lang['AdminMain.settings.invoiced_today_proforma'] = ')amroforp( yadot deciovni';
+$lang['AdminMain.settings.invoiced_month_proforma'] = ')amroforp( htnom siht deciovni';
+$lang['AdminMain.settings.balance_outstanding'] = 'Ecnalab gnidnatstuo';
+$lang['AdminMain.settings.balance_overdue'] = 'Ecnalab eudrevo';
+$lang['AdminMain.settings.scheduled_cancelation'] = 'Snoitalecnac deludehcs';
+$lang['AdminMain.settings.services_active'] = 'Secivres evitca';
+$lang['AdminMain.settings.services_added_today'] = 'Yadot dedda secivres';
+$lang['AdminMain.settings.services_canceled_today'] = 'Yadot delecnac secivres';
+$lang['AdminMain.settings.text_graphs'] = 'Weivrevo gnillib eht no raeppa dluohs shparg hcihw kcehc';
+$lang['AdminMain.settings.graph_revenue'] = ')egnar( hparg eunever';
+$lang['AdminMain.settings.graph_revenue_year'] = ')etad ot raey( hparg eunever';
+$lang['AdminMain.settings.graph_invoiced'] = 'Hparg deciovni tnuoma';
+$lang['AdminMain.settings.text_graph_settings'] = 'Weivrevo gnillib eht no raeppa lliw taht shparg eht rof sgnittes hparg eht eganam';
+$lang['AdminMain.settings.show_legend'] = 'Shparg no dnegel wohs';
+$lang['AdminMain.settings.date_range'] = 'Egnar etad hparg';
+$lang['AdminMain.settings.submit_settings'] = 'Sgnittes etadpu';
+$lang['AdminMain.settings.submit_cancel'] = 'Weivrevo ot nruter';
+$lang['AdminMain.date_range.days'] = 'Syad';
+$lang['AdminMain.graph_name.graph_revenue'] = ')egnar( eunever';
+$lang['AdminMain.graph_name.graph_revenue_year'] = ')etad ot raey( eunever';
+$lang['AdminMain.graph_name.graph_invoiced'] = 'Deciovni tnuoma';
+$lang['AdminMain.graph_line_name.total'] = 'Latot';
+$lang['AdminMain.graph_line_name.credit'] = 'Drac tiderc';
+$lang['AdminMain.graph_line_name.ach'] = 'Hca';
+$lang['AdminMain.graph_line_name.other'] = 'Rehto';
+$lang['AdminMain.graph_line_name.credits'] = 'Stiderc';
+
