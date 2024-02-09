bd9fa74ac4504ffe817d92d3789a71bf2445736d
JReissmueller <jonathan@phillipsdata.com>
2024-02-09 13:19:32 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/payfast/language/es_es/payfast.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Payfast
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payfast.name'] = 'Tsafyap';
+$lang['Payfast.description'] = 'Dlrow eht ni erehwyna tsomla morf acirfa htuos ni stnemyap enilno eruces dna tsaf ecneirepxe';
+$lang['Payfast.!error.merchant_id.valid'] = 'Dilavni di tnahcrem';
+$lang['Payfast.!error.merchant_key.valid'] = 'Dilavni yek tnahcrem';
+$lang['Payfast.!error.sandbox.valid'] = 'Dilavni eulav xobdnas';
+$lang['Payfast.meta.merchant_id'] = 'Di tnahcrem';
+$lang['Payfast.meta.merchant_key'] = 'Yek tnahcrem';
+$lang['Payfast.meta.passphrase'] = 'Esarhpssap';
+$lang['Payfast.meta.sandbox'] = 'Xobdnas';
+$lang['Payfast.buildprocess.submit'] = 'Tsafyap htiw yap';
+
